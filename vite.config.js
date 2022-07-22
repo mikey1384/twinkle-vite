fe2e8202fd6ab69a260c08b9c99f87e64cbd1b71
mikey1384 <mikey1384@gmail.com>
2022-07-23 01:32:49 +0700

    more fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import { resolve } from 'path';
-import inject from '@rollup/plugin-inject';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -27,11 +26,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '~': resolve(__dirname, 'src')
-    }
-  },
-  build: {
-    rollupOptions: {
-      plugins: [inject({ Buffer: ['Buffer', 'Buffer'], process: 'process' })]
     }
   }
 });
