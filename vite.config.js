34843c5d42e9ff732aee5188a748b34f1b53827a
mikey1384 <mikey1384@gmail.com>
2022-07-23 13:38:43 +0700

    trying to make page navigation work

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import { resolve } from 'path';
+import { splitVendorChunkPlugin } from 'vite';
 import inject from '@rollup/plugin-inject';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   server: {
     port: '3000'
   },
@@ -34,7 +35,10 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      plugins: [inject({ Buffer: ['buffer', 'Buffer'], process: 'process' })]
+      plugins: [inject({ Buffer: ['buffer', 'Buffer'], process: 'process' })],
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
     }
   }
 });
