86fff1a5e3f8ed2f0ae4d4bb1a0f6369b04015e8
mikey1384 <mikey1384@gmail.com>
2022-07-30 09:32:43 +0700

    more fix

--- src/components/Forms/CodeSandbox/Editor/Preview.jsx
+++ src/components/Forms/CodeSandbox/Editor/Preview.jsx
@@ -4,23 +4,15 @@ import Loading from '~/components/Loading';
 import { css } from '@emotion/css';
 
 Preview.propTypes = {
-  numKeyInput: PropTypes.number,
   evaling: PropTypes.bool,
   style: PropTypes.object,
   children: PropTypes.node,
   onError: PropTypes.func
 };
 
-export default function Preview({
-  numKeyInput,
-  evaling,
-  style,
-  children,
-  onError
-}) {
+export default function Preview({ evaling, style, children, onError }) {
   return (
     <PreviewErrorBoundary
-      numKeyInput={numKeyInput}
       onError={onError}
       style={{
         opacity: evaling ? 0.5 : 1,
