d887e7d28b8b97971c053414361fb2e85551387f
mikey1384 <mikey1384@gmail.com>
2022-08-06 20:25:59 +0700

    more updates

--- src/containers/Home/Earn/EarnSuggester/StartMenu.jsx
+++ src/containers/Home/Earn/EarnSuggester/StartMenu.jsx
@@ -1,7 +1,45 @@
+import ErrorBoundary from '~/components/ErrorBoundary';
+import Button from '~/components/Button';
+import Icon from '~/components/Icon';
+
 export default function StartMenu() {
   return (
-    <div>
-      <div>start menu</div>
-    </div>
+    <ErrorBoundary componentPath="Home/Earn/EarnSuggester/StartMenu">
+      <div
+        style={{
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column'
+        }}
+      >
+        <p style={{ fontWeight: 'bold', fontSize: '2rem' }}>Earn XP</p>
+        <Button style={{ marginTop: '1rem' }} filled color="logoBlue">
+          <Icon icon="bolt" />
+          <span style={{ marginLeft: '1rem' }}>
+            Respond to high XP subjects
+          </span>
+        </Button>
+      </div>
+      <div
+        style={{
+          marginTop: '2rem',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column'
+        }}
+      >
+        <p style={{ fontWeight: 'bold', fontSize: '2rem' }}>
+          Earn Karma Points
+        </p>
+        <Button style={{ marginTop: '0.7rem' }} filled color="brownOrange">
+          <Icon icon="heart" />
+          <span style={{ marginLeft: '0.7rem' }}>Recommend posts</span>
+        </Button>
+        <Button style={{ marginTop: '0.7rem' }} filled color="pink">
+          <Icon icon="certificate" />
+          <span style={{ marginLeft: '0.7rem' }}>Reward posts</span>
+        </Button>
+      </div>
+    </ErrorBoundary>
   );
 }
