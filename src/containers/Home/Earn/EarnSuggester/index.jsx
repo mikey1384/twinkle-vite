71b729489b1f06a649e4f1b7042a0410ad4abecf
mikey1384 <mikey1384@gmail.com>
2022-08-06 20:03:01 +0700

    add start menu component for Earn section

--- src/containers/Home/Earn/EarnSuggester/index.jsx
+++ src/containers/Home/Earn/EarnSuggester/index.jsx
@@ -1,15 +1,19 @@
 import PropTypes from 'prop-types';
 import { panel } from '../Styles';
+import { useHomeContext } from '~/contexts';
 import EarnXPFromSubjects from './EarnXPFromSubjects';
+import StartMenu from './StartMenu';
 
 EarnSuggester.propTypes = {
   style: PropTypes.object
 };
 
 export default function EarnSuggester({ style }) {
+  const earnSection = useHomeContext((v) => v.state.earnSection);
+
   return (
     <div style={style} className={panel}>
-      <EarnXPFromSubjects />
+      {earnSection === 'start' ? <StartMenu /> : <EarnXPFromSubjects />}
     </div>
   );
 }
