0a27475c37f094f0c2065308b953a1b32e64157f
mikey1384 <mikey1384@gmail.com>
2022-08-12 19:51:04 +0700

    minor fix

--- src/containers/Profile/Body/Home/Achievements/NotableActivities/ActivityItem.jsx
+++ src/containers/Profile/Body/Home/Achievements/NotableActivities/ActivityItem.jsx
@@ -1,9 +1,12 @@
+import { useEffect, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import ContentFileViewer from '~/components/ContentFileViewer';
 import LoginToViewContent from '~/components/LoginToViewContent';
+import SecretComment from '~/components/SecretComment';
 import { useNavigate } from 'react-router-dom';
+import { useContentState } from '~/helpers/hooks';
 import { Color, borderRadius, mobileMaxWidth } from '~/constants/css';
-import { useKeyContext } from '~/contexts';
+import { useAppContext, useKeyContext, useContentContext } from '~/contexts';
 import { css } from '@emotion/css';
 
 ActivityItem.propTypes = {
@@ -13,8 +16,69 @@ ActivityItem.propTypes = {
 
 export default function ActivityItem({ post, style }) {
   const { userId } = useKeyContext((v) => v.myState);
+  const onChangeSpoilerStatus = useContentContext(
+    (v) => v.actions.onChangeSpoilerStatus
+  );
+  const checkIfUserResponded = useAppContext(
+    (v) => v.requestHelpers.checkIfUserResponded
+  );
   const navigate = useNavigate();
-  const { contentId, content, filePath, fileName, fileSize, thumbUrl } = post;
+  const {
+    contentId,
+    content,
+    filePath,
+    fileName,
+    fileSize,
+    thumbUrl,
+    secretAnswer,
+    secretAttachment,
+    subjectId,
+    subjectUploaderId
+  } = post;
+
+  const subjectState = useContentState({
+    contentType: 'subject',
+    contentId: subjectId
+  });
+
+  useEffect(() => {
+    if (
+      userId &&
+      (secretAnswer || secretAttachment) &&
+      subjectId &&
+      subjectState.prevSecretViewerId !== userId
+    ) {
+      handleCheckSecretShown();
+    }
+    if (!userId) {
+      onChangeSpoilerStatus({
+        shown: false,
+        subjectId
+      });
+    }
+
+    async function handleCheckSecretShown() {
+      const { responded } = await checkIfUserResponded(subjectId);
+      onChangeSpoilerStatus({
+        shown: responded,
+        subjectId,
+        prevSecretViewerId: userId
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [subjectId, subjectState?.prevSecretViewerId, userId]);
+
+  const isHidden = useMemo(() => {
+    const secretShown =
+      subjectState.secretShown || subjectUploaderId === userId;
+    return (secretAnswer || secretAttachment) && !secretShown;
+  }, [
+    secretAnswer,
+    secretAttachment,
+    subjectState.secretShown,
+    subjectUploaderId,
+    userId
+  ]);
 
   return (
     <div
@@ -77,52 +141,56 @@ export default function ActivityItem({ post, style }) {
                   <p style={{ fontWeight: 'bold', fontSize: '1.7rem' }}>
                     {post.subjectTitle}
                   </p>
-                  <div
-                    style={{
-                      fontSize: '1.5rem',
-                      marginTop: '1.7rem',
-                      width: '100%',
-                      textAlign: 'left',
-                      color: Color.black(),
-                      whiteSpace: 'pre-wrap',
-                      overflowWrap: 'break-word',
-                      wordBreak: 'break-word',
-                      display: '-webkit-box',
-                      WebkitBoxOrient: 'vertical',
-                      overflow: 'hidden',
-                      WebkitLineClamp: 7
-                    }}
-                  >
-                    {filePath && (
-                      <div>
-                        {userId ? (
-                          <div style={{ width: '100%' }}>
-                            <ContentFileViewer
-                              isThumb
-                              contentId={contentId}
-                              contentType="comment"
-                              fileName={fileName}
-                              filePath={filePath}
-                              fileSize={Number(fileSize)}
-                              thumbUrl={thumbUrl}
-                              videoHeight="100%"
-                              thumbHeight="100%"
-                              style={{
-                                display: 'flex',
-                                justifyContent: 'center',
-                                width: '100%',
-                                height: 'auto',
-                                marginBottom: '1rem'
-                              }}
-                            />
-                          </div>
-                        ) : (
-                          <LoginToViewContent />
-                        )}
-                      </div>
-                    )}
-                    <div>{content}</div>
-                  </div>
+                  {isHidden ? (
+                    <SecretComment style={{ marginTop: '1.7rem' }} />
+                  ) : (
+                    <div
+                      style={{
+                        fontSize: '1.5rem',
+                        marginTop: '1.7rem',
+                        width: '100%',
+                        textAlign: 'left',
+                        color: Color.black(),
+                        whiteSpace: 'pre-wrap',
+                        overflowWrap: 'break-word',
+                        wordBreak: 'break-word',
+                        display: '-webkit-box',
+                        WebkitBoxOrient: 'vertical',
+                        overflow: 'hidden',
+                        WebkitLineClamp: 7
+                      }}
+                    >
+                      {filePath && (
+                        <div>
+                          {userId ? (
+                            <div style={{ width: '100%' }}>
+                              <ContentFileViewer
+                                isThumb
+                                contentId={contentId}
+                                contentType="comment"
+                                fileName={fileName}
+                                filePath={filePath}
+                                fileSize={Number(fileSize)}
+                                thumbUrl={thumbUrl}
+                                videoHeight="100%"
+                                thumbHeight="100%"
+                                style={{
+                                  display: 'flex',
+                                  justifyContent: 'center',
+                                  width: '100%',
+                                  height: 'auto',
+                                  marginBottom: '1rem'
+                                }}
+                              />
+                            </div>
+                          ) : (
+                            <LoginToViewContent />
+                          )}
+                        </div>
+                      )}
+                      <div>{content}</div>
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
