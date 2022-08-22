1b2253b5b834c2bfe6d92d987f30fac694cabaa9
mikey1384 <mikey1384@gmail.com>
2022-08-22 10:34:44 +0700

    more css updates

--- src/containers/Chat/LeftMenu/Subchannels.jsx
+++ src/containers/Chat/LeftMenu/Subchannels.jsx
@@ -1,10 +1,26 @@
+import ErrorBoundary from '~/components/ErrorBoundary';
+
 export default function SubChannels() {
   return (
-    <div>
-      <div>menu1</div>
-      <div>menu2</div>
-      <div>menu3</div>
-      <div>menu4</div>
-    </div>
+    <ErrorBoundary componentPath="Chat/LeftMenu/Subchannels">
+      <div
+        style={{
+          border: '1px solid #e6e6e6',
+          padding: '1rem',
+          marginTop: '2rem',
+          marginLeft: '1rem',
+          marginRight: '1rem',
+          marginBottom: '-1rem',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center'
+        }}
+      >
+        <div>menu1</div>
+        <div>menu2</div>
+        <div>menu3</div>
+        <div>menu4</div>
+      </div>
+    </ErrorBoundary>
   );
 }
