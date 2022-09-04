da2c05e58f0328f4a3e89853807867769e948461
mikey1384 <mikey1384@gmail.com>
2022-09-04 15:14:07 +0700

    more fixes

--- src/contexts/Chat/reducer.js
+++ src/contexts/Chat/reducer.js
@@ -919,14 +919,14 @@ export default function ChatReducer(state, action) {
                   }
                 },
                 numUnreads: 0,
-                members: state.channelsObj[
-                  action.data.channelId
-                ].members.filter((member) => member.id !== action.data.userId)
+                members: (
+                  state.channelsObj[action.data.channelId].members || []
+                ).filter((member) => member.id !== action.data.userId)
               }
             }
           }
         : state;
-      // this will mean that if the channel where the user has left is not loaded in the left channel list initially, it will not appear in the list when user scrolls down and triggers "load more" event (because load more event only loads channels with older update time than the bottom item) but is that really that bad? this channel will surface when user reloads the website anyway and user wasn't really interested in this channel to keep it bumped up in the first place.
+      // this will mean that if the channel where the user has left is not loaded in the left channel list initially, it will not appear in the list when user scrolls down and triggers "load more" event (because load more event only loads channels with older update time than the bottom item) and because this is new update. but is that really that bad? this channel will surface when user reloads the website anyway and user wasn't really interested in this channel to keep it bumped up in the first place.
     }
     case 'OPEN_NEW_TAB':
       return {
