8a0c6304449e37c3faf973da25d59b2856dec6d7
mikey1384 <mikey1384@gmail.com>
2022-09-14 10:53:50 +0700

    bug fix

--- src/containers/Chat/Router.jsx
+++ src/containers/Chat/Router.jsx
@@ -1,7 +1,12 @@
 import { Route, Routes, useParams } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Main from './Main';
 
-export default function Router() {
+Router.propTypes = {
+  onFileUpload: PropTypes.func.isRequired
+};
+
+export default function Router({ onFileUpload }) {
   const { currentPathId } = useParams();
   return (
     <Routes>
@@ -9,7 +14,12 @@ export default function Router() {
         path="/:subchannelPath"
         element={<Main currentPathId={currentPathId} />}
       />
-      <Route path="/*" element={<Main currentPathId={currentPathId} />} />
+      <Route
+        path="/*"
+        element={
+          <Main onFileUpload={onFileUpload} currentPathId={currentPathId} />
+        }
+      />
     </Routes>
   );
 }
