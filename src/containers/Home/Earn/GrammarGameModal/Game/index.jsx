896d1d433e7eea56f55738c2b06ac4a0ff828ba7
mikey1384 <mikey1384@gmail.com>
2022-09-29 20:48:23 +0700

    more refactoring

--- src/containers/Home/Earn/GrammarGameModal/Game/index.jsx
+++ src/containers/Home/Earn/GrammarGameModal/Game/index.jsx
@@ -1,4 +1,3 @@
-import { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import Loading from '~/components/Loading';
 import QuestionCarousel from './QuestionCarousel';
@@ -8,23 +7,10 @@ Game.propTypes = {
 };
 
 export default function Game({ questions = [] }) {
-  const [submitDisabled, setSubmitDisabled] = useState(true);
-  const statusRef = useRef(null);
-  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
-
   return (
     <div style={{ width: '100%', padding: '0 3rem 2rem 3rem' }}>
       {questions.length > 0 ? (
-        <QuestionCarousel
-          currentSlideIndex={currentSlideIndex}
-          onAfterSlide={(index) => {
-            statusRef.current = null;
-            setSubmitDisabled(true);
-            setCurrentSlideIndex(index);
-          }}
-          questions={questions}
-          submitDisabled={submitDisabled}
-        />
+        <QuestionCarousel questions={questions} />
       ) : (
         <Loading />
       )}
