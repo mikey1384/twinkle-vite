6928661bb0274ba2f04deb44f1b96049c8bceb22
mikey1384 <mikey1384@gmail.com>
2022-09-29 10:58:52 +0700

    implement load grammar api helper

--- src/containers/Home/Earn/GrammarGameModal/index.jsx
+++ src/containers/Home/Earn/GrammarGameModal/index.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Modal from '~/components/Modal';
 import FilterBar from '~/components/FilterBar';
 import Game from './Game';
+import { useAppContext } from '~/contexts';
 
 GrammarGameModal.propTypes = {
   onHide: PropTypes.func.isRequired
 };
 export default function GrammarGameModal({ onHide }) {
   const [activeTab, setActiveTab] = useState('game');
+  const [questions, setQuestions] = useState([]);
+  const loadGrammarGame = useAppContext(
+    (v) => v.requestHelpers.loadGrammarGame
+  );
+
+  useEffect(() => {
+    init();
+    async function init() {
+      const questions = await loadGrammarGame();
+      setQuestions(questions);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Modal large closeWhenClickedOutside={false} onHide={onHide}>
       <header style={{ padding: 0 }}>
@@ -39,7 +54,7 @@ export default function GrammarGameModal({ onHide }) {
         }}
       >
         {activeTab === 'game' ? (
-          <Game />
+          <Game questions={questions} />
         ) : activeTab === 'rankings' ? (
           <div>Rankings</div>
         ) : (
