880e3559f2d58da26a3746acb608aeb913ad9eec
mikey1384 <mikey1384@gmail.com>
2022-09-30 09:58:02 +0700

    more viewer updates

--- src/containers/Home/Earn/GrammarGameModal/Game/QuestionViewer.jsx
+++ src/containers/Home/Earn/GrammarGameModal/Game/QuestionViewer.jsx
@@ -1,47 +1,25 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 import ErrorBoundary from '~/components/ErrorBoundary';
 import QuestionSlide from './QuestionSlide';
-import * as d3Ease from 'd3-ease';
-import { Animate } from 'react-move';
-import { addEvent, removeEvent } from '~/helpers/listenerHelpers';
+import SlideContainer from './SlideContainer';
+import Loading from '~/components/Loading';
 
 QuestionViewer.propTypes = {
   questions: PropTypes.array,
   slideIndex: PropTypes.number,
-  slidesToShow: PropTypes.number,
-  style: PropTypes.object
+  slidesToShow: PropTypes.number
 };
 
 export default function QuestionViewer({
   questions,
   slideIndex = 0,
-  slidesToShow = 1,
-  style
+  slidesToShow = 1
 }) {
-  const [questionIds, setQuestionIds] = useState([]);
+  const [questionIds, setQuestionIds] = useState(null);
   const [questionObj, setQuestionObj] = useState({});
-  const DEFAULT_DURATION = 300;
-  const DEFAULT_EASING = 'easeCircleOut';
-  const [left, setLeft] = useState(0);
-  const [easing, setEasing] = useState(DEFAULT_EASING);
   const [currentSlide, setCurrentSlide] = useState(slideIndex);
-  const [slideWidth, setSlideWidth] = useState(0);
-  const FrameRef = useRef(null);
   const slideCount = questionIds?.length;
-
-  useEffect(() => {
-    setSlideWidth(FrameRef.current.offsetWidth / slidesToShow);
-  }, [slidesToShow]);
-
-  useEffect(() => {
-    addEvent(window, 'resize', onResize);
-    addEvent(document, 'readystatechange', onReadyStateChange);
-    return function cleanUp() {
-      removeEvent(window, 'resize', onResize);
-      removeEvent(document, 'readystatechange', onReadyStateChange);
-    };
-  });
 
   useEffect(() => {
     const resultObj = questions.reduce((prev, curr, index) => {
@@ -62,164 +40,51 @@ export default function QuestionViewer({
     setQuestionIds([...Array(questions.length).keys()]);
   }, [questions]);
 
+  const Slides = useMemo(() => {
+    return questionIds?.map((questionId) => (
+      <QuestionSlide
+        key={questionId}
+        gotWrong={questionObj[questionId].gotWrong}
+        question={questionObj[questionId].question}
+        choices={questionObj[questionId].choices}
+        answerIndex={questionObj[questionId].answerIndex}
+        onSelectChoice={(selectedIndex) => {
+          handleSelectChoice({ selectedIndex, questionId });
+        }}
+      />
+    ));
+    function handleSelectChoice({ selectedIndex, questionId }) {
+      setQuestionObj((questionObj) => ({
+        ...questionObj,
+        [questionId]: {
+          ...questionObj[questionId],
+          choices: questionObj[questionId].choices.map((choice, index) =>
+            index === selectedIndex
+              ? { ...choice, checked: true }
+              : { ...choice, checked: false }
+          )
+        },
+        selectedChoiceIndex: selectedIndex
+      }));
+      handleGoToNextSlide();
+    }
+
+    function handleGoToNextSlide() {
+      if (currentSlide < slideCount - slidesToShow) {
+        handleGoToSlide(Math.min(currentSlide + 1, slideCount - slidesToShow));
+      }
+      function handleGoToSlide(index) {
+        if (index >= slideCount || index < 0 || currentSlide === index) {
+          return;
+        }
+        setCurrentSlide(index);
+      }
+    }
+  }, [currentSlide, questionIds, questionObj, slideCount, slidesToShow]);
+
   return (
     <ErrorBoundary componentPath="GrammarGameModal/Game/Carousel/index">
-      <div
-        style={{
-          width: '100%',
-          position: 'relative',
-          fontSize: '1.5rem',
-          height: 'auto',
-          boxSizing: 'border-box',
-          visibility: slideWidth ? 'visible' : 'hidden',
-          ...style
-        }}
-      >
-        <div
-          className="slider-frame"
-          ref={FrameRef}
-          style={{
-            position: 'relative',
-            display: 'block',
-            overflowX: 'hidden',
-            height: 'auto',
-            margin: 0,
-            padding: '6px',
-            transform: 'translate3d(0, 0, 0)',
-            boxSizing: 'border-box'
-          }}
-        >
-          <Animate
-            show
-            start={{ tx: 0, ty: 0 }}
-            update={() => {
-              const { tx, ty } = handleGetOffsetDeltas();
-              return {
-                tx,
-                ty,
-                timing: {
-                  duration: DEFAULT_DURATION,
-                  ease: d3Ease[easing]
-                },
-                events: {
-                  end: () => {
-                    const newLeft = handleGetTargetLeft();
-                    if (newLeft !== left) {
-                      setLeft(newLeft);
-                    }
-                  }
-                }
-              };
-            }}
-          >
-            {({ tx, ty }) => (
-              <ul
-                style={{
-                  position: 'relative',
-                  transform: `translate3d(${tx}px, ${ty}px, 0)`,
-                  display: !!slideWidth ? 'block' : 'none',
-                  margin: 0,
-                  padding: 0,
-                  height: 'auto',
-                  width: slideWidth * slideCount,
-                  cursor: 'pointer',
-                  boxSizing: 'border-box'
-                }}
-              >
-                {questionIds.map((questionId, index) => (
-                  <li
-                    style={{
-                      display: 'inline-block',
-                      listStyleType: 'none',
-                      verticalAlign: 'top',
-                      width: slideWidth,
-                      height: 'auto',
-                      boxSizing: 'border-box',
-                      MozBoxSizing: 'border-box',
-                      marginLeft: -0.5,
-                      marginRight: -0.5,
-                      marginTop: 'auto',
-                      marginBottom: 'auto'
-                    }}
-                    key={index}
-                  >
-                    <QuestionSlide
-                      key={questionId}
-                      gotWrong={questionObj[questionId].gotWrong}
-                      question={questionObj[questionId].question}
-                      choices={questionObj[questionId].choices}
-                      answerIndex={questionObj[questionId].answerIndex}
-                      onSelectChoice={(selectedIndex) => {
-                        handleSelectChoice({ selectedIndex, questionId });
-                      }}
-                    />
-                  </li>
-                ))}
-              </ul>
-            )}
-          </Animate>
-        </div>
-      </div>
+      <SlideContainer>{Slides || <Loading />}</SlideContainer>
     </ErrorBoundary>
   );
-
-  function handleGetTargetLeft() {
-    const target = currentSlide;
-    const left = slideWidth * target;
-    return left * -1;
-  }
-
-  function handleGetOffsetDeltas() {
-    const offset = handleGetTargetLeft();
-    return {
-      tx: [offset],
-      ty: [0]
-    };
-  }
-
-  function handleSelectChoice({ selectedIndex, questionId }) {
-    setQuestionObj((questionObj) => ({
-      ...questionObj,
-      [questionId]: {
-        ...questionObj[questionId],
-        choices: questionObj[questionId].choices.map((choice, index) =>
-          index === selectedIndex
-            ? { ...choice, checked: true }
-            : { ...choice, checked: false }
-        )
-      },
-      selectedChoiceIndex: selectedIndex
-    }));
-    handleGoToNextSlide();
-  }
-
-  function handleGoToNextSlide() {
-    if (currentSlide < slideCount - slidesToShow) {
-      handleGoToSlide(Math.min(currentSlide + 1, slideCount - slidesToShow));
-    }
-    function handleGoToSlide(index) {
-      if (index >= slideCount || index < 0 || currentSlide === index) {
-        return;
-      }
-      setEasing(DEFAULT_EASING);
-      setLeft(handleGetTargetLeft(slideWidth, currentSlide));
-      setCurrentSlide(index);
-    }
-  }
-
-  function onResize() {
-    renderDimensions(FrameRef);
-  }
-
-  function onReadyStateChange() {
-    renderDimensions(FrameRef);
-  }
-
-  function renderDimensions(ref) {
-    const firstSlide = ref.current.childNodes[0].childNodes[0];
-    if (firstSlide) {
-      firstSlide.style.height = 'auto';
-    }
-    setSlideWidth(ref.current.offsetWidth / slidesToShow);
-  }
 }
