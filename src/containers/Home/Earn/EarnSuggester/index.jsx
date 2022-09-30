adbf207c1a5ec2c52fe899eedba3145f70e5a381
mikey1384 <mikey1384@gmail.com>
2022-09-30 16:34:49 +0700

    more updates

--- src/containers/Home/Earn/EarnSuggester/index.jsx
+++ src/containers/Home/Earn/EarnSuggester/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { panel } from '../Styles';
 import { useHomeContext } from '~/contexts';
@@ -5,24 +6,31 @@ import EarnXPFromSubjects from './EarnXPFromSubjects';
 import RecommendPosts from './RecommendPosts';
 import RewardPosts from './RewardPosts';
 import StartMenu from './StartMenu';
+import GrammarGameModal from '../GrammarGameModal';
 
 EarnSuggester.propTypes = {
   style: PropTypes.object
 };
 
 export default function EarnSuggester({ style }) {
+  const [grammarGameModalShown, setGrammarGameModalShown] = useState(false);
   const earnSection = useHomeContext((v) => v.state.earnSection);
 
   return (
     <div style={style} className={panel}>
       {earnSection === 'start' ? (
-        <StartMenu />
+        <StartMenu onSetGrammarGameModalShown={setGrammarGameModalShown} />
       ) : earnSection === 'subject' ? (
-        <EarnXPFromSubjects />
+        <EarnXPFromSubjects
+          onSetGrammarGameModalShown={setGrammarGameModalShown}
+        />
       ) : earnSection === 'recommend' ? (
-        <RecommendPosts />
+        <RecommendPosts onSetGrammarGameModalShown={setGrammarGameModalShown} />
       ) : (
-        <RewardPosts />
+        <RewardPosts onSetGrammarGameModalShown={setGrammarGameModalShown} />
+      )}
+      {grammarGameModalShown && (
+        <GrammarGameModal onHide={() => setGrammarGameModalShown(false)} />
       )}
     </div>
   );
