f1038fe43e127f5030e52408b4cfdc6dc0dcb95d
mikey1384 <mikey1384@gmail.com>
2022-10-07 18:46:40 +0700

    more updates

--- src/containers/Home/Stories/InputPanel/InputModal/index.jsx
+++ src/containers/Home/Stories/InputPanel/InputModal/index.jsx
@@ -1,12 +1,31 @@
+import PropTypes from 'prop-types';
 import ContentInput from './ContentInput';
 import SubjectInput from './SubjectInput';
 import ErrorBoundary from '~/components/ErrorBoundary';
+import Modal from '~/components/Modal';
+import Button from '~/components/Button';
+import { useKeyContext } from '~/contexts';
 
-export default function InputPanel() {
+InputModal.propTypes = {
+  onHide: PropTypes.func.isRequired
+};
+
+export default function InputModal({ onHide }) {
+  const {
+    done: { color: doneColor }
+  } = useKeyContext((v) => v.theme);
+
   return (
-    <ErrorBoundary componentPath="Home/Stories/InputPanel/index">
-      <SubjectInput />
-      <ContentInput />
+    <ErrorBoundary componentPath="Home/Stories/InputPanel/InputModal">
+      <Modal onHide={onHide}>
+        <SubjectInput />
+        <ContentInput />
+        <footer>
+          <Button color={doneColor} onClick={onHide}>
+            OK
+          </Button>
+        </footer>
+      </Modal>
     </ErrorBoundary>
   );
 }
--- src/containers/Home/Stories/InputPanel/index.jsx
+++ src/containers/Home/Stories/InputPanel/index.jsx
@@ -1,3 +1,41 @@
+import { useState } from 'react';
+import ErrorBoundary from '~/components/ErrorBoundary';
+import { css } from '@emotion/css';
+import { Color } from '~/constants/css';
+import InputModal from './InputModal';
+
 export default function InputPanel() {
-  return <div>This is input Panel</div>;
+  const [inputValue, setInputValue] = useState('');
+  const [inputModalShown, setInputModalShown] = useState(false);
+  return (
+    <ErrorBoundary componentPath="Home/Stories/InputPanel">
+      <div>
+        <input
+          style={{ width: '100%', marginTop: '1rem', marginBottom: '1rem' }}
+          placeholder="Post Something..."
+          value={inputValue}
+          onChange={() => setInputValue('')}
+          onClick={() => setInputModalShown(true)}
+          className={css`
+            line-height: 2rem;
+            padding: 1rem;
+            border: 1px solid ${Color.darkerBorderGray()};
+            font-size: 1.7rem;
+            &:focus {
+              outline: none;
+              ::placeholder {
+                color: ${Color.lighterGray()};
+              }
+            }
+            ::placeholder {
+              color: ${Color.gray()};
+            }
+          `}
+        />
+        {inputModalShown && (
+          <InputModal onHide={() => setInputModalShown(false)} />
+        )}
+      </div>
+    </ErrorBoundary>
+  );
 }
