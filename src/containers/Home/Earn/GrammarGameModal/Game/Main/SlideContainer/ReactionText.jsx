e0c28a67f920a9a456493ad520b4221d73e8ffad
mikey1384 <mikey1384@gmail.com>
2022-10-09 17:42:51 +0700

    more updates

--- src/containers/Home/Earn/GrammarGameModal/Game/Main/SlideContainer/ReactionText.jsx
+++ src/containers/Home/Earn/GrammarGameModal/Game/Main/SlideContainer/ReactionText.jsx
@@ -1,5 +1,6 @@
 import { useMemo } from 'react';
 import { Color } from '~/constants/css';
+import { useSpring, animated } from 'react-spring';
 
 export default function ReactionText() {
   const reactionFontSize = useMemo(() => {
@@ -9,6 +10,16 @@ export default function ReactionText() {
   const reactionColor = useMemo(() => {
     return Color.gold();
   }, []);
+
+  const animationOutput = useMemo(() => {
+    return [1, 0.8, 0.5, 1.1, 0.5, 1.1, 1.03, 1];
+  }, []);
+
+  const { x } = useSpring({
+    from: { x: 0 },
+    x: 1,
+    config: { duration: 1000 }
+  });
 
   return (
     <div
@@ -23,15 +34,21 @@ export default function ReactionText() {
         textAlign: 'center'
       }}
     >
-      <div
+      <animated.div
         style={{
           marginBottom: '5rem',
           fontSize: reactionFontSize,
-          color: reactionColor
+          color: reactionColor,
+          fontWeight: 'bold',
+          opacity: x.to({ range: [0, 1], output: [0.3, 1] }),
+          scale: x.to({
+            range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+            output: animationOutput
+          })
         }}
       >
         AWESOME
-      </div>
+      </animated.div>
     </div>
   );
 }
