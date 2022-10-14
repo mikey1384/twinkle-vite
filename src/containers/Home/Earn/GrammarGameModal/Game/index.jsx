660f48f3f5f0fda4dd25661ec41453386c644201
mikey1384 <mikey1384@gmail.com>
2022-10-14 17:02:52 +0700

    more minor changes

--- src/containers/Home/Earn/GrammarGameModal/Game/index.jsx
+++ src/containers/Home/Earn/GrammarGameModal/Game/index.jsx
@@ -6,7 +6,7 @@ Game.propTypes = {
   isOnStreak: PropTypes.bool,
   questionIds: PropTypes.array,
   questionObj: PropTypes.object,
-  onSetGameState: PropTypes.func.isRequired,
+  onGameFinish: PropTypes.func.isRequired,
   onSetQuestionObj: PropTypes.func.isRequired
 };
 
@@ -14,17 +14,17 @@ export default function Game({
   isOnStreak,
   questionIds,
   questionObj,
-  onSetGameState,
+  onGameFinish,
   onSetQuestionObj
 }) {
   return (
     <div style={{ width: '100%', paddingTop: '3.5rem' }}>
-      {onSetGameState.length > 0 ? (
+      {questionIds.length > 0 ? (
         <Main
           questionIds={questionIds}
           questionObj={questionObj}
           isOnStreak={isOnStreak}
-          onSetGameState={onSetGameState}
+          onGameFinish={onGameFinish}
           onSetQuestionObj={onSetQuestionObj}
         />
       ) : (
