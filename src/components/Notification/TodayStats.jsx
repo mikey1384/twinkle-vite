958aab0a13d33d68a0080d0a2f9645846b205d0d
mikey1384 <mikey1384@gmail.com>
2022-10-15 10:11:29 +0700

    more updates

--- src/components/Notification/TodayStats.jsx
+++ src/components/Notification/TodayStats.jsx
@@ -1,9 +1,23 @@
 import ErrorBoundary from '~/components/ErrorBoundary';
+import { css } from '@emotion/css';
+import { Color, borderRadius } from '~/constants/css';
 
 export default function TodayStats() {
   return (
     <ErrorBoundary componentPath="Notification/TodayStats">
-      <div>today stats</div>
+      <div
+        style={{ marginBottom: '1rem' }}
+        className={css`
+          padding: 1.5rem 0;
+          font-size: 2rem;
+          text-align: center;
+          border-radius: ${borderRadius};
+          border: 1px solid ${Color.borderGray()};
+          background: #fff;
+        `}
+      >
+        today stats
+      </div>
     </ErrorBoundary>
   );
 }
