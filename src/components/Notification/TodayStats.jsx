94b3684f40a9592a2cfb05dfcc735f675c9a3b53
mikey1384 <mikey1384@gmail.com>
2022-10-15 11:01:22 +0700

    more updates

--- src/components/Notification/TodayStats.jsx
+++ src/components/Notification/TodayStats.jsx
@@ -1,22 +1,38 @@
 import ErrorBoundary from '~/components/ErrorBoundary';
 import { css } from '@emotion/css';
 import { Color, borderRadius } from '~/constants/css';
+import { useNotiContext } from '~/contexts';
+import Loading from '~/components/Loading';
 
 export default function TodayStats() {
+  const todayStats = useNotiContext((v) => v.state.todayStats);
+
   return (
     <ErrorBoundary componentPath="Notification/TodayStats">
       <div
         style={{ marginBottom: '1rem' }}
         className={css`
           padding: 1.5rem 0;
-          font-size: 2rem;
           text-align: center;
           border-radius: ${borderRadius};
           border: 1px solid ${Color.borderGray()};
           background: #fff;
         `}
       >
-        today stats
+        {todayStats?.loaded ? (
+          <div>
+            <b style={{ fontSize: '1.7rem' }}>{`Today's Progress`}</b>
+            <div style={{ marginTop: '1rem' }}>
+              <p>
+                <b>XP</b> Earned: {todayStats.xpEarned}
+              </p>
+              <p>Karma Points Earned: {todayStats.kpEarned}</p>
+              <p>Coins Earned: {todayStats.coinsEarned}</p>
+            </div>
+          </div>
+        ) : (
+          <Loading />
+        )}
       </div>
     </ErrorBoundary>
   );
