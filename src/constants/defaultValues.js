072899c90752c2298a2787fcf53619b9515eaa62
mikey1384 <mikey1384@gmail.com>
2022-10-20 08:37:36 +0700

    more exploit fix

--- src/constants/defaultValues.js
+++ src/constants/defaultValues.js
@@ -1,6 +1,6 @@
 import localize from '~/constants/localize';
 
-export const clientVersion = '1.6.83';
+export const clientVersion = '1.6.84';
 export const defaultChatSubject = 'Welcome!';
 export const defaultContentState = {
   isEditing: false,
