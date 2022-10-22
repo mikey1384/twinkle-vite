98d6313ca8d3b42a9f45e28c05dd3e7f84c684e5
mikey1384 <mikey1384@gmail.com>
2022-10-22 18:33:36 +0700

    more fix

--- src/containers/Chat/Modals/WordleModal/index.jsx
+++ src/containers/Chat/Modals/WordleModal/index.jsx
@@ -8,6 +8,7 @@ import Countdown from 'react-countdown';
 import FilterBar from '~/components/FilterBar';
 import Streaks from './Streaks';
 import Rankings from './Rankings';
+import ErrorBoundary from '~/components/ErrorBoundary';
 import { css } from '@emotion/css';
 import { MAX_GUESSES } from './constants/settings';
 import { useAppContext, useChatContext, useKeyContext } from '~/contexts';
@@ -85,139 +86,149 @@ export default function WordleModal({
 
   return (
     <Modal closeWhenClickedOutside={false} onHide={onHide}>
-      <header style={{ padding: 0 }}>
-        <FilterBar
+      <ErrorBoundary componentPath="Chat/Modals/WordleModal">
+        <header style={{ padding: 0 }}>
+          <FilterBar
+            style={{
+              marginTop: '3rem',
+              height: '5rem'
+            }}
+          >
+            <nav
+              className={activeTab === 'game' ? 'active' : null}
+              onClick={() => setActiveTab('game')}
+            >
+              Wordle
+            </nav>
+            <nav
+              className={activeTab === 'rankings' ? 'active' : null}
+              onClick={() => setActiveTab('rankings')}
+            >
+              Top Scorers
+            </nav>
+            <nav
+              className={activeTab === 'streaks' ? 'active' : null}
+              onClick={() => setActiveTab('streaks')}
+            >
+              Top Streaks
+            </nav>
+          </FilterBar>
+        </header>
+        <main
           style={{
-            marginTop: '3rem',
-            height: '5rem'
+            padding: 0,
+            marginTop: 0
           }}
         >
-          <nav
-            className={activeTab === 'game' ? 'active' : null}
-            onClick={() => setActiveTab('game')}
-          >
-            Wordle
-          </nav>
-          <nav
-            className={activeTab === 'rankings' ? 'active' : null}
-            onClick={() => setActiveTab('rankings')}
-          >
-            Top Scorers
-          </nav>
-          <nav
-            className={activeTab === 'streaks' ? 'active' : null}
-            onClick={() => setActiveTab('streaks')}
-          >
-            Top Streaks
-          </nav>
-        </FilterBar>
-      </header>
-      <main
-        style={{
-          padding: 0,
-          marginTop: 0
-        }}
-      >
-        {activeTab === 'game' ? (
-          <Game
-            isRevealing={isRevealing}
-            onSetIsRevealing={setIsRevealing}
-            channelName={channelName}
-            channelId={channelId}
-            guesses={guesses}
-            isGameOver={isGameOver}
-            isGameWon={isGameWon}
-            isGameLost={isGameLost}
-            isStrictMode={!!isStrictMode}
-            nextDayTimeStamp={nextDayTimeStamp}
-            solution={solution}
-            onSetOverviewModalShown={setOverviewModalShown}
-            socketConnected={socketConnected}
-          />
-        ) : activeTab === 'rankings' ? (
+          {activeTab === 'game' ? (
+            <Game
+              isRevealing={isRevealing}
+              onSetIsRevealing={setIsRevealing}
+              channelName={channelName}
+              channelId={channelId}
+              guesses={guesses}
+              isGameOver={isGameOver}
+              isGameWon={isGameWon}
+              isGameLost={isGameLost}
+              isStrictMode={!!isStrictMode}
+              nextDayTimeStamp={nextDayTimeStamp}
+              solution={solution}
+              onSetOverviewModalShown={setOverviewModalShown}
+              socketConnected={socketConnected}
+            />
+          ) : activeTab === 'rankings' ? (
+            <div
+              style={{
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'center'
+              }}
+            >
+              <Rankings
+                channelId={channelId}
+                onSetRankingsTab={setRankingsTab}
+                rankingsTab={rankingsTab}
+              />
+            </div>
+          ) : (
+            <Streaks
+              channelId={channelId}
+              streaksTab={streaksTab}
+              onSetStreaksTab={setStreaksTab}
+              theme={theme}
+            />
+          )}
+          {overviewModalShown && (
+            <OverviewModal
+              numGuesses={guesses.length}
+              solution={solution}
+              wordLevel={wordLevel}
+              wordleStats={wordleStats}
+              isGameOver={isGameOver}
+              isSolved={isGameWon}
+              attemptState={attemptState}
+              onHide={() => setOverviewModalShown(false)}
+            />
+          )}
+        </main>
+        <footer>
           <div
             style={{
               width: '100%',
-              display: 'flex',
-              justifyContent: 'center'
+              display: 'grid',
+              gridTemplateColumns: '1fr 1fr 1fr'
             }}
           >
-            <Rankings
-              channelId={channelId}
-              onSetRankingsTab={setRankingsTab}
-              rankingsTab={rankingsTab}
-            />
-          </div>
-        ) : (
-          <Streaks
-            channelId={channelId}
-            streaksTab={streaksTab}
-            onSetStreaksTab={setStreaksTab}
-            theme={theme}
-          />
-        )}
-        {overviewModalShown && (
-          <OverviewModal
-            numGuesses={guesses.length}
-            solution={solution}
-            wordLevel={wordLevel}
-            wordleStats={wordleStats}
-            isGameOver={isGameOver}
-            isSolved={isGameWon}
-            attemptState={attemptState}
-            onHide={() => setOverviewModalShown(false)}
-          />
-        )}
-      </main>
-      <footer>
-        <div
-          style={{
-            width: '100%',
-            display: 'grid',
-            gridTemplateColumns: '1fr 1fr 1fr'
-          }}
-        >
-          <div
-            style={{ width: '100%', display: 'flex', justifyContent: 'center' }}
-          >
-            <Button
-              color={doneColor}
-              onClick={() => setOverviewModalShown(true)}
-              isGameWon={isGameWon}
+            <div
+              style={{
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'center'
+              }}
             >
-              Show {isGameOver ? 'Overview' : 'Stats'}
-            </Button>
+              <Button
+                color={doneColor}
+                onClick={() => setOverviewModalShown(true)}
+                isGameWon={isGameWon}
+              >
+                Show {isGameOver ? 'Overview' : 'Stats'}
+              </Button>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column'
+              }}
+            >
+              <p style={{ fontWeight: 'bold', fontSize: '1.5rem' }}>
+                Next Wordle
+              </p>
+              <Countdown
+                key={nextDayTimeStamp}
+                className={css`
+                  font-size: 1.3rem;
+                `}
+                date={nextDayTimeStamp}
+                daysInHours={true}
+                onComplete={handleCountdownComplete}
+              />
+            </div>
+            <div
+              style={{
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'center'
+              }}
+            >
+              <Button transparent onClick={onHide}>
+                Close
+              </Button>
+            </div>
           </div>
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              flexDirection: 'column'
-            }}
-          >
-            <p style={{ fontWeight: 'bold', fontSize: '1.5rem' }}>
-              Next Wordle
-            </p>
-            <Countdown
-              key={nextDayTimeStamp}
-              className={css`
-                font-size: 1.3rem;
-              `}
-              date={nextDayTimeStamp}
-              daysInHours={true}
-              onComplete={handleCountdownComplete}
-            />
-          </div>
-          <div
-            style={{ width: '100%', display: 'flex', justifyContent: 'center' }}
-          >
-            <Button transparent onClick={onHide}>
-              Close
-            </Button>
-          </div>
-        </div>
-      </footer>
+        </footer>
+      </ErrorBoundary>
     </Modal>
   );
 
