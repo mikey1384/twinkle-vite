12b5b6067da3cb4e5aeecf727a8f0c75d0f21a34
mikey1384 <mikey1384@gmail.com>
2022-10-23 06:56:35 +0700

    more updates

--- src/containers/MissionPage/Management/Main/QuestionEditor.jsx
+++ src/containers/MissionPage/Management/Main/QuestionEditor.jsx
@@ -1,22 +1,48 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import { useAppContext } from '~/contexts';
+import { Color } from '~/constants/css';
+import ErrorBoundary from '~/components/ErrorBoundary';
 
-export default function QuestionEditor() {
+QuestionEditor.propTypes = {
+  missionId: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+};
+
+export default function QuestionEditor({ missionId }) {
+  const [questions, setQuestions] = useState([]);
   const loadGoogleMissionQuestions = useAppContext(
     (v) => v.requestHelpers.loadGoogleMissionQuestions
   );
   useEffect(() => {
     init();
     async function init() {
-      const data = await loadGoogleMissionQuestions();
-      console.log(data);
+      const { questionObj, questionIds } = await loadGoogleMissionQuestions({
+        missionId
+      });
+      setQuestions(questionIds.map((id) => questionObj[id]));
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [missionId]);
 
   return (
-    <div>
-      <div>question editor</div>
-    </div>
+    <ErrorBoundary componentPath="MissionPage/Main/QuestionEditor">
+      <div>
+        {questions.map((question, index) => (
+          <div
+            style={{
+              background: '#fff',
+              marginTop: index === 0 ? '0' : '1rem',
+              padding: '2rem',
+              fontSize: '1.7rem',
+              textAlign: 'center',
+              border: `1px solid ${Color.borderGray()}`
+            }}
+            key={question.id}
+          >
+            {question.content}
+          </div>
+        ))}
+      </div>
+    </ErrorBoundary>
   );
 }
