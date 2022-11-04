23e3d887820399eb1e0838dbd7a42a1d21146a91
mikey1384 <mikey1384@gmail.com>
2022-11-04 19:38:05 +0700

    more updates

--- src/containers/Chat/Modals/ChessModal/Rewind.jsx
+++ src/containers/Chat/Modals/ChessModal/Rewind.jsx
@@ -1,5 +1,7 @@
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Chess from '../../Chess';
+import { useAppContext } from '~/contexts';
 
 Rewind.propTypes = {
   channelId: PropTypes.number.isRequired,
@@ -8,10 +10,27 @@ Rewind.propTypes = {
 };
 
 export default function Rewind({ channelId, initialState, myId }) {
+  const fetchCurrentRewindRequest = useAppContext(
+    (v) => v.requestHelpers.fetchCurrentRewindRequest
+  );
+  const [rewindRequestMessage, setRewindRequestMessage] = useState();
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    init();
+    async function init() {
+      const data = await fetchCurrentRewindRequest(channelId);
+      console.log(data);
+      setRewindRequestMessage({});
+      setLoaded(true);
+      console.log(rewindRequestMessage);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>
       <Chess
-        loaded
+        loaded={loaded}
         myId={myId}
         channelId={channelId}
         initialState={initialState}
