add8d19d2446f16bcc7c257ebdc8182695e36ce7
mikey1384 <mikey1384@gmail.com>
2022-11-04 19:17:21 +0700

    more fix

--- src/containers/Chat/Modals/ChessModal/Rewind.jsx
+++ src/containers/Chat/Modals/ChessModal/Rewind.jsx
@@ -1,7 +1,22 @@
-export default function Rewind() {
+import PropTypes from 'prop-types';
+import Chess from '../../Chess';
+
+Rewind.propTypes = {
+  channelId: PropTypes.number.isRequired,
+  initialState: PropTypes.object.isRequired,
+  myId: PropTypes.number.isRequired
+};
+
+export default function Rewind({ channelId, initialState, myId }) {
   return (
     <div>
-      <h1>Rewind</h1>
+      <Chess
+        loaded
+        myId={myId}
+        channelId={channelId}
+        initialState={initialState}
+        style={{ width: '100%' }}
+      />
     </div>
   );
 }
