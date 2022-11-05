a638cfda26fa9af56f105fd4c8a75f32814a5417
mikey1384 <mikey1384@gmail.com>
2022-11-05 19:20:10 +0700

    more updates

--- src/containers/MissionPage/Management/GrammarManager/index.jsx
+++ src/containers/MissionPage/Management/GrammarManager/index.jsx
@@ -1,6 +1,9 @@
 import PropTypes from 'prop-types';
 import ErrorBoundary from '~/components/ErrorBoundary';
 import GrammarQuestionGenerator from './GrammarQuestionGenerator';
+import SideMenu from '~/components/SideMenu';
+import Icon from '~/components/Icon';
+import { NavLink } from 'react-router-dom';
 
 GrammarManager.propTypes = {
   mission: PropTypes.object.isRequired,
@@ -10,9 +13,32 @@ GrammarManager.propTypes = {
 export default function GrammarManager({ mission, onSetMissionState }) {
   return (
     <ErrorBoundary componentPath="MissionPage/Management/GrammarManager">
-      <div style={{ width: '100%', display: 'flex' }}>
+      <div
+        style={{
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'flex-end',
+          position: 'relative'
+        }}
+      >
+        <SideMenu style={{ left: 0 }}>
+          <NavLink
+            to="/subjects"
+            className={(navData) => (navData.isActive ? 'active' : '')}
+          >
+            <Icon icon="bolt" />
+            <span style={{ marginLeft: '1.1rem' }}>Questions</span>
+          </NavLink>
+          <NavLink
+            to="/videos"
+            className={(navData) => (navData.isActive ? 'active' : '')}
+          >
+            <Icon icon="film" />
+            <span style={{ marginLeft: '1.1rem' }}>Categories</span>
+          </NavLink>
+        </SideMenu>
         <GrammarQuestionGenerator
-          style={{ width: '100%' }}
+          style={{ width: 'CALC(100% - 20rem)' }}
           mission={mission}
           onSetMissionState={onSetMissionState}
         />
