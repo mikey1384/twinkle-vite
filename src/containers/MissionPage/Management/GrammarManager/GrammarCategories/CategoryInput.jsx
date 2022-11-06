6f407e46a7153658e197160a6452d4b8771110c5
mikey1384 <mikey1384@gmail.com>
2022-11-06 19:19:15 +0700

    more updates

--- src/containers/MissionPage/Management/GrammarManager/GrammarCategories/CategoryInput.jsx
+++ src/containers/MissionPage/Management/GrammarManager/GrammarCategories/CategoryInput.jsx
@@ -1,5 +1,8 @@
 import PropTypes from 'prop-types';
 import Input from '~/components/Texts/Input';
+import Button from '~/components/Button';
+import Icon from '~/components/Icon';
+import { stringIsEmpty } from '~/helpers/stringHelpers';
 
 CategoryInput.propTypes = {
   categoryText: PropTypes.string,
@@ -8,11 +11,18 @@ CategoryInput.propTypes = {
 
 export default function CategoryInput({ onChange, categoryText }) {
   return (
-    <Input
-      onChange={onChange}
-      placeholder="Enter category..."
-      value={categoryText}
-      style={{ width: '100%' }}
-    />
+    <div style={{ display: 'flex' }}>
+      <Input
+        onChange={onChange}
+        placeholder="Enter category..."
+        value={categoryText}
+        style={{ width: '100%' }}
+      />
+      {!stringIsEmpty(categoryText) && (
+        <Button color="blue" filled style={{ marginLeft: '1rem' }}>
+          <Icon icon="plus" />
+        </Button>
+      )}
+    </div>
   );
 }
