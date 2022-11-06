d7e247f05a2fcf8fb0b713b295ac28a158dca21d
mikey1384 <mikey1384@gmail.com>
2022-11-06 10:53:52 +0700

    more fix

--- src/containers/MissionPage/Management/GrammarManager/GrammarCategories/index.jsx
+++ src/containers/MissionPage/Management/GrammarManager/GrammarCategories/index.jsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import ErrorBoundary from '~/components/ErrorBoundary';
 import { css } from '@emotion/css';
 import { borderRadius, Color, mobileMaxWidth } from '~/constants/css';
+import Input from '~/components/Texts/Input';
 import { useKeyContext } from '~/contexts';
 
 GrammarCategories.propTypes = {
@@ -11,6 +13,7 @@ export default function GrammarCategories({ style }) {
   const {
     link: { color: linkColor }
   } = useKeyContext((v) => v.theme);
+  const [categoryText, setCategoryText] = useState('');
   return (
     <ErrorBoundary componentPath="MissionPage/Management/GrammarManager/GrammarCategories">
       <div style={style}>
@@ -18,7 +21,7 @@ export default function GrammarCategories({ style }) {
           className={css`
             width: 100%;
             display: flex;
-            font-size: 2rem;
+            font-size: 1.5rem;
             flex-direction: column;
             background: #fff;
             padding: 1rem 1rem 1.5rem 1rem;
@@ -31,17 +34,39 @@ export default function GrammarCategories({ style }) {
             }
           `}
         >
-          <p
+          <b>Add a category</b>
+          <div
             className={css`
-              color: ${Color[linkColor]()};
-              cursor: pointer;
-              &:hover {
-                text-decoration: underline;
+              margin-top: 0.5rem;
+              width: 50%;
+              @media (max-width: ${mobileMaxWidth}) {
+                width: 100%;
               }
             `}
           >
-            Uncategorized
-          </p>
+            <Input
+              onChange={(text) => setCategoryText(text)}
+              placeholder="Enter category..."
+              value={categoryText}
+              style={{ width: '100%' }}
+            />
+          </div>
+          <div style={{ marginTop: '2rem' }}>
+            <ul>
+              <li
+                className={css`
+                  font-size: 1.7rem;
+                  color: ${Color[linkColor]()};
+                  cursor: pointer;
+                  &:hover {
+                    text-decoration: underline;
+                  }
+                `}
+              >
+                Uncategorized
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </ErrorBoundary>
