e916225121ba4ed204af2709ab8909ce0535cfcf
mikey1384 <mikey1384@gmail.com>
2022-11-07 10:58:19 +0700

    minor fix

--- src/containers/MissionPage/Management/GrammarManager/GrammarCategories/CategoryModal.jsx
+++ src/containers/MissionPage/Management/GrammarManager/GrammarCategories/CategoryModal.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import Modal from '~/components/Modal';
 import Button from '~/components/Button';
 import Icon from '~/components/Icon';
+import Input from '~/components/Texts/Input';
+import { stringIsEmpty } from '~/helpers/stringHelpers';
 import { Color } from '~/constants/css';
 import { useAppContext } from '~/contexts';
 import { css } from '@emotion/css';
@@ -16,6 +18,8 @@ export default function CategoryModal({ category, onHide }) {
   const loadGrammarCategoryQuestions = useAppContext(
     (v) => v.requestHelpers.loadGrammarCategoryQuestions
   );
+  const [isEditing, setIsEditing] = useState(false);
+  const [editiedCategory, setEditedCategory] = useState(category);
   const [questions, setQuestions] = useState([]);
   useEffect(() => {
     init();
@@ -29,11 +33,30 @@ export default function CategoryModal({ category, onHide }) {
   return (
     <Modal onHide={onHide}>
       <header>
-        <div style={{ display: 'flex' }}>
-          <div>
-            <span style={{ textTransform: 'capitalize' }}>{category}</span>
+        <div style={{ display: 'flex', width: '100%' }}>
+          <div style={{ width: isEditing ? '60%' : 'auto' }}>
+            {isEditing ? (
+              <Input
+                style={{ width: '100%' }}
+                maxLength={100}
+                placeholder="Enter a new label..."
+                autoFocus
+                onChange={(text) => setEditedCategory(text)}
+                onKeyPress={(event) => {
+                  if (
+                    !stringIsEmpty(editiedCategory) &&
+                    event.key === 'Enter'
+                  ) {
+                    handleChangeLabel(editiedCategory);
+                  }
+                }}
+                value={editiedCategory}
+              />
+            ) : (
+              <span style={{ textTransform: 'capitalize' }}>{category}</span>
+            )}
           </div>
-          {category !== 'uncategorized' && (
+          {category !== 'uncategorized' && !isEditing && (
             <div
               className={`unselectable ${css`
                 margin-left: 3rem;
@@ -48,7 +71,7 @@ export default function CategoryModal({ category, onHide }) {
                 }
               `}`}
             >
-              <small>
+              <small onClick={() => setIsEditing(true)}>
                 <Icon icon="pencil-alt" /> Edit
               </small>
               <small style={{ marginLeft: '2rem' }}>
@@ -70,4 +93,8 @@ export default function CategoryModal({ category, onHide }) {
       </footer>
     </Modal>
   );
+
+  async function handleChangeLabel(text) {
+    console.log(text);
+  }
 }
