b98df8c167fb30ed4f27ca1a5f854b21da3f9d16
mikey1384 <mikey1384@gmail.com>
2022-11-07 10:41:38 +0700

    more updates

--- src/containers/MissionPage/Management/GrammarManager/GrammarCategories/CategoryModal.jsx
+++ src/containers/MissionPage/Management/GrammarManager/GrammarCategories/CategoryModal.jsx
@@ -2,7 +2,10 @@ import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Modal from '~/components/Modal';
 import Button from '~/components/Button';
+import Icon from '~/components/Icon';
+import { Color } from '~/constants/css';
 import { useAppContext } from '~/contexts';
+import { css } from '@emotion/css';
 
 CategoryModal.propTypes = {
   category: PropTypes.string,
@@ -26,7 +29,32 @@ export default function CategoryModal({ category, onHide }) {
   return (
     <Modal onHide={onHide}>
       <header>
-        <span style={{ textTransform: 'capitalize' }}>{category}</span>
+        <div style={{ display: 'flex' }}>
+          <div>
+            <span style={{ textTransform: 'capitalize' }}>{category}</span>
+          </div>
+          <div
+            className={`unselectable ${css`
+              margin-left: 3rem;
+              > small {
+                cursor: pointer;
+                color: ${Color.gray()};
+                font-weight: normal;
+                font-size: 1.5rem;
+                &:hover {
+                  text-decoration: underline;
+                }
+              }
+            `}`}
+          >
+            <small>
+              <Icon icon="pencil-alt" /> Edit
+            </small>
+            <small style={{ marginLeft: '2rem' }}>
+              <Icon icon="trash-alt" /> Delete
+            </small>
+          </div>
+        </div>
       </header>
       <main>
         {questions.map((question) => (
