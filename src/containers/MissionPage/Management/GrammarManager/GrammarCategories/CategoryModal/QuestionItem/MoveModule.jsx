8908d6a8bd206a539b453223885f6abe76d088f6
mikey1384 <mikey1384@gmail.com>
2022-11-08 19:26:27 +0700

    more updates

--- src/containers/MissionPage/Management/GrammarManager/GrammarCategories/CategoryModal/QuestionItem/MoveModule.jsx
+++ src/containers/MissionPage/Management/GrammarManager/GrammarCategories/CategoryModal/QuestionItem/MoveModule.jsx
@@ -1,4 +1,6 @@
 import PropTypes from 'prop-types';
+import { Color } from '~/constants/css';
+import { css } from '@emotion/css';
 
 MoveModule.propTypes = {
   categories: PropTypes.array.isRequired
@@ -6,9 +8,30 @@ MoveModule.propTypes = {
 
 export default function MoveModule({ categories }) {
   return (
-    <div>
+    <div
+      style={{
+        width: '100%',
+        textAlign: 'center',
+        padding: '1rem',
+        border: `1px solid ${Color.borderGray()}`
+      }}
+    >
       {categories.map((category, index) => (
-        <div key={index}>{category}</div>
+        <div key={index}>
+          <span
+            className={css`
+              line-height: 2;
+              width: auto;
+              cursor: pointer;
+              color: ${Color.blue()};
+              &:hover {
+                text-decoration: underline;
+              }
+            `}
+          >
+            {category}
+          </span>
+        </div>
       ))}
     </div>
   );
