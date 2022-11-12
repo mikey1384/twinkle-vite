27135b8448ffbfe1f8bb322eea5bfb2e9d8e57e7
mikey1384 <mikey1384@gmail.com>
2022-11-12 10:28:01 +0700

    more fix

--- src/containers/Chat/Body/Collect/index.jsx
+++ src/containers/Chat/Body/Collect/index.jsx
@@ -1,5 +1,11 @@
+import PropTypes from 'prop-types';
 import Vocabulary from './Vocabulary';
+import { VOCAB_CHAT_TYPE } from '~/constants/defaultValues';
 
-export default function Collect() {
-  return <Vocabulary />;
+Collect.propTypes = {
+  chatType: PropTypes.string
+};
+
+export default function Collect({ chatType }) {
+  return chatType === VOCAB_CHAT_TYPE ? <Vocabulary /> : <div>not vocab</div>;
 }
