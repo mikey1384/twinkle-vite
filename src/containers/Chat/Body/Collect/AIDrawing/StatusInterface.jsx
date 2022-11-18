d99a893716c99043d3afc2738350cf81dcfe192e
mikey1384 <mikey1384@gmail.com>
2022-11-18 09:44:15 +0700

    more updates

--- src/containers/Chat/Body/Collect/AIDrawing/StatusInterface.jsx
+++ src/containers/Chat/Body/Collect/AIDrawing/StatusInterface.jsx
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
 import { Color } from '~/constants/css';
 import { stringIsEmpty } from '~/helpers/stringHelpers';
+import { css } from '@emotion/css';
 
 StatusInterface.propTypes = {
+  posting: PropTypes.bool,
   statusMessage: PropTypes.string
 };
 
-export default function StatusInterface({ statusMessage }) {
+export default function StatusInterface({ posting, statusMessage }) {
   return (
     <div
       style={{
@@ -18,6 +20,21 @@ export default function StatusInterface({ statusMessage }) {
     >
       {!stringIsEmpty(statusMessage) && (
         <div
+          className={css`
+            p {
+              font-family: monospace;
+              display: inline-block;
+              &.posting {
+                clip-path: inset(0 3ch 0 0);
+                animation: dotdotdot 1s steps(4) infinite;
+              }
+            }
+            @keyframes dotdotdot {
+              to {
+                clip-path: inset(0 -1ch 0 0);
+              }
+            }
+          `}
           style={{
             textAlign: 'center',
             width: '100%',
@@ -26,7 +43,7 @@ export default function StatusInterface({ statusMessage }) {
             padding: '1rem'
           }}
         >
-          {statusMessage}
+          <p className={posting ? 'posting' : ''}>{statusMessage}</p>
         </div>
       )}
     </div>
