d1c6aa80c4b50b5faedf036a011c6707d99a91b0
mikey1384 <mikey1384@gmail.com>
2022-11-21 19:42:48 +0700

    more updates

--- src/containers/Chat/Body/Collect/AIDrawing/ActivitiesContainer/Activity/CardInfo.jsx
+++ src/containers/Chat/Body/Collect/AIDrawing/ActivitiesContainer/Activity/CardInfo.jsx
@@ -3,16 +3,38 @@ import { Color } from '~/constants/css';
 
 CardInfo.propTypes = {
   style: PropTypes.object,
-  quality: PropTypes.string,
-  cardObj: PropTypes.object.isRequired
+  quality: PropTypes.string
 };
 
-export default function CardInfo({ cardObj, quality, style }) {
+const qualityProps = {
+  common: {
+    color: Color.vantaBlack(),
+    fontWeight: 'normal'
+  },
+  superior: {
+    color: Color.darkBlue(),
+    fontWeight: 'bold'
+  },
+  rare: {
+    color: Color.purple(),
+    fontWeight: 'bold'
+  },
+  elite: {
+    color: Color.brownOrange(),
+    fontWeight: 'bold'
+  },
+  legendary: {
+    color: Color.darkGold(),
+    fontWeight: 'bold'
+  }
+};
+
+export default function CardInfo({ quality, style }) {
   return (
     <div style={style}>
       <div>
-        created {quality === 'elite' ? 'an' : 'a'} {quality}{' '}
-        <b style={{ color: Color[cardObj?.color]() }}>{cardObj?.label}</b> card
+        created {quality === 'elite' ? 'an' : 'a'}{' '}
+        <span style={{ ...qualityProps[quality] }}>{quality}</span> card
       </div>
     </div>
   );
