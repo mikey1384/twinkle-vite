9a4397441010b13b909506c03ca11424218c9314
mikey1384 <mikey1384@gmail.com>
2022-11-26 21:28:29 +0700

    more updates

--- src/containers/Chat/RightMenu/AICardInfo/Collected.jsx
+++ src/containers/Chat/RightMenu/AICardInfo/Collected.jsx
@@ -1,7 +1,15 @@
 export default function Collected() {
   return (
-    <div>
-      <div>here</div>
+    <div style={{ width: '100%' }}>
+      <div
+        style={{
+          textAlign: 'center',
+          padding: '1.5rem 0',
+          fontSize: '1.7rem'
+        }}
+      >
+        <b>Your Collections</b>
+      </div>
     </div>
   );
 }
