f227d00eafa3f8f6be0a0956187be9c690dc6d99
mikey1384 <mikey1384@gmail.com>
2022-11-29 16:45:28 +0700

    more updates

--- src/containers/Chat/RightMenu/AICardInfo/Collected.jsx
+++ src/containers/Chat/RightMenu/AICardInfo/Collected.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { Color } from '~/constants/css';
 import { useAppContext, useChatContext } from '~/contexts';
 
 export default function Collected() {
@@ -18,15 +19,26 @@ export default function Collected() {
   }, []);
 
   return (
-    <div style={{ width: '100%' }}>
+    <div style={{ width: '100%', height: '50%' }}>
       <div
         style={{
-          textAlign: 'center',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
           padding: '1.5rem 0',
-          fontSize: '1.7rem'
+          fontSize: '1.7rem',
+          height: '5rem',
+          borderBottom: `1px solid ${Color.borderGray()}`
         }}
       >
         <b>My Collections</b>
+      </div>
+      <div
+        style={{
+          height: 'CALC(100% - 5rem)',
+          overflow: 'scroll'
+        }}
+      >
         {myCards.map((card) => (
           <div key={card.id}>{card.imagePath}</div>
         ))}
