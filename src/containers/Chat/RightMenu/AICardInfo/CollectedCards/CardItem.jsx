69042e79412bcb176b3a7dddacf076ba017634e2
mikey1384 <mikey1384@gmail.com>
2022-11-29 17:11:36 +0700

    more updates

--- src/containers/Chat/RightMenu/AICardInfo/CollectedCards/CardItem.jsx
+++ src/containers/Chat/RightMenu/AICardInfo/CollectedCards/CardItem.jsx
@@ -1,14 +1,33 @@
+import { useMemo } from 'react';
 import PropTypes from 'prop-types';
-import { cloudFrontURL } from '~/constants/defaultValues';
+import { Color } from '~/constants/css';
+import { cardLevelHash, cloudFrontURL } from '~/constants/defaultValues';
 
 CardItem.propTypes = {
   card: PropTypes.object.isRequired
 };
 
 export default function CardItem({ card }) {
+  const cardObj = useMemo(() => cardLevelHash[card?.level], [card?.level]);
+  const cardColor = useMemo(() => Color[cardObj?.color](), [cardObj?.color]);
   return (
-    <div key={card.id}>
-      <img src={`${cloudFrontURL}${card.imagePath}`} />
+    <div style={{ height: '10rem', display: 'flex' }} key={card.id}>
+      <div
+        style={{
+          width: '5rem',
+          height: '7rem',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          backgroundColor: cardColor
+        }}
+      >
+        <img
+          style={{ width: '100%' }}
+          src={`${cloudFrontURL}${card.imagePath}`}
+        />
+      </div>
+      <div>{card.prompt}</div>
     </div>
   );
 }
