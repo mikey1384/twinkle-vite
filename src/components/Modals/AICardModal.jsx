15ef5677f99d9bc1766ab4ac1a36584751bbb274
mikey1384 <mikey1384@gmail.com>
2022-12-02 15:47:38 +0700

    more updates

--- src/components/Modals/AICardModal.jsx
+++ src/components/Modals/AICardModal.jsx
@@ -111,18 +111,23 @@ export default function AICardModal({ card, onHide }) {
             <div
               style={{
                 width: '100%',
+                height: '100%',
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
                 flexDirection: 'column'
               }}
             >
-              <div>
+              <div
+                style={{ height: '50%', display: 'flex', alignItems: 'center' }}
+              >
                 <Button color="oceanBlue" filled style={{ border: 'none' }}>
                   Sell
                 </Button>
               </div>
-              <div>
+              <div
+                style={{ height: '50%', display: 'flex', alignItems: 'center' }}
+              >
                 <Button color="oceanGreen" filled style={{ border: 'none' }}>
                   Burn
                 </Button>
