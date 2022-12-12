fe856d952a7c4956ec3903cfebbd90b5b7e18af9
mikey1384 <mikey1384@gmail.com>
2022-12-12 14:59:21 +0700

    launch the first version of the story maker

--- src/containers/Home/AIStoriesModal/index.jsx
+++ src/containers/Home/AIStoriesModal/index.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Modal from '~/components/Modal';
+import Loading from '~/components/Loading';
 import { useAppContext } from '~/contexts';
-import { Color, mobileMaxWidth } from '~/constants/css';
+import { mobileMaxWidth } from '~/constants/css';
 import { css } from '@emotion/css';
 
 AIStoriesModal.propTypes = {
@@ -10,16 +11,17 @@ AIStoriesModal.propTypes = {
 };
 
 export default function AIStoriesModal({ onHide }) {
+  const [loading, setLoading] = useState(true);
   const loadAIStory = useAppContext((v) => v.requestHelpers.loadAIStory);
-  const [imageUrl, setImageUrl] = useState('');
   const [storyObj, setStoryObj] = useState({});
 
   useEffect(() => {
     init();
     async function init() {
-      const { imageUrl, storyObj } = await loadAIStory();
-      setImageUrl(imageUrl);
+      setLoading(true);
+      const { storyObj } = await loadAIStory();
       setStoryObj(storyObj);
+      setLoading(false);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -27,43 +29,41 @@ export default function AIStoriesModal({ onHide }) {
   return (
     <Modal
       modalStyle={{
-        backgroundImage: `url(${imageUrl})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
         height: '80vh'
       }}
       large
       closeWhenClickedOutside={false}
-      closeColor="#fff"
       onHide={onHide}
     >
       <main
         style={{
           height: '100%',
-          color: '#fff',
           overflow: 'scroll',
+          scrollBehavior: 'smooth',
           justifyContent: 'flex-start',
           alignItems: 'center'
         }}
       >
-        <div
-          className={css`
-            width: 50%;
-            @media (max-width: ${mobileMaxWidth}) {
-              width: 100%;
-            }
-          `}
-          style={{
-            marginTop: '60vh',
-            marginBottom: '60vh',
-            padding: '2rem',
-            fontSize: '1.7rem',
-            backgroundColor: Color.black(0.9)
-          }}
-        >
-          <div style={{ lineHeight: 3 }}>{storyObj?.story}</div>
-          <div style={{ marginTop: '20rem' }}>{storyObj?.question}</div>
-        </div>
+        {loading ? (
+          <Loading style={{ marginTop: '20vh' }} text="Generating a Story..." />
+        ) : (
+          <div
+            className={css`
+              width: 50%;
+              @media (max-width: ${mobileMaxWidth}) {
+                width: 100%;
+              }
+            `}
+            style={{
+              marginTop: '60vh',
+              marginBottom: '60vh',
+              padding: '2rem',
+              fontSize: '1.7rem'
+            }}
+          >
+            <div style={{ lineHeight: 3 }}>{storyObj?.story}</div>
+          </div>
+        )}
       </main>
     </Modal>
   );
