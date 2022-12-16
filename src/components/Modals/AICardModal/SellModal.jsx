67e422def62a3feb1d476383a7b2cc1f8c3da4c4
mikey1384 <mikey1384@gmail.com>
2022-12-16 16:40:15 +0700

    more update

--- src/components/Modals/AICardModal/SellModal.jsx
+++ src/components/Modals/AICardModal/SellModal.jsx
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import Modal from '~/components/Modal';
 import Button from '~/components/Button';
 import Input from '~/components/Texts/Input';
-import 'react-day-picker/dist/style.css';
+import Icon from '~/components/Icon';
+import { borderRadius, Color } from '~/constants/css';
 
 SellModal.propTypes = {
   onHide: PropTypes.func.isRequired
@@ -11,36 +12,51 @@ SellModal.propTypes = {
 
 export default function SellModal({ onHide }) {
   const [amount, setAmount] = useState(0);
-  const [errors, setErrors] = useState({});
 
   return (
     <Modal large modalOverModal onHide={onHide}>
       <header>List for Sale</header>
       <main>
-        <div>
-          <p style={{ fontSize: '1.4rem', fontWeight: 'bold', color: '#333' }}>
-            Set a price
-          </p>
+        <div
+          style={{
+            height: '30rem',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column'
+          }}
+        >
+          <div
+            style={{
+              fontSize: '1.7rem',
+              fontWeight: 'bold',
+              color: Color.darkerGray(),
+              display: 'flex',
+              alignItems: 'center'
+            }}
+          >
+            Set a price (<Icon icon={['far', 'badge-dollar']} />)
+          </div>
           <Input
             onChange={handleAmountChange}
             placeholder="Amount"
             value={amount}
-            error={errors.amount}
             style={{
-              fontSize: '1.4rem',
-              padding: '0.5rem 1rem',
-              border: '1px solid #333',
-              borderRadius: '4px',
+              fontSize: '1.7rem',
+              padding: '0.5rem',
+              borderRadius,
+              lineHeight: 1.5,
               marginTop: '1rem'
             }}
           />
           <Button
             filled
+            color="oceanBlue"
             onClick={handleCompleteListing}
+            disabled={!amount}
             style={{
               fontSize: '1.4rem',
-              padding: '0.5rem 1rem',
-              marginTop: '1rem'
+              marginTop: '2rem'
             }}
           >
             List for Sale
@@ -56,25 +72,13 @@ export default function SellModal({ onHide }) {
   );
 
   function handleAmountChange(amount) {
-    amount.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
-    setAmount(amount);
-    setErrors({
-      ...errors,
-      amount: amount === '' ? 'Please enter a valid amount.' : ''
-    });
+    const newAmount = Number(
+      amount.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1')
+    );
+    setAmount(newAmount);
   }
 
   function handleCompleteListing() {
-    let hasErrors = false;
-
-    if (amount === '') {
-      setErrors({ ...errors, amount: 'Please enter a valid amount.' });
-      hasErrors = true;
-    }
-
-    if (!hasErrors) {
-      // Calculate listing price and duration using values of `amount`, `startDate`, and `endDate`
-      // Create the listing
-    }
+    console.log('listing');
   }
 }
