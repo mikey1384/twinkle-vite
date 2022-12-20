20cf851eed8c0b6c53caf30df560fcac51142ed9
mikey1384 <mikey1384@gmail.com>
2022-12-20 19:32:33 +0700

    more updates

--- src/components/Modals/AICardModal/UnlistedMenu.jsx
+++ src/components/Modals/AICardModal/UnlistedMenu.jsx
@@ -1,15 +1,55 @@
+import { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import Icon from '~/components/Icon';
 import Button from '~/components/Button';
 import { mobileMaxWidth } from '~/constants/css';
 import { css } from '@emotion/css';
+import { addCommasToNumber } from '~/helpers/stringHelpers';
 
 UnlistedMenu.propTypes = {
+  cardLevel: PropTypes.number.isRequired,
+  cardQuality: PropTypes.string.isRequired,
   onSetSellModalShown: PropTypes.func.isRequired,
   onSetIsBurned: PropTypes.func.isRequired
 };
 
-export default function UnlistedMenu({ onSetSellModalShown, onSetIsBurned }) {
+export default function UnlistedMenu({
+  onSetSellModalShown,
+  onSetIsBurned,
+  cardLevel,
+  cardQuality
+}) {
+  const burnXP = useMemo(() => {
+    // base XP value
+    let xp = 150;
+
+    // color probabilities
+    const colorProbs = {
+      1: 0.5,
+      2: 0.2,
+      3: 0.15,
+      4: 0.1,
+      5: 0.05
+    };
+
+    // adjust XP based on color
+    xp *= 1 / colorProbs[cardLevel];
+
+    // quality probabilities
+    const qualityProbs = {
+      common: 0.5,
+      superior: 0.3,
+      rare: 0.13,
+      elite: 0.05,
+      legendary: 0.02
+    };
+
+    // adjust XP based on quality
+    xp *= 1 / qualityProbs[cardQuality];
+
+    return Math.round(xp);
+  }, [cardLevel, cardQuality]);
+
   return (
     <div
       style={{
@@ -71,7 +111,9 @@ export default function UnlistedMenu({ onSetSellModalShown, onSetIsBurned }) {
             }
           `}
         >
-          {`Destroy this card and receive XP based on its color and quality. The more valuable the color and the higher the quality, the more XP you will receive. This action is irreversible, so use it wisely.`}
+          {`Burn this card and earn ${addCommasToNumber(
+            burnXP
+          )} XP. The more valuable the color and the higher the quality of a card, the more XP you earn by burning it. This action is irreversible, so use it wisely.`}
         </p>
         <Button onClick={() => onSetIsBurned(true)} color="redOrange" filled>
           <Icon icon="fire" />
