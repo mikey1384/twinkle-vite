2ee4f05b366a0fbb6f1caefb5833ac0870253077
mikey1384 <mikey1384@gmail.com>
2022-12-22 19:56:12 +0700

    more updates

--- src/components/AICard/Card.jsx
+++ src/components/AICard/Card.jsx
@@ -1,8 +1,12 @@
 import { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import useAICard from '~/helpers/hooks/useAICard';
+import UsernameText from '~/components/Texts/UsernameText';
 import { css } from '@emotion/css';
-import { cloudFrontURL } from '~/constants/defaultValues';
+import { Color } from '~/constants/css';
+import { useKeyContext } from '~/contexts';
+import { addCommasToNumber } from '~/helpers/stringHelpers';
+import { cloudFrontURL, returnCardBurnXP } from '~/constants/defaultValues';
 import { animated } from 'react-spring';
 
 LiveCard.propTypes = {
@@ -25,6 +29,16 @@ export default function LiveCard({
   onMouseLeave,
   onMouseMove
 }) {
+  const {
+    userLink: { color: userLinkColor },
+    xpNumber: { color: xpNumberColor }
+  } = useKeyContext((v) => v.theme);
+  const burnXP = useMemo(() => {
+    return returnCardBurnXP({
+      cardLevel: card.level,
+      cardQuality: card.quality
+    });
+  }, [card.level, card.quality]);
   const imageExists = useMemo(() => !!card.imagePath, [card.imagePath]);
   const frontPicUrl = `${cloudFrontURL}${card.imagePath}`;
   const { cardCss } = useAICard(card);
@@ -79,6 +93,31 @@ export default function LiveCard({
               src={frontPicUrl}
             />
           ) : null}
+          {card.isBurned && (
+            <div
+              style={{
+                background: '#fff',
+                textAlign: 'center',
+                padding: '5rem 0',
+                lineHeight: 1.7
+              }}
+            >
+              <div>
+                <UsernameText
+                  color={Color[userLinkColor]()}
+                  user={{
+                    username: card.owner.username,
+                    id: card.owner.id
+                  }}
+                />
+              </div>
+              <div>burned this card and earned</div>
+              <b style={{ color: Color[xpNumberColor]() }}>
+                {addCommasToNumber(burnXP)}
+              </b>{' '}
+              <b style={{ color: Color.gold() }}>XP</b>
+            </div>
+          )}
         </div>
       </animated.div>
     </div>
