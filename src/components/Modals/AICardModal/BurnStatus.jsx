765a064e94f2ec115edfd1c6734063e9edd856cc
mikey1384 <mikey1384@gmail.com>
2022-12-22 21:13:32 +0700

    more fix

--- src/components/Modals/AICardModal/BurnStatus.jsx
+++ src/components/Modals/AICardModal/BurnStatus.jsx
@@ -1,6 +1,18 @@
+import { Color } from '~/constants/css';
+
 export default function BurnStatus() {
   return (
-    <div>
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontWeight: 'bold',
+        color: Color.darkerGray()
+      }}
+    >
       <div>This card was burned</div>
     </div>
   );
