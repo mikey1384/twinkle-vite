a40891640c1a4a543fa2867ff7a1450cc331add3
mikey1384 <mikey1384@gmail.com>
2022-12-26 19:16:21 +0700

    more updates

--- src/components/Modals/AICardModal/UnlistedMenu/OwnerMenu/Offers.jsx
+++ src/components/Modals/AICardModal/UnlistedMenu/OwnerMenu/Offers.jsx
@@ -1,6 +1,25 @@
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useAppContext } from '~/contexts';
 import ErrorBoundary from '~/components/ErrorBoundary';
 
-export default function Offers() {
+Offers.propTypes = {
+  cardId: PropTypes.number.isRequired
+};
+
+export default function Offers({ cardId }) {
+  const getOffersForCard = useAppContext(
+    (v) => v.requestHelpers.getOffersForCard
+  );
+  useEffect(() => {
+    init();
+    async function init() {
+      const { offers, loadMoreShown } = await getOffersForCard({ cardId });
+      console.log(offers, loadMoreShown);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <ErrorBoundary componentPath="components/Modals/AICardModal/UnlistedMenu/OwnerMenu/Offers">
       <div>Offers</div>
