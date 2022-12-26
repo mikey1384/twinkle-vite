7b6dd98f76f63d08ed3f47c6eeeca1c11ad7fe72
mikey1384 <mikey1384@gmail.com>
2022-12-26 15:16:12 +0700

    more updates

--- src/components/Modals/AICardModal/UnlistedMenu/NonOwnerMenu/index.jsx
+++ src/components/Modals/AICardModal/UnlistedMenu/NonOwnerMenu/index.jsx
@@ -1,10 +1,9 @@
 import PropTypes from 'prop-types';
-import UsernameText from '~/components/Texts/UsernameText';
-import Button from '~/components/Button';
 import { Color, mobileMaxWidth } from '~/constants/css';
 import { css } from '@emotion/css';
 import { addCommasToNumber } from '~/helpers/stringHelpers';
 import { useKeyContext } from '~/contexts';
+import MakeOffer from './MakeOffer';
 
 NonOwnerMenu.propTypes = {
   burnXP: PropTypes.number.isRequired,
@@ -58,55 +57,12 @@ export default function NonOwnerMenu({
           <b style={{ color: Color.gold() }}>XP</b>)
         </p>
       </div>
-      <div style={{ width: '100%', marginTop: '3rem', textAlign: 'center' }}>
-        <div>
-          Owned by
-          <div>
-            <UsernameText
-              color={Color[userLinkColor]()}
-              user={{
-                username: owner.username,
-                id: owner.id
-              }}
-            />
-          </div>
-        </div>
-        <div
-          className={css`
-            margin-top: 1.7rem;
-            @media (max-width: ${mobileMaxWidth}) {
-              margin-top: 1rem;
-            }
-          `}
-          style={{
-            width: '100%',
-            display: 'flex',
-            justifyContent: 'center'
-          }}
-        >
-          <Button
-            className={css`
-              @media (max-width: ${mobileMaxWidth}) {
-                padding: 0.7rem !important;
-              }
-            `}
-            onClick={() => onSetOfferModalShown(true)}
-            color="oceanBlue"
-            filled
-          >
-            <span
-              className={css`
-                font-size: 1.6rem;
-                @media (max-width: ${mobileMaxWidth}) {
-                  font-size: 1rem;
-                }
-              `}
-            >
-              Make offer
-            </span>
-          </Button>
-        </div>
-      </div>
+      <MakeOffer
+        owner={owner}
+        onSetOfferModalShown={onSetOfferModalShown}
+        userLinkColor={userLinkColor}
+        style={{ width: '100%', marginTop: '3rem', textAlign: 'center' }}
+      />
     </div>
   );
 }
