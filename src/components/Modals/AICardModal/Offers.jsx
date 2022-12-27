a0e6c4e720b9a6ccf6038e0a7f9ba865d49c3b8b
mikey1384 <mikey1384@gmail.com>
2022-12-27 09:30:45 +0700

    more updates

--- src/components/Modals/AICardModal/Offers.jsx
+++ src/components/Modals/AICardModal/Offers.jsx
@@ -1,15 +1,20 @@
 import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import UsernameText from '~/components/Texts/UsernameText';
 import ErrorBoundary from '~/components/ErrorBoundary';
-import { useAppContext } from '~/contexts';
+import Icon from '~/components/Icon';
+import { useAppContext, useKeyContext } from '~/contexts';
 import { css } from '@emotion/css';
-import { mobileMaxWidth } from '~/constants/css';
+import { Color, mobileMaxWidth } from '~/constants/css';
 
 Offers.propTypes = {
   cardId: PropTypes.number.isRequired
 };
 
 export default function Offers({ cardId }) {
+  const {
+    userLink: { color: userLinkColor }
+  } = useKeyContext((v) => v.theme);
   const [offers, setOffers] = useState([]);
   const getOffersForCard = useAppContext(
     (v) => v.requestHelpers.getOffersForCard
@@ -31,9 +36,14 @@ export default function Offers({ cardId }) {
       <div>
         {offers.map((offer) => {
           return (
-            <div
+            <nav
               className={css`
+                height: 7rem;
+                display: flex;
+                justify-content: center;
+                align-items: center;
                 font-size: 1.6rem;
+                border-bottom: 1px solid ${Color.borderGray()};
                 @media (max-width: ${mobileMaxWidth}) {
                   font-size: 1.1rem;
                 }
@@ -41,8 +51,22 @@ export default function Offers({ cardId }) {
               key={offer.id}
               style={{ marginBottom: '1rem' }}
             >
-              {offer.user.username}
-            </div>
+              <Icon
+                style={{ color: Color.brownOrange() }}
+                icon={['far', 'badge-dollar']}
+              />
+              <span style={{ marginLeft: '0.2rem' }}>
+                {offer.offerPrice} offer from
+              </span>
+              <UsernameText
+                style={{ marginLeft: '0.5rem' }}
+                color={Color[userLinkColor]()}
+                user={{
+                  username: offer.user.username,
+                  id: offer.user.id
+                }}
+              />
+            </nav>
           );
         })}
       </div>
