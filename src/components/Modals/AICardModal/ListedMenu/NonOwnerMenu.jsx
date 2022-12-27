d449808d2c2c6c9b4e57a5df374d4aea7bcffa99
mikey1384 <mikey1384@gmail.com>
2022-12-27 10:17:08 +0700

    more updates

---
+++ src/components/Modals/AICardModal/ListedMenu/NonOwnerMenu.jsx
@@ -0,0 +1,40 @@
+import Button from '~/components/Button';
+import Icon from '~/components/Icon';
+import { css } from '@emotion/css';
+import { mobileMaxWidth } from '~/constants/css';
+
+export default function NonOwnerMenu() {
+  return (
+    <Button
+      className={css`
+        @media (max-width: ${mobileMaxWidth}) {
+          padding: 0.7rem !important;
+        }
+      `}
+      onClick={() => console.log('buy')}
+      color="oceanBlue"
+      filled
+    >
+      <Icon
+        className={css`
+          font-size: 1.6rem;
+          @media (max-width: ${mobileMaxWidth}) {
+            font-size: 1rem;
+          }
+        `}
+        icon="shopping-cart"
+      />
+      <span
+        className={css`
+          font-size: 1.6rem;
+          @media (max-width: ${mobileMaxWidth}) {
+            font-size: 1rem;
+          }
+        `}
+        style={{ marginLeft: '0.7rem' }}
+      >
+        Buy
+      </span>
+    </Button>
+  );
+}
