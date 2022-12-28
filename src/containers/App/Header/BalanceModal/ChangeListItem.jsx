cf39ad46dc19526b6b230f8e2d5fb28220b1fe1d
mikey1384 <mikey1384@gmail.com>
2022-12-28 17:40:07 +0700

    more updates

--- src/containers/App/Header/BalanceModal/ChangeListItem.jsx
+++ src/containers/App/Header/BalanceModal/ChangeListItem.jsx
@@ -1,14 +1,31 @@
+import { useMemo } from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 ChangeListItem.propTypes = {
   change: PropTypes.object.isRequired
 };
 
 export default function ChangeListItem({ change }) {
+  const displayedTimeStamp = useMemo(
+    () => moment.unix(change.timeStamp).format('lll'),
+    [change?.timeStamp]
+  );
+
   return (
-    <nav>
-      <div>
-        {change.timeStamp} {change.action} {change.target} {change.type}{' '}
+    <nav
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+      }}
+    >
+      <div>{displayedTimeStamp}</div>
+      <div style={{ marginLeft: '2rem' }}>
+        {change.action} {change.target}
+      </div>
+      <div style={{ marginLeft: '2rem' }}>
+        {change.type}
         {change.amount}
       </div>
     </nav>
