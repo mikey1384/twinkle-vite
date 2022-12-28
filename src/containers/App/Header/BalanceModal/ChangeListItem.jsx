0498c6402c04aafe95e109629e4cce2ddc6aeec1
mikey1384 <mikey1384@gmail.com>
2022-12-28 17:43:44 +0700

    more updates

--- src/containers/App/Header/BalanceModal/ChangeListItem.jsx
+++ src/containers/App/Header/BalanceModal/ChangeListItem.jsx
@@ -1,6 +1,8 @@
 import { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
+import { Color, mobileMaxWidth } from '~/constants/css';
+import { css } from '@emotion/css';
 
 ChangeListItem.propTypes = {
   change: PropTypes.object.isRequired
@@ -20,7 +22,17 @@ export default function ChangeListItem({ change }) {
         justifyContent: 'space-between'
       }}
     >
-      <div>{displayedTimeStamp}</div>
+      <div
+        className={css`
+          font-size: 1.3rem;
+          color: ${Color.darkGray()};
+          @media (max-width: ${mobileMaxWidth}) {
+            font-size: 1.2rem;
+          }
+        `}
+      >
+        {displayedTimeStamp}
+      </div>
       <div style={{ marginLeft: '2rem' }}>
         {change.action} {change.target}
       </div>
