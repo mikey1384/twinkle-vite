ced46e240eb537d8ecacdd94b7d11418b853f33a
mikey1384 <mikey1384@gmail.com>
2022-12-28 17:08:15 +0700

    more updates

---
+++ src/containers/App/Header/BalanceModal/ChangeListItem.jsx
@@ -0,0 +1,7 @@
+export default function ChangeListItem() {
+  return (
+    <div>
+      <div>change list item</div>
+    </div>
+  );
+}
--- src/containers/App/Header/BalanceModal/index.jsx
+++ src/containers/App/Header/BalanceModal/index.jsx
@@ -1,22 +1,32 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Modal from '~/components/Modal';
 import Button from '~/components/Button';
-import { useAppContext } from '~/contexts';
+import ChangeListItem from './ChangeListItem';
+import LoadMoreButton from '~/components/Buttons/LoadMoreButton';
+import { useAppContext, useKeyContext } from '~/contexts';
 
 BalanceModal.propTypes = {
   onHide: PropTypes.func.isRequired
 };
 
 export default function BalanceModal({ onHide }) {
+  const {
+    loadMoreButton: { color: loadMoreButtonColor }
+  } = useKeyContext((v) => v.theme);
   const loadCoinHistory = useAppContext(
     (v) => v.requestHelpers.loadCoinHistory
   );
+  const [changes, setChanges] = useState([]);
+  const [loadMoreShown, setLoadMoreShown] = useState(false);
+  const [loadingMore, setLoadingMore] = useState(false);
+
   useEffect(() => {
     init();
     async function init() {
       const { changes, loadMoreShown } = await loadCoinHistory();
-      console.log(changes, loadMoreShown);
+      setChanges(changes);
+      setLoadMoreShown(loadMoreShown);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -24,7 +34,20 @@ export default function BalanceModal({ onHide }) {
   return (
     <Modal onHide={onHide}>
       <header>Your Twinkle Coins</header>
-      <main>balance goes here</main>
+      <main>
+        {changes.map((change) => (
+          <ChangeListItem key={change.id} change={change} />
+        ))}
+        {loadMoreShown && (
+          <LoadMoreButton
+            style={{ marginTop: '1.5em' }}
+            loading={loadingMore}
+            filled
+            color={loadMoreButtonColor}
+            onClick={() => setLoadingMore(true)}
+          />
+        )}
+      </main>
       <footer>
         <Button transparent onClick={onHide}>
           Close
