b18fbece99d76d1a3e0e520f7d042f4365e1009e
mikey1384 <mikey1384@gmail.com>
2022-12-28 18:18:48 +0700

    more updates

--- src/containers/App/Header/BalanceModal/ChangeListItem/ActionBlock.jsx
+++ src/containers/App/Header/BalanceModal/ChangeListItem/ActionBlock.jsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import PropTypes from 'prop-types';
 
 ActionBlock.propTypes = {
@@ -7,9 +8,47 @@ ActionBlock.propTypes = {
 };
 
 export default function ActionBlock({ action, target, style }) {
-  return (
-    <div style={style}>
-      {action} {target}
-    </div>
-  );
+  const displayedAction = useMemo(() => {
+    if (action === 'generate') {
+      return 'summoned a card';
+    }
+    if (action === 'repeat') {
+      return 'completed a repeatable mission';
+    }
+    if (action === 'recommend') {
+      if (target === 'pass') {
+        return 'recommended an achievement';
+      }
+      return `recommended a ${target}`;
+    }
+    if (action === 'watch') {
+      return 'watched a video';
+    }
+    if (action === 'purchase') {
+      if (target === 'chatSubject') {
+        return 'unlocked chat group topic feature';
+      }
+      if ((target || '').includes('chatTheme')) {
+        return `unlocked ${target
+          .replace('chatTheme ', '')
+          .toLowerCase()} chat theme`;
+      }
+      if (target === 'user') {
+        return 'changed username';
+      }
+    }
+    if (action === 'attempt') {
+      if (target === 'mission') {
+        return 'completed a mission';
+      }
+    }
+    if (action === 'register') {
+      return 'collected a vocabulary';
+    }
+    if (action === 'reward') {
+      return 'rewarded twinkles';
+    }
+    return `${action} ${target}`;
+  }, [action, target]);
+  return <div style={style}>{displayedAction}</div>;
 }
