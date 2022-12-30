af898a312a6b59b4703ba9c27bd9678593936b30
mikey1384 <mikey1384@gmail.com>
2022-12-30 17:46:01 +0700

    more updates

--- src/containers/Chat/Body/Collect/AICards/ActivitiesContainer/Activity/OfferActivity.jsx
+++ src/containers/Chat/Body/Collect/AICards/ActivitiesContainer/Activity/OfferActivity.jsx
@@ -1,4 +1,8 @@
 import PropTypes from 'prop-types';
+import UsernameText from '~/components/Texts/UsernameText';
+import ProfilePic from '~/components/ProfilePic';
+import { css } from '@emotion/css';
+import { mobileMaxWidth } from '~/constants/css';
 import CardThumb from '../../../../../CardThumb';
 
 OfferActivity.propTypes = {
@@ -7,9 +11,29 @@ OfferActivity.propTypes = {
 };
 
 export default function OfferActivity({ card, feed }) {
+  const offer = feed.offer;
   return (
     <div>
-      {JSON.stringify(feed)}
+      <ProfilePic
+        style={{ width: '100%' }}
+        profilePicUrl={offer.user.profilePicUrl}
+        userId={offer.user.id}
+      />
+      <div>
+        <UsernameText
+          className={css`
+            font-size: 1.7rem;
+            line-height: 1;
+            @media (max-width: ${mobileMaxWidth}) {
+              font-size: 1.3rem;
+            }
+          `}
+          user={{
+            id: offer.user.id,
+            username: offer.user.username
+          }}
+        />
+      </div>
       <div>
         <CardThumb card={card} />
       </div>
