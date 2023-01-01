374743beb28cafc694c9565ab4b081be01538ec5
mikey1384 <mikey1384@gmail.com>
2023-01-01 17:29:27 +0700

    more updates

--- src/components/Modals/AICardModal/Offers/OfferDetailModal/OfferListItem.jsx
+++ src/components/Modals/AICardModal/Offers/OfferDetailModal/OfferListItem.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import ErrorBoundary from '~/components/ErrorBoundary';
 import Icon from '~/components/Icon';
 import UsernameText from '~/components/Texts/UsernameText';
+import Button from '~/components/Button';
 import { Color, mobileMaxWidth } from '~/constants/css';
 import { css } from '@emotion/css';
 
@@ -27,34 +28,53 @@ export default function OfferListItem({
           justify-content: center;
           align-items: center;
           font-size: 1.6rem;
-          cursor: pointer;
-          &:hover {
-            background-color: ${Color.highlightGray()};
-          }
           @media (max-width: ${mobileMaxWidth}) {
             height: 3rem;
             font-size: 0.8rem;
           }
         `}
       >
-        <div>
-          <Icon
-            style={{ color: Color.brownOrange() }}
-            icon={['far', 'badge-dollar']}
-          />
-          <span style={{ marginLeft: '0.2rem' }}>
-            <b style={{ color: Color.darkerGray() }}>{offer.offerPrice}</b>
-            <span> offer </span> from{' '}
-          </span>
-          <UsernameText
-            onMenuShownChange={onUserMenuShown}
-            color={Color[userLinkColor]()}
-            displayedName={offer.userId === userId ? 'you' : offer.username}
-            user={{
-              username: offer.username,
-              id: offer.userId
+        <div
+          style={{
+            width: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between '
+          }}
+        >
+          <div
+            style={{
+              flexGrow: 1,
+              display: 'flex',
+              width: '100%',
+              justifyContent: 'center'
             }}
-          />
+          >
+            <div>
+              <Icon
+                style={{ color: Color.brownOrange() }}
+                icon={['far', 'badge-dollar']}
+              />
+              <span style={{ marginLeft: '0.2rem' }}>
+                <b style={{ color: Color.darkerGray() }}>{offer.offerPrice}</b>
+                <span> offer </span> from{' '}
+              </span>
+              <UsernameText
+                onMenuShownChange={onUserMenuShown}
+                color={Color[userLinkColor]()}
+                displayedName={offer.userId === userId ? 'you' : offer.username}
+                user={{
+                  username: offer.username,
+                  id: offer.userId
+                }}
+              />
+            </div>
+          </div>
+          <div>
+            <Button color="oceanBlue" filled>
+              Accept
+            </Button>
+          </div>
         </div>
       </nav>
     </ErrorBoundary>
