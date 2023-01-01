fb76fd64a834af572f29fa8c6d758119c87828c9
mikey1384 <mikey1384@gmail.com>
2023-01-01 12:42:19 +0700

    more fix

--- vite.config.js
+++ vite.config.js
@@ -35,6 +35,6 @@ export default defineConfig({
     rollupOptions: {
       plugins: [inject({ Buffer: ['buffer', 'Buffer'], process: 'process' })]
     },
-    sourcemap: true
+    sourcemap: 'inline'
   }
 });
