ff1179b76902dadaa2a494d165c11963e8123874
mikey1384 <mikey1384@gmail.com>
2023-01-08 15:00:10 +0700

    minor fix

--- src/containers/Chat/LeftMenu/Collect/AICards.jsx
+++ src/containers/Chat/LeftMenu/Collect/AICards.jsx
@@ -42,18 +42,18 @@ export default function AICards() {
     if (lastActivity?.type === 'offer') {
       return `offered ${addCommasToNumber(lastActivity?.offer?.price)} coin${
         lastActivity?.offer?.price > 1 ? 's' : ''
-      } for a`;
+      } for a${card.quality === 'elite' ? 'n' : ''}`;
     }
     if (lastActivity?.type === 'transfer') {
       if (!!lastActivity?.transfer?.askId) {
-        return 'bought a';
+        return `bought a${card.quality === 'elite' ? 'n' : ''}`;
       }
       if (!!lastActivity?.transfer?.offerId) {
-        return 'sold a';
+        return `sold a${card.quality === 'elite' ? 'n' : ''}`;
       }
     }
-    return 'summoned a';
-  }, [aiCardFeeds]);
+    return `summoned a${card.quality === 'elite' ? 'n' : ''}`;
+  }, [aiCardFeeds, card.quality]);
 
   return (
     <div style={{ height: '5rem', position: 'relative' }}>
