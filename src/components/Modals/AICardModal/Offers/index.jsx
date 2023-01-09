eccb1be66c7681c9b5b477e6e0947055b354bd94
mikey1384 <mikey1384@gmail.com>
2023-01-09 17:10:20 +0700

    minor fix

--- src/components/Modals/AICardModal/Offers/index.jsx
+++ src/components/Modals/AICardModal/Offers/index.jsx
@@ -4,6 +4,7 @@ import ErrorBoundary from '~/components/ErrorBoundary';
 import LoadMoreButton from '~/components/Buttons/LoadMoreButton';
 import OfferPriceListItem from './OfferPriceListItem';
 import Button from '~/components/Button';
+import Loading from '~/components/Loading';
 import { useKeyContext } from '~/contexts';
 import { css } from '@emotion/css';
 import { Color, mobileMaxWidth } from '~/constants/css';
@@ -57,75 +58,80 @@ export default function Offers({
           overflow: 'scroll'
         }}
       >
-        {loaded && offers.length === 0 && (
-          <div
-            className={css`
-              font-size: 1.6rem;
-              @media (max-width: ${mobileMaxWidth}) {
-                font-size: 1.1rem;
-              }
-            `}
-            style={{
-              width: '100%',
-              height: '100%',
-              padding: '0 0.5rem',
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center',
-              textAlign: 'center'
-            }}
-          >
-            There is no offer for this card, yet
-            {ownerId !== userId && (
-              <Button
-                style={{ marginTop: '2rem' }}
-                className={css`
-                  @media (max-width: ${mobileMaxWidth}) {
-                    padding: 0.7rem !important;
-                  }
-                `}
-                onClick={() => onSetOfferModalShown(true)}
-                color="green"
-                disabled={!userId}
-                filled
-              >
-                <span
+        {loaded ? (
+          offers.length === 0 ? (
+            <div
+              className={css`
+                font-size: 1.6rem;
+                @media (max-width: ${mobileMaxWidth}) {
+                  font-size: 1.1rem;
+                }
+              `}
+              style={{
+                width: '100%',
+                height: '100%',
+                padding: '0 0.5rem',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                textAlign: 'center'
+              }}
+            >
+              There is no offer for this card, yet
+              {ownerId !== userId && (
+                <Button
+                  style={{ marginTop: '2rem' }}
                   className={css`
-                    font-size: 1.6rem;
                     @media (max-width: ${mobileMaxWidth}) {
-                      font-size: 1rem;
+                      padding: 0.7rem !important;
                     }
                   `}
+                  onClick={() => onSetOfferModalShown(true)}
+                  color="green"
+                  disabled={!userId}
+                  filled
                 >
-                  Make offer
-                </span>
-              </Button>
-            )}
-          </div>
+                  <span
+                    className={css`
+                      font-size: 1.6rem;
+                      @media (max-width: ${mobileMaxWidth}) {
+                        font-size: 1rem;
+                      }
+                    `}
+                  >
+                    Make offer
+                  </span>
+                </Button>
+              )}
+            </div>
+          ) : (
+            offers.map((offer) => {
+              let offerers = offer.users;
+              const myOffer = offerers.find((offerer) => offerer.id === userId);
+              if (myOffer) {
+                offerers = [myOffer].concat(
+                  offerers.filter((offerer) => offerer.id !== userId)
+                );
+              }
+              return (
+                <OfferPriceListItem
+                  key={offer.price}
+                  cardId={cardId}
+                  offer={offer}
+                  offerers={offerers}
+                  onSetActiveTab={onSetActiveTab}
+                  onUserMenuShownChange={onUserMenuShownChange}
+                  ownerId={ownerId}
+                  userId={userId}
+                  usermenuShown={usermenuShown}
+                />
+              );
+            })
+          )
+        ) : (
+          <Loading style={{ height: 'CALC(100% - 3rem)' }} />
         )}
-        {offers.map((offer) => {
-          let offerers = offer.users;
-          const myOffer = offerers.find((offerer) => offerer.id === userId);
-          if (myOffer) {
-            offerers = [myOffer].concat(
-              offerers.filter((offerer) => offerer.id !== userId)
-            );
-          }
-          return (
-            <OfferPriceListItem
-              key={offer.price}
-              cardId={cardId}
-              offer={offer}
-              offerers={offerers}
-              onSetActiveTab={onSetActiveTab}
-              onUserMenuShownChange={onUserMenuShownChange}
-              ownerId={ownerId}
-              userId={userId}
-              usermenuShown={usermenuShown}
-            />
-          );
-        })}
         {loadMoreShown && (
           <LoadMoreButton
             filled
