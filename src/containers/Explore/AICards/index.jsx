01eb4da2d955d1dc8ae5b57bd91e3ab79f41f0d1
mikey1384 <mikey1384@gmail.com>
2023-01-11 22:37:51 +0700

    more updates

--- src/containers/Explore/AICards/index.jsx
+++ src/containers/Explore/AICards/index.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
 import ErrorBoundary from '~/components/ErrorBoundary';
+import { useAppContext } from '~/contexts';
 
 export default function AICards() {
+  const loadAICards = useAppContext((v) => v.requestHelpers.loadAICards);
+  useEffect(() => {
+    init();
+    async function init() {
+      const data = await loadAICards();
+      console.log(data);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <ErrorBoundary componentPath="Explore/AICards">
       <div>
