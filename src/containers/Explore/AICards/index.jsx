517a72730893cec2cf102cc98eab20758aac0f32
mikey1384 <mikey1384@gmail.com>
2023-01-11 23:10:04 +0700

    more updates

--- src/containers/Explore/AICards/index.jsx
+++ src/containers/Explore/AICards/index.jsx
@@ -1,23 +1,23 @@
 import { useEffect } from 'react';
 import ErrorBoundary from '~/components/ErrorBoundary';
-import { useAppContext } from '~/contexts';
+import { useAppContext, useExploreContext } from '~/contexts';
 
 export default function AICards() {
   const loadAICards = useAppContext((v) => v.requestHelpers.loadAICards);
+  const cards = useExploreContext((v) => v.state.aiCards.cards);
+  const onLoadAICards = useExploreContext((v) => v.actions.onLoadAICards);
   useEffect(() => {
     init();
     async function init() {
       const cards = await loadAICards();
-      console.log(cards);
+      onLoadAICards({ cards });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <ErrorBoundary componentPath="Explore/AICards">
-      <div>
-        <h1>AICards</h1>
-      </div>
+      <div>{cards.map((card) => card.id)}</div>
     </ErrorBoundary>
   );
 }
