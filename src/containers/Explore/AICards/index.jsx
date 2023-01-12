00e55d474a4f698c44ea72db70b70b2b22e9a32b
mikey1384 <mikey1384@gmail.com>
2023-01-12 08:38:36 +0700

    more updates

--- src/containers/Explore/AICards/index.jsx
+++ src/containers/Explore/AICards/index.jsx
@@ -1,12 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import ErrorBoundary from '~/components/ErrorBoundary';
 import { useAppContext, useExploreContext } from '~/contexts';
+import AICardModal from '~/components/Modals/AICardModal';
 import AICard from '~/components/AICard';
+import queryString from 'query-string';
+import { useLocation } from 'react-router-dom';
 
 export default function AICards() {
+  const { search } = useLocation();
+  const [aiCardModalCardId, setAICardModalCardId] = useState(null);
   const loadAICards = useAppContext((v) => v.requestHelpers.loadAICards);
   const cards = useExploreContext((v) => v.state.aiCards.cards);
   const onLoadAICards = useExploreContext((v) => v.actions.onLoadAICards);
+  useEffect(() => {
+    const { cardId } = queryString.parse(search);
+    if (cardId) {
+      setAICardModalCardId(Number(cardId));
+    }
+  }, [search]);
   useEffect(() => {
     init();
     async function init() {
@@ -27,10 +38,22 @@ export default function AICards() {
       >
         {cards.map((card) => (
           <div key={card.id} style={{ margin: '1rem' }}>
-            <AICard card={card} detailShown />
+            <AICard
+              card={card}
+              onClick={() => setAICardModalCardId(card.id)}
+              detailShown
+            />
           </div>
         ))}
       </div>
+      {aiCardModalCardId && (
+        <AICardModal
+          cardId={aiCardModalCardId}
+          onHide={() => {
+            setAICardModalCardId(null);
+          }}
+        />
+      )}
     </ErrorBoundary>
   );
 }
