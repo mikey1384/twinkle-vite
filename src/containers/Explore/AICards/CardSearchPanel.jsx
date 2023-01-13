ec8cdc0a73eb989938a853b5753a4399466d6493
mikey1384 <mikey1384@gmail.com>
2023-01-13 16:06:34 +0700

    more search panel updates

--- src/containers/Explore/AICards/CardSearchPanel.jsx
+++ src/containers/Explore/AICards/CardSearchPanel.jsx
@@ -1,6 +1,17 @@
+import { css } from '@emotion/css';
+import { Color } from '~/constants/css';
+
 export default function CardSearchPanel() {
   return (
-    <div>
+    <div
+      className={css`
+        font-size: 1.7rem;
+        width: 100%;
+        padding: 1rem;
+        background: #fff;
+        border: 1px solid ${Color.borderGray()};
+      `}
+    >
       <div>search panel</div>
     </div>
   );
