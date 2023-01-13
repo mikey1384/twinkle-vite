8b27a69e71d83dcec3b6fc78443ab022d709b276
mikey1384 <mikey1384@gmail.com>
2023-01-13 18:51:13 +0700

    more updates

--- src/containers/Explore/AICards/FilterModal/index.jsx
+++ src/containers/Explore/AICards/FilterModal/index.jsx
@@ -1,6 +1,13 @@
+import { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import Modal from '~/components/Modal';
 import Button from '~/components/Button';
+import OwnerFilter from './OwnerFilter';
+import ColorFilter from './ColorFilter';
+import QualityFilter from './QualityFilter';
+import PriceFilter from './PriceFilter';
+import CardIdFilter from './CardIdFilter';
+import WordFilter from './WordFilter';
 
 FilterModal.propTypes = {
   onHide: PropTypes.func.isRequired,
@@ -8,10 +15,46 @@ FilterModal.propTypes = {
 };
 
 export default function FilterModal({ selectedFilter, onHide }) {
+  const filterComponents = useMemo(() => {
+    const defaultFilters = [
+      'owner',
+      'color',
+      'quality',
+      'price',
+      'cardId',
+      'word'
+    ];
+    const result = [selectedFilter].concat(
+      defaultFilters.filter((f) => f !== selectedFilter)
+    );
+    return result;
+  }, [selectedFilter]);
   return (
     <Modal large onHide={onHide}>
       <header>Search Cards</header>
-      <main>{selectedFilter}</main>
+      <main>
+        {filterComponents.map((component) => {
+          if (component === 'owner') {
+            return <OwnerFilter key={component} />;
+          }
+          if (component === 'color') {
+            return <ColorFilter key={component} />;
+          }
+          if (component === 'quality') {
+            return <QualityFilter key={component} />;
+          }
+          if (component === 'price') {
+            return <PriceFilter key={component} />;
+          }
+          if (component === 'cardId') {
+            return <CardIdFilter key={component} />;
+          }
+          if (component === 'word') {
+            return <WordFilter key={component} />;
+          }
+          return null;
+        })}
+      </main>
       <footer>
         <Button transparent onClick={onHide}>
           Close
