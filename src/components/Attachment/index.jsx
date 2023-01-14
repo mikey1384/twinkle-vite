e301b0dcf0448424e49f2883671bd23d52ad3744
mikey1384 <mikey1384@gmail.com>
2023-01-14 20:55:00 +0700

    more fix

--- src/components/Attachment/index.jsx
+++ src/components/Attachment/index.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import Icon from '~/components/Icon';
+import CloseButton from '~/components/Buttons/CloseButton';
 import ErrorBoundary from '~/components/ErrorBoundary';
 import WebsiteContent from './WebsiteContent';
 import FileContent from '~/components/FileContent';
@@ -29,26 +29,7 @@ export default function Attachment({
         ...style
       }}
     >
-      <Icon
-        icon="times"
-        style={{
-          zIndex: 1,
-          display: 'flex',
-          background: '#000',
-          color: '#fff',
-          borderRadius: '50%',
-          justifyContent: 'center',
-          alignItems: 'center',
-          padding: '0.2rem',
-          width: '2rem',
-          height: '2rem',
-          position: 'absolute',
-          cursor: 'pointer',
-          right: '-0.5rem',
-          top: '-1rem'
-        }}
-        onClick={onClose}
-      />
+      <CloseButton onClick={onClose} />
       {contentType === 'file' ? (
         <FileContent
           file={attachment.file}
