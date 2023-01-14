cfda3cfa440ab6336b1af19cd81fc4d34a652178
mikey1384 <mikey1384@gmail.com>
2023-01-14 16:54:15 +0700

    more updates

--- src/containers/Explore/AICards/FilterModal/OwnerFilter.jsx
+++ src/containers/Explore/AICards/FilterModal/OwnerFilter.jsx
@@ -1,7 +1,49 @@
+import { useState } from 'react';
+import SearchInput from '~/components/Texts/SearchInput';
+import Loading from '~/components/Loading';
+import { useAppContext } from '~/contexts';
+import { useSearch } from '~/helpers/hooks';
+
 export default function OwnerFilter() {
+  const searchUsers = useAppContext((v) => v.requestHelpers.searchUsers);
+  const [searchText, setSearchText] = useState('');
+  const [searchedUsers, setSearchedUsers] = useState([]);
+  const { handleSearch, searching } = useSearch({
+    onSearch: handleUserSearch,
+    onClear: () => setSearchedUsers([]),
+    onSetSearchText: setSearchText
+  });
+
   return (
-    <div>
-      <div>this is the owner filter</div>
+    <div style={{ position: 'relative' }}>
+      <SearchInput
+        placeholder="Search..."
+        onChange={handleSearch}
+        value={searchText}
+        searchResults={searchedUsers}
+        renderItemLabel={(item) => (
+          <span>
+            {item.username} <small>{`(${item.realName})`}</small>
+          </span>
+        )}
+        onClickOutSide={() => {
+          setSearchText('');
+          setSearchedUsers([]);
+        }}
+        onSelect={handleSelectUser}
+      />
+      {searching && <Loading style={{ position: 'absolute', top: 0 }} />}
     </div>
   );
+
+  function handleSelectUser(user) {
+    console.log(user);
+    setSearchedUsers([]);
+    setSearchText('');
+  }
+
+  async function handleUserSearch(text) {
+    const users = await searchUsers(text);
+    setSearchedUsers(users);
+  }
 }
