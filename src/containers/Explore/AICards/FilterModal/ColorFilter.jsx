36d7fc86419bb0e79bfedbd7e1f5dab94f7d6e3a
mikey1384 <mikey1384@gmail.com>
2023-01-14 19:49:46 +0700

    more updates

--- src/containers/Explore/AICards/FilterModal/ColorFilter.jsx
+++ src/containers/Explore/AICards/FilterModal/ColorFilter.jsx
@@ -1,7 +1,49 @@
-export default function ColorFilter() {
+import { useMemo } from 'react';
+import PropTypes from 'prop-types';
+import DropdownButton from '~/components/Buttons/DropdownButton';
+
+ColorFilter.propTypes = {
+  selectedColor: PropTypes.string,
+  onDropdownShown: PropTypes.func,
+  onSelectColor: PropTypes.func
+};
+
+export default function ColorFilter({
+  selectedColor,
+  onDropdownShown,
+  onSelectColor
+}) {
+  const menuProps = useMemo(() => {
+    const colors = ['blue', 'pink', 'orange', 'gold'];
+    const rearrangedColor = colors.filter((color) => color !== selectedColor);
+    return rearrangedColor.map((color) => ({
+      label: color,
+      onClick: () => onSelectColor(color)
+    }));
+  }, [onSelectColor, selectedColor]);
+
   return (
-    <div>
-      <div>this is the color filter</div>
+    <div
+      style={{
+        padding: '1rem',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <div>
+        <b>Color:</b>
+      </div>
+      <div style={{ marginLeft: '1rem' }}>
+        <DropdownButton
+          skeuomorphic
+          color="darkerGray"
+          icon="caret-down"
+          text={selectedColor}
+          onDropdownShown={onDropdownShown}
+          menuProps={menuProps}
+        />
+      </div>
     </div>
   );
 }
