cde4865b3167e49b7f07ddc475c9b266212a5920
mikey1384 <mikey1384@gmail.com>
2023-01-15 08:39:40 +0700

    more updates

---
+++ src/containers/Explore/AICards/SearchView.jsx
@@ -0,0 +1,14 @@
+export default function SearchView() {
+  return (
+    <div
+      style={{
+        marginTop: '3rem',
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center'
+      }}
+    >
+      search view
+    </div>
+  );
+}
