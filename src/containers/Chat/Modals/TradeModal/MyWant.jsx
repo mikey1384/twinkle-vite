ac41a7cc115a4fad00a9ec15c33b97d7dee46adf
mikey1384 <mikey1384@gmail.com>
2023-01-30 20:45:19 +0700

    more fix

--- src/containers/Chat/Modals/TradeModal/MyWant.jsx
+++ src/containers/Chat/Modals/TradeModal/MyWant.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Icon from '~/components/Icon';
 import Input from '~/components/Texts/Input';
+import Button from '~/components/Button';
 import { css } from '@emotion/css';
 import { borderRadius, Color } from '~/constants/css';
 
@@ -26,56 +27,74 @@ export default function MyWant({ style }) {
           border: `1px solid ${Color.borderGray()}`,
           display: 'flex',
           justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
           borderRadius
         }}
       >
-        <div
-          style={{ display: 'flex', flexDirection: 'column', width: 'auto' }}
-        >
-          <div>
-            <div
-              className={css`
-                font-weight: bold;
-                font-size: 1.6rem;
-                color: ${Color.darkerGray()};
-                display: flex;
-                align-items: center;
-              `}
-            >
-              Twinkle Coins (
-              <Icon
-                style={{ color: Color.brownOrange() }}
-                icon={['far', 'badge-dollar']}
-              />
-              )
-            </div>
-            <Input
-              onChange={handleAmountChange}
-              placeholder="Amount"
-              value={amount}
-              style={{
-                fontSize: '1.7rem',
-                padding: '0.5rem',
-                borderRadius,
-                lineHeight: 1.5,
-                marginTop: '0.5rem'
-              }}
+        <div style={{ width: 'auto' }}>
+          <div
+            className={css`
+              font-weight: bold;
+              font-size: 1.6rem;
+              color: ${Color.darkerGray()};
+              display: flex;
+              align-items: center;
+            `}
+          >
+            Twinkle Coins (
+            <Icon
+              style={{ color: Color.brownOrange() }}
+              icon={['far', 'badge-dollar']}
             />
+            )
           </div>
-          <div style={{ marginTop: '2rem' }}>
-            <div
-              className={css`
-                font-weight: bold;
-                font-size: 1.6rem;
-                color: ${Color.darkerGray()};
-                display: flex;
-                align-items: center;
-              `}
-            >
-              AI Cards
-            </div>
-            button
+          <Input
+            onChange={handleAmountChange}
+            placeholder="Amount"
+            value={amount}
+            style={{
+              fontSize: '1.7rem',
+              padding: '0.5rem',
+              borderRadius,
+              lineHeight: 1.5,
+              marginTop: '0.5rem'
+            }}
+          />
+        </div>
+        <div
+          style={{
+            marginTop: '2rem',
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+        >
+          <div
+            className={css`
+              font-weight: bold;
+              font-size: 1.6rem;
+              color: ${Color.darkerGray()};
+              display: flex;
+              align-items: center;
+            `}
+          >
+            AI Cards
           </div>
+          <Button
+            skeuomorphic
+            style={{
+              fontSize: '3.5rem',
+              padding: '1.5rem',
+              marginTop: '0.5rem'
+            }}
+            color="blue"
+            onClick={() => console.log('clicked')}
+          >
+            <Icon icon="cards-blank" />
+          </Button>
         </div>
       </div>
     </div>
