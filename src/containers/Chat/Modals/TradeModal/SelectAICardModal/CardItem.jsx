e8ac1550e3c8503b0e77b0e829113858602e222d
mikey1384 <mikey1384@gmail.com>
2023-01-31 20:22:29 +0700

    minor fix

--- src/containers/Chat/Modals/TradeModal/SelectAICardModal/CardItem.jsx
+++ src/containers/Chat/Modals/TradeModal/SelectAICardModal/CardItem.jsx
@@ -5,35 +5,19 @@ import {
   cardLevelHash,
   cloudFrontURL,
   cardProps,
-  returnCardBurnXP,
   qualityProps
 } from '~/constants/defaultValues';
 import { css } from '@emotion/css';
-import { useKeyContext } from '~/contexts';
 
 CardItem.propTypes = {
   card: PropTypes.object.isRequired
 };
 
 export default function CardItem({ card }) {
-  const {
-    xpNumber: { color: xpNumberColor }
-  } = useKeyContext((v) => v.theme);
   const cardDetailObj = useMemo(
     () => cardLevelHash[card?.level],
     [card?.level]
   );
-  const burnXP = useMemo(() => {
-    return returnCardBurnXP({
-      cardLevel: card?.level,
-      cardQuality: card?.quality
-    });
-  }, [card?.level, card?.quality]);
-  const displayedBurnXP = useMemo(() => {
-    if (burnXP < 1000) return burnXP;
-    if (burnXP < 1000000) return `${(burnXP / 1000).toFixed(1)}K`;
-    return `${(burnXP / 1000000).toFixed(1)}M`;
-  }, [burnXP]);
   const cardColor = useMemo(
     () => Color[card.isBurned ? 'black' : cardDetailObj?.color](),
     [card.isBurned, cardDetailObj?.color]
@@ -72,20 +56,6 @@ export default function CardItem({ card }) {
           style={{ width: '100%' }}
           src={`${cloudFrontURL}${card.imagePath}`}
         />
-      )}
-      {!!card.isBurned && (
-        <div
-          className={css`
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            padding: 0.5rem 0;
-            font-size: 0.7rem;
-          `}
-        >
-          <b style={{ color: Color[xpNumberColor]() }}>{displayedBurnXP}</b>
-          <b style={{ color: Color.gold(), marginLeft: '2px' }}>XP</b>
-        </div>
       )}
     </div>
   );
