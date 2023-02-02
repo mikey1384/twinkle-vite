c2293b01259bf77857e3a79c0f5f300d80ba2034
mikey1384 <mikey1384@gmail.com>
2023-02-02 19:24:54 +0700

    more updates

--- src/containers/Chat/Modals/TradeModal/index.jsx
+++ src/containers/Chat/Modals/TradeModal/index.jsx
@@ -6,6 +6,7 @@ import MyWant from './MyWant';
 import MyOffer from './MyOffer';
 import Options from './Options';
 import SelectAICardModal from './SelectAICardModal';
+import ErrorBoundary from '~/components/ErrorBoundary';
 import { useKeyContext } from '~/contexts';
 
 TradeModal.propTypes = {
@@ -32,69 +33,71 @@ export default function TradeModal({
   });
 
   return (
-    <Modal onHide={isAICardModalShown ? null : onHide}>
-      <header>Trade</header>
-      <main>
-        <div
-          style={{
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            paddingBottom: !!selectedOption ? 0 : '2rem'
-          }}
-        >
-          <Options
-            onSelectOption={setSelectedOption}
-            partnerName={partner?.username}
-            selectedOption={selectedOption}
-          />
-          {selectedOption === 'want' ? (
-            <MyWant
-              style={{ marginTop: '3rem' }}
-              selectedCardIds={selectedCardIdsObj.want}
-              onShowAICard={() => setAICardModalType('want')}
-            />
-          ) : null}
-          {!!selectedOption && (
-            <MyOffer
-              selectedCardIds={selectedCardIdsObj.offer}
+    <ErrorBoundary componentPath="Chat/Modals/TradeModal">
+      <Modal onHide={isAICardModalShown ? null : onHide}>
+        <header>Trade</header>
+        <main>
+          <div
+            style={{
+              width: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              paddingBottom: !!selectedOption ? 0 : '2rem'
+            }}
+          >
+            <Options
+              onSelectOption={setSelectedOption}
+              partnerName={partner?.username}
               selectedOption={selectedOption}
-              style={{ marginTop: '3rem' }}
-              onShowAICard={() => setAICardModalType('offer')}
             />
-          )}
-        </div>
-      </main>
-      <footer>
-        <Button
-          transparent
-          style={{ marginRight: '0.7rem' }}
-          onClick={isAICardModalShown ? null : onHide}
-        >
-          Cancel
-        </Button>
-        <Button disabled={true} color={doneColor} onClick={onHide}>
-          Propose
-        </Button>
-      </footer>
-      {!!aiCardModalType && (
-        <SelectAICardModal
-          aiCardModalType={aiCardModalType}
-          partnerName={partner?.username}
-          onSetAICardModalCardId={onSetAICardModalCardId}
-          onSelectDone={(cardIds) =>
-            setSelectedCardIdsObj((prevCardIdsObj) => {
-              return {
-                ...prevCardIdsObj,
-                [aiCardModalType]: cardIds
-              };
-            })
-          }
-          onHide={isAICardModalShown ? null : () => setAICardModalType(null)}
-        />
-      )}
-    </Modal>
+            {selectedOption === 'want' ? (
+              <MyWant
+                style={{ marginTop: '3rem' }}
+                selectedCardIds={selectedCardIdsObj.want}
+                onShowAICard={() => setAICardModalType('want')}
+              />
+            ) : null}
+            {!!selectedOption && (
+              <MyOffer
+                selectedCardIds={selectedCardIdsObj.offer}
+                selectedOption={selectedOption}
+                style={{ marginTop: '3rem' }}
+                onShowAICard={() => setAICardModalType('offer')}
+              />
+            )}
+          </div>
+        </main>
+        <footer>
+          <Button
+            transparent
+            style={{ marginRight: '0.7rem' }}
+            onClick={isAICardModalShown ? null : onHide}
+          >
+            Cancel
+          </Button>
+          <Button disabled={true} color={doneColor} onClick={onHide}>
+            Propose
+          </Button>
+        </footer>
+        {!!aiCardModalType && (
+          <SelectAICardModal
+            aiCardModalType={aiCardModalType}
+            partnerName={partner?.username}
+            onSetAICardModalCardId={onSetAICardModalCardId}
+            onSelectDone={(cardIds) =>
+              setSelectedCardIdsObj((prevCardIdsObj) => {
+                return {
+                  ...prevCardIdsObj,
+                  [aiCardModalType]: cardIds
+                };
+              })
+            }
+            onHide={isAICardModalShown ? null : () => setAICardModalType(null)}
+          />
+        )}
+      </Modal>
+    </ErrorBoundary>
   );
 }
