e3949baafe98ab15750ef37c2114c476932cb357
mikey1384 <mikey1384@gmail.com>
2023-02-02 19:24:54 +0700

    more updates

---
+++ src/containers/Chat/Modals/TradeModal/SelectedCards.jsx
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types';
+
+SelectedCards.propTypes = {
+  selectedCards: PropTypes.array.isRequired
+};
+export default function SelectedCards({ selectedCards }) {
+  return (
+    <div>
+      <div>{selectedCards.length}</div>;
+    </div>
+  );
+}
