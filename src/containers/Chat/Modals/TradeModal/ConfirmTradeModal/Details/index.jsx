246f16df899db3d5edca4b14e41287e991772c42
mikey1384 <mikey1384@gmail.com>
2023-02-03 20:10:33 +0700

    more updates

--- src/containers/Chat/Modals/TradeModal/ConfirmTradeModal/Details/index.jsx
+++ src/containers/Chat/Modals/TradeModal/ConfirmTradeModal/Details/index.jsx
@@ -3,13 +3,15 @@ import OfferDetail from './OfferDetail';
 import WantDetail from './WantDetail';
 
 Details.propTypes = {
-  selectedOption: PropTypes.string.isRequired
+  selectedOption: PropTypes.string.isRequired,
+  cardsOffered: PropTypes.array.isRequired,
+  coinOffered: PropTypes.number.isRequired
 };
 
-export default function Details({ selectedOption }) {
+export default function Details({ selectedOption, cardsOffered, coinOffered }) {
   return (
     <div>
-      <OfferDetail />
+      <OfferDetail cards={cardsOffered} coins={coinOffered} />
       {selectedOption === 'want' && <WantDetail />}
     </div>
   );
