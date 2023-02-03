c51b869aebbeaa825712631c1630f399dac53da7
mikey1384 <mikey1384@gmail.com>
2023-02-03 20:27:55 +0700

    more updates

--- src/containers/Chat/Modals/TradeModal/ConfirmTradeModal/Details/OfferDetail.jsx
+++ src/containers/Chat/Modals/TradeModal/ConfirmTradeModal/Details/OfferDetail.jsx
@@ -1,7 +1,16 @@
-export default function OfferDetail() {
+import PropTypes from 'prop-types';
+
+OfferDetail.propTypes = {
+  cards: PropTypes.array.isRequired,
+  coins: PropTypes.number.isRequired
+};
+
+export default function OfferDetail({ cards, coins }) {
   return (
     <div>
       <div>these are the details of the offer</div>
+      <div>{cards.length} cards</div>
+      <div>{coins} coins</div>
     </div>
   );
 }
