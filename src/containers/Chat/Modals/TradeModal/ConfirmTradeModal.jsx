dca7e136f6536e1768f81f953244635478b95814
mikey1384 <mikey1384@gmail.com>
2023-02-03 17:42:49 +0700

    more updates

--- src/containers/Chat/Modals/TradeModal/ConfirmTradeModal.jsx
+++ src/containers/Chat/Modals/TradeModal/ConfirmTradeModal.jsx
@@ -5,52 +5,29 @@ import Button from '~/components/Button';
 import localize from '~/constants/localize';
 import { useKeyContext } from '~/contexts';
 
-const areYouSureLabel = localize('areYouSure');
 const cancelLabel = localize('cancel');
 const confirmLabel = localize('confirm');
 
 ConfirmTradeModal.propTypes = {
-  description: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
-  disabled: PropTypes.bool,
-  descriptionFontSize: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number
-  ]),
-  modalOverModal: PropTypes.bool,
   onHide: PropTypes.func.isRequired,
-  title: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
   onConfirm: PropTypes.func.isRequired
 };
 
-export default function ConfirmTradeModal({
-  disabled = false,
-  description = areYouSureLabel,
-  descriptionFontSize = '2.5rem',
-  modalOverModal,
-  onHide,
-  title,
-  onConfirm
-}) {
+export default function ConfirmTradeModal({ onHide, onConfirm }) {
   const {
     done: { color: doneColor }
   } = useKeyContext((v) => v.theme);
+
   const [submitting, setSubmitting] = useState(false);
   return (
-    <Modal modalOverModal={modalOverModal} onHide={onHide}>
-      <header>{title}</header>
-      <main style={{ fontSize: descriptionFontSize, paddingTop: 0 }}>
-        {description}
-      </main>
+    <Modal modalOverModal onHide={onHide}>
+      <header>this is the title</header>
+      <main>this is to confirm the trade</main>
       <footer>
         <Button transparent style={{ marginRight: '0.7rem' }} onClick={onHide}>
           {cancelLabel}
         </Button>
-        <Button
-          loading={submitting}
-          disabled={disabled}
-          color={doneColor}
-          onClick={handleConfirm}
-        >
+        <Button loading={submitting} color={doneColor} onClick={handleConfirm}>
           {confirmLabel}
         </Button>
       </footer>
