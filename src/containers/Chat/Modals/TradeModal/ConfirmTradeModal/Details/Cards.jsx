02c521258cf1c8799def637f5f950cb6618d5a5c
mikey1384 <mikey1384@gmail.com>
2023-02-04 09:36:16 +0700

    more updates

--- src/containers/Chat/Modals/TradeModal/ConfirmTradeModal/Details/Cards.jsx
+++ src/containers/Chat/Modals/TradeModal/ConfirmTradeModal/Details/Cards.jsx
@@ -1,5 +1,9 @@
+import { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import CardThumb from '../../CardThumb';
+import { isMobile } from '~/helpers';
+
+const deviceIsMobile = isMobile(navigator);
 
 Cards.propTypes = {
   cards: PropTypes.array.isRequired,
@@ -7,9 +11,17 @@ Cards.propTypes = {
 };
 
 export default function Cards({ cards, onSetAICardModalCardId }) {
+  const displayedCards = useMemo(() => {
+    const numShown = deviceIsMobile ? 3 : 5;
+    if (cards.length <= numShown) {
+      return cards;
+    }
+    return cards.slice(0, numShown);
+  }, [cards]);
+
   return (
     <div style={{ display: 'flex' }}>
-      {cards.map((card, index) => (
+      {displayedCards.map((card, index) => (
         <div key={card.id} style={{ position: 'relative' }}>
           <CardThumb
             card={card}
