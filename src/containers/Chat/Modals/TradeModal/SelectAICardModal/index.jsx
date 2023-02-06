c039b7326b0235951e038158f16c7121e9e42cf7
mikey1384 <mikey1384@gmail.com>
2023-02-06 09:26:34 +0700

    minor fix

--- src/containers/Chat/Modals/TradeModal/SelectAICardModal/index.jsx
+++ src/containers/Chat/Modals/TradeModal/SelectAICardModal/index.jsx
@@ -73,6 +73,10 @@ export default function SelectAICardModal({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useEffect(() => {
+    console.log(filters?.color, filters?.quality);
+  }, [filters?.color, filters?.quality]);
+
   const headerLabel = useMemo(() => {
     if (aiCardModalType === 'want') {
       return `${partner.username}'s AI Cards`;
