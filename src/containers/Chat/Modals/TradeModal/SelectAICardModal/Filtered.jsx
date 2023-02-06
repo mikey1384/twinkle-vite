70751c7fc3334e47c0fb46d96e0c0fac8d61d09f
mikey1384 <mikey1384@gmail.com>
2023-02-06 10:19:46 +0700

    more updates

--- src/containers/Chat/Modals/TradeModal/SelectAICardModal/Filtered.jsx
+++ src/containers/Chat/Modals/TradeModal/SelectAICardModal/Filtered.jsx
@@ -1,7 +1,19 @@
-export default function Filtered() {
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+Filtered.propTypes = {
+  color: PropTypes.string,
+  quality: PropTypes.string
+};
+
+export default function Filtered({ color, quality }) {
+  useEffect(() => {
+    console.log(color, quality);
+  }, [color, quality]);
+
   return (
     <div>
-      <div>this isfiltered</div>
+      <div>this is filtered</div>
     </div>
   );
 }
