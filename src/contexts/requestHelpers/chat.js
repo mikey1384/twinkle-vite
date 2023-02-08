25f266c4e8076980fbbc952c12231f5e26a3e38d
mikey1384 <mikey1384@gmail.com>
2023-02-08 10:14:06 +0700

    more updates

--- src/contexts/requestHelpers/chat.js
+++ src/contexts/requestHelpers/chat.js
@@ -771,6 +771,18 @@ export default function chatRequestHelpers({ auth, handleError }) {
         return handleError(error);
       }
     },
+    async postTradeRequest({ cardId, cardName, cardImageUrl }) {
+      try {
+        const { data } = await request.post(
+          `${URL}/chat/trade`,
+          { cardId, cardName, cardImageUrl },
+          auth()
+        );
+        return Promise.resolve(data);
+      } catch (error) {
+        return handleError(error);
+      }
+    },
     async putFavoriteChannel(channelId) {
       try {
         const {
