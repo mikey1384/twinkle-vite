ba2edd87b11fda8c7bf1b527527445c3a3d972eb
mikey1384 <mikey1384@gmail.com>
2023-02-10 20:33:44 +0700

    more updates

--- src/containers/Chat/Message/TransactionMessage/Trade.jsx
+++ src/containers/Chat/Message/TransactionMessage/Trade.jsx
@@ -1,23 +1,58 @@
+import { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import AICardsPreview from '~/components/AICardsPreview';
+import UsernameText from '~/components/Texts/UsernameText';
+import { Color } from '~/constants/css';
 
 Trade.propTypes = {
-  partnerId: PropTypes.number.isRequired,
+  myId: PropTypes.number.isRequired,
+  myUsername: PropTypes.string.isRequired,
+  partner: PropTypes.object.isRequired,
   onSetAICardModalCardId: PropTypes.func.isRequired,
-  wantCardIds: PropTypes.array.isRequired
+  wantCardIds: PropTypes.array.isRequired,
+  fromId: PropTypes.number.isRequired,
+  toId: PropTypes.number.isRequired
 };
 
 export default function Trade({
+  myId,
+  myUsername,
+  partner,
+  onSetAICardModalCardId,
   wantCardIds,
-  partnerId,
-  onSetAICardModalCardId
+  fromId,
+  toId
 }) {
+  const from = useMemo(() => {
+    return fromId === myId ? { id: myId, username: myUsername } : partner;
+  }, [fromId, myId, myUsername, partner]);
+  const to = useMemo(() => {
+    return toId === myId ? { id: myId, username: myUsername } : partner;
+  }, [myId, myUsername, partner, toId]);
+
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
+      from:
+      <UsernameText
+        displayedName={from.id === myId ? 'You' : from.username}
+        color={Color.black()}
+        user={{
+          id: from.id,
+          username: from.username
+        }}
+      />
+      to:
+      <UsernameText
+        displayedName={to.id === myId ? 'You' : to.username}
+        color={Color.black()}
+        user={{
+          id: to.id,
+          username: to.username
+        }}
+      />
       <AICardsPreview
         cardIds={wantCardIds}
         onSetAICardModalCardId={onSetAICardModalCardId}
-        partnerId={partnerId}
       />
     </div>
   );
