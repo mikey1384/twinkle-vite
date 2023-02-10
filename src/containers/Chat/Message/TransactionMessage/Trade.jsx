c880ec2957c9973f056d02446675702c9cf982f0
mikey1384 <mikey1384@gmail.com>
2023-02-10 09:21:16 +0700

    more updates

--- src/containers/Chat/Message/TransactionMessage/Trade.jsx
+++ src/containers/Chat/Message/TransactionMessage/Trade.jsx
@@ -1,7 +1,13 @@
-export default function Trade() {
+import PropTypes from 'prop-types';
+
+Trade.propTypes = {
+  wantCards: PropTypes.array.isRequired
+};
+
+export default function Trade({ wantCards }) {
   return (
     <div>
-      <div>Trade</div>
+      <div>{wantCards.length}</div>
     </div>
   );
 }
