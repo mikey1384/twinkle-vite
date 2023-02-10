d94ba300803424d77f2b6679b8622a8d180ae802
mikey1384 <mikey1384@gmail.com>
2023-02-10 08:55:55 +0700

    more updates

--- src/containers/Chat/Message/TransactionMessage.jsx
+++ src/containers/Chat/Message/TransactionMessage.jsx
@@ -5,11 +5,16 @@ TransactionMessage.propTypes = {
 };
 
 export default function TransactionMessage({ transaction }) {
+  const { type, want = {}, offer = {} } = transaction;
+  const { cardIds: wantCardIds = [], coins: wantCoins = 0 } = want || {};
+  const { cardIds: offerCardIds = [], coins: offerCoins = 0 } = offer || {};
+
   return (
     <div>
       <div>
-        this is for transaction!!
-        {(transaction?.id, transaction.offer.coins)}
+        this is for {type}!!
+        {(wantCardIds?.length, wantCoins)}
+        {(offerCardIds?.length, offerCoins)}
       </div>
     </div>
   );
