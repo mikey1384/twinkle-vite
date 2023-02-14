5f57680dfb0a2580d73dd7c3470150a5b40683c0
mikey1384 <mikey1384@gmail.com>
2023-02-14 17:30:39 +0700

    more updates

--- src/containers/Chat/Modals/TransactionModal/TransactionHandler.jsx
+++ src/containers/Chat/Modals/TransactionModal/TransactionHandler.jsx
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
 import TransactionDetails from '../../TransactionDetails';
+import Button from '~/components/Button';
+import Icon from '~/components/Icon';
 
 TransactionHandler.propTypes = {
   onSetAICardModalCardId: PropTypes.func.isRequired,
@@ -13,13 +15,27 @@ export default function TransactionHandler({
   transactionDetails
 }) {
   return (
-    <div style={{ width: '100%' }}>
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
       <TransactionDetails
         partner={partner}
         onSetAICardModalCardId={onSetAICardModalCardId}
         transaction={transactionDetails}
-        style={{ marginTop: '-1rem' }}
+        style={{ marginTop: '-1rem', width: '100%' }}
       />
+      <div>
+        <Button onClick={() => console.log('clicked')} color="orange" filled>
+          <Icon icon="redo" />
+          <span style={{ marginLeft: '0.7rem' }}>Withdraw Offer</span>
+        </Button>
+      </div>
     </div>
   );
 }
