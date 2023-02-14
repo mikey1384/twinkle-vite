143fa3aefbe21e501d47f1b7a1deebb6d06e0c6b
mikey1384 <mikey1384@gmail.com>
2023-02-14 18:39:31 +0700

    more updates

--- src/containers/Chat/Modals/TransactionModal/TransactionHandler.jsx
+++ src/containers/Chat/Modals/TransactionModal/TransactionHandler.jsx
@@ -5,15 +5,18 @@ import Icon from '~/components/Icon';
 
 TransactionHandler.propTypes = {
   onSetAICardModalCardId: PropTypes.func.isRequired,
+  myId: PropTypes.number.isRequired,
   partner: PropTypes.object.isRequired,
   transactionDetails: PropTypes.object.isRequired
 };
 
 export default function TransactionHandler({
   onSetAICardModalCardId,
+  myId,
   partner,
   transactionDetails
 }) {
+  const isFromMe = transactionDetails.from === myId;
   return (
     <div
       style={{
@@ -30,12 +33,30 @@ export default function TransactionHandler({
         transaction={transactionDetails}
         style={{ marginTop: '-1rem', width: '100%' }}
       />
-      <div>
-        <Button onClick={() => console.log('clicked')} color="orange" filled>
-          <Icon icon="redo" />
-          <span style={{ marginLeft: '0.7rem' }}>Withdraw Proposal</span>
-        </Button>
-      </div>
+      {isFromMe ? (
+        <div>
+          <Button onClick={() => console.log('clicked')} color="orange" filled>
+            <Icon icon="redo" />
+            <span style={{ marginLeft: '0.7rem' }}>Withdraw Proposal</span>
+          </Button>
+        </div>
+      ) : (
+        <div style={{ display: 'flex' }}>
+          <Button onClick={() => console.log('clicked')} color="rose" filled>
+            <Icon icon="xmark" />
+            <span style={{ marginLeft: '0.7rem' }}>Decline</span>
+          </Button>
+          <Button
+            style={{ marginLeft: '2rem' }}
+            onClick={() => console.log('clicked')}
+            color="green"
+            filled
+          >
+            <Icon icon="check" />
+            <span style={{ marginLeft: '0.7rem' }}>Accept</span>
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
