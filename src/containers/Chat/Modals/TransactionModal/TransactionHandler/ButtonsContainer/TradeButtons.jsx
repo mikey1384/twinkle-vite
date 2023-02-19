23b43078cf76524e1471e5a572a74b1a27a549ec
mikey1384 <mikey1384@gmail.com>
2023-02-19 08:32:31 +0700

    more updates

---
+++ src/containers/Chat/Modals/TransactionModal/TransactionHandler/ButtonsContainer/TradeButtons.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import Button from '~/components/Button';
+import Icon from '~/components/Icon';
+
+TradeButtons.propTypes = {
+  onWithdrawTransaction: PropTypes.func.isRequired
+};
+
+export default function TradeButtons({ onWithdrawTransaction }) {
+  return (
+    <div style={{ marginTop: '0.5rem', display: 'flex' }}>
+      <Button
+        onClick={() => onWithdrawTransaction('decline')}
+        color="rose"
+        filled
+      >
+        <Icon icon="xmark" />
+        <span style={{ marginLeft: '0.7rem' }}>Decline</span>
+      </Button>
+      <Button
+        style={{ marginLeft: '2rem' }}
+        onClick={() => console.log('clicked')}
+        color="green"
+        filled
+      >
+        <Icon icon="check" />
+        <span style={{ marginLeft: '0.7rem' }}>Accept</span>
+      </Button>
+    </div>
+  );
+}
