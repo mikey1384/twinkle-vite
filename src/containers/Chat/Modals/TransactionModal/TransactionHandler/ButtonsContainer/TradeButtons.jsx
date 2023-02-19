e1c764ff0a2efbb9b656b17175ae5821997dc5fa
mikey1384 <mikey1384@gmail.com>
2023-02-19 08:37:44 +0700

    more updates

--- src/containers/Chat/Modals/TransactionModal/TransactionHandler/ButtonsContainer/TradeButtons.jsx
+++ src/containers/Chat/Modals/TransactionModal/TransactionHandler/ButtonsContainer/TradeButtons.jsx
@@ -8,24 +8,28 @@ TradeButtons.propTypes = {
 
 export default function TradeButtons({ onWithdrawTransaction }) {
   return (
-    <div style={{ marginTop: '0.5rem', display: 'flex' }}>
-      <Button
-        onClick={() => onWithdrawTransaction('decline')}
-        color="rose"
-        filled
-      >
-        <Icon icon="xmark" />
-        <span style={{ marginLeft: '0.7rem' }}>Decline</span>
-      </Button>
-      <Button
-        style={{ marginLeft: '2rem' }}
-        onClick={() => console.log('clicked')}
-        color="green"
-        filled
-      >
-        <Icon icon="check" />
-        <span style={{ marginLeft: '0.7rem' }}>Accept</span>
-      </Button>
+    <div
+      style={{ marginTop: '0.5rem', display: 'flex', flexDirection: 'column' }}
+    >
+      <div style={{ display: 'flex' }}>
+        <Button
+          onClick={() => onWithdrawTransaction('decline')}
+          color="rose"
+          filled
+        >
+          <Icon icon="xmark" />
+          <span style={{ marginLeft: '0.7rem' }}>Decline</span>
+        </Button>
+        <Button
+          style={{ marginLeft: '1.5rem' }}
+          onClick={() => console.log('clicked')}
+          color="green"
+          filled
+        >
+          <Icon icon="check" />
+          <span style={{ marginLeft: '0.7rem' }}>Accept</span>
+        </Button>
+      </div>
     </div>
   );
 }
