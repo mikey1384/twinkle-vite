8cc0d4051d6f5215aab3bb331eb61cedd1f8b2e2
mikey1384 <mikey1384@gmail.com>
2023-02-19 16:37:46 +0700

    minor fix

--- src/containers/Chat/Body/MessagesContainer/AddButtons.jsx
+++ src/containers/Chat/Body/MessagesContainer/AddButtons.jsx
@@ -1,30 +1,51 @@
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Button from '~/components/Button';
 import Icon from '~/components/Icon';
-import { useKeyContext } from '~/contexts';
+import { useAppContext, useKeyContext } from '~/contexts';
 
 AddButtons.propTypes = {
+  channelId: PropTypes.number.isRequired,
   currentTransactionId: PropTypes.number,
   disabled: PropTypes.bool,
   isTwoPeopleChannel: PropTypes.bool,
+  myId: PropTypes.number.isRequired,
   onUploadButtonClick: PropTypes.func.isRequired,
   onSelectVideoButtonClick: PropTypes.func.isRequired,
   onSetTransactionModalShown: PropTypes.func.isRequired
 };
 
 export default function AddButtons({
+  channelId,
   currentTransactionId,
   disabled,
   isTwoPeopleChannel,
+  myId,
   onUploadButtonClick,
   onSelectVideoButtonClick,
   onSetTransactionModalShown
 }) {
+  const [transactionButtonIsGlowing, setTransactionButtonIsGlowing] =
+    useState(false);
   const {
     alert: { color: alertColor },
     button: { color: buttonColor },
     buttonHovered: { color: buttonHoverColor }
   } = useKeyContext((v) => v.theme);
+  const loadPendingTransaction = useAppContext(
+    (v) => v.requestHelpers.loadPendingTransaction
+  );
+
+  useEffect(() => {
+    if (isTwoPeopleChannel) {
+      init();
+    }
+    async function init() {
+      const { transaction } = await loadPendingTransaction(channelId);
+      setTransactionButtonIsGlowing(transaction && transaction.from !== myId);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [channelId, currentTransactionId]);
 
   return (
     <div
@@ -37,12 +58,12 @@ export default function AddButtons({
       {isTwoPeopleChannel && (
         <Button
           skeuomorphic
-          filled={!!currentTransactionId}
+          filled={transactionButtonIsGlowing}
           disabled={disabled}
           onClick={onSetTransactionModalShown}
-          color={!!currentTransactionId ? alertColor : buttonColor}
+          color={transactionButtonIsGlowing ? alertColor : buttonColor}
           mobilePadding="0.5rem"
-          hoverColor={!!currentTransactionId ? alertColor : buttonColor}
+          hoverColor={transactionButtonIsGlowing ? alertColor : buttonColor}
         >
           <Icon size="lg" icon={['far', 'badge-dollar']} />
         </Button>
