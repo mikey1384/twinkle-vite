d8e512ed31f32df68eca3e25c1436e88b815d926
mikey1384 <mikey1384@gmail.com>
2023-03-01 06:15:54 +0700

    minor fix

--- src/components/AICardsPreview/index.jsx
+++ src/components/AICardsPreview/index.jsx
@@ -63,6 +63,7 @@ export default function AICardsPreview({
         )}
         {moreAICardsModalShown && (
           <MoreAICardsModal
+            modalOverModal
             cards={cards}
             onSetAICardModalCardId={onSetAICardModalCardId}
             onHide={() =>
