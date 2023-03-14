e51cf11367738c6c06e0002e945bae4da3e655f2
mikey1384 <mikey1384@gmail.com>
2023-03-14 16:04:04 +0700

    more updates

--- src/components/Buttons/ZeroButton/ZeroModal/Greeting.jsx
+++ src/components/Buttons/ZeroButton/ZeroModal/Greeting.jsx
@@ -1,27 +1,40 @@
 import { ZERO_TWINKLE_ID } from '~/constants/defaultValues';
 import ZeroPic from './ZeroPic';
 import UsernameText from '~/components/Texts/UsernameText';
+import { useKeyContext } from '~/contexts';
 
 export default function Greeting() {
+  const { username } = useKeyContext((v) => v.myState);
   return (
     <div>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          flexDirection: 'column'
-        }}
-      >
-        <div style={{ width: '7rem' }}>
-          <ZeroPic />
-        </div>
-        <UsernameText
-          user={{
-            username: 'Zero',
-            id: ZERO_TWINKLE_ID
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column'
           }}
-        />
+        >
+          <div style={{ width: '7rem' }}>
+            <ZeroPic />
+          </div>
+          <UsernameText
+            user={{
+              username: 'Zero',
+              id: ZERO_TWINKLE_ID
+            }}
+          />
+        </div>
+        <div
+          style={{
+            marginLeft: '3rem',
+            fontSize: '1.7rem',
+            fontFamily: 'Roboto Mono, monospace'
+          }}
+        >
+          Hello, {username}! How can I help you?
+        </div>
       </div>
     </div>
   );
