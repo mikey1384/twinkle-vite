af531be2bf16f8a5ba32ffce2949c93093b32d31
mikey1384 <mikey1384@gmail.com>
2023-03-14 15:38:33 +0700

    more updates

--- src/components/Buttons/ZeroButton/ZeroModal/Greeting.jsx
+++ src/components/Buttons/ZeroButton/ZeroModal/Greeting.jsx
@@ -1,8 +1,10 @@
 import { ZERO_TWINKLE_ID } from '~/constants/defaultValues';
+import zeroFull from './zero-full.png';
 
 export default function Greeting() {
   return (
     <div>
+      <img src={zeroFull} alt="Zero" />
       <div>{ZERO_TWINKLE_ID}</div>
     </div>
   );
