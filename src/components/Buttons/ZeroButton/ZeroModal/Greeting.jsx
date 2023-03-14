1e76947a5f34151146835e57aa16d541cad5ca02
mikey1384 <mikey1384@gmail.com>
2023-03-14 15:52:13 +0700

    more updates

--- src/components/Buttons/ZeroButton/ZeroModal/Greeting.jsx
+++ src/components/Buttons/ZeroButton/ZeroModal/Greeting.jsx
@@ -1,11 +1,28 @@
 import { ZERO_TWINKLE_ID } from '~/constants/defaultValues';
 import ZeroPic from './ZeroPic';
+import UsernameText from '~/components/Texts/UsernameText';
 
 export default function Greeting() {
   return (
     <div>
-      <div>{ZERO_TWINKLE_ID}</div>
-      <ZeroPic />
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column'
+        }}
+      >
+        <div style={{ width: '7rem' }}>
+          <ZeroPic />
+        </div>
+        <UsernameText
+          user={{
+            username: 'Zero',
+            id: ZERO_TWINKLE_ID
+          }}
+        />
+      </div>
     </div>
   );
 }
