bbe3030e52600ddeea024f4e919e963913f970ab
mikey1384 <mikey1384@gmail.com>
2023-03-15 07:28:15 +0700

    more updates

--- src/components/Buttons/ZeroButton/ZeroModal/Menu.jsx
+++ src/components/Buttons/ZeroButton/ZeroModal/Menu.jsx
@@ -1,25 +1,33 @@
 import Button from '~/components/Button';
+import { useAppContext } from '~/contexts';
 
 export default function Menu() {
+  const getZerosReview = useAppContext((v) => v.requestHelpers.getZerosReview);
   return (
     <div>
-      <Button skeuomorphic onClick={() => console.log('clicked')}>
+      <Button skeuomorphic onClick={() => handleButtonClick('easier')}>
         Make it easier to understand
       </Button>
       <Button
         skeuomorphic
         style={{ marginTop: '1rem' }}
-        onClick={() => console.log('clicked')}
+        onClick={() => handleButtonClick('natural')}
       >
         Make it sound more natural
       </Button>
       <Button
         skeuomorphic
         style={{ marginTop: '1rem' }}
-        onClick={() => console.log('clicked')}
+        onClick={() => handleButtonClick('grammar')}
       >
         Check grammar
       </Button>
     </div>
   );
+
+  async function handleButtonClick(type) {
+    console.log(type);
+    const data = await getZerosReview(type);
+    console.log(data);
+  }
 }
