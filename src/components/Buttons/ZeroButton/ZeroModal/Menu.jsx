3e0e5f1c61e8b232b5efbc14aad3ddabc859ad2d
mikey1384 <mikey1384@gmail.com>
2023-03-15 09:16:23 +0700

    add zero button

--- src/components/Buttons/ZeroButton/ZeroModal/Menu.jsx
+++ src/components/Buttons/ZeroButton/ZeroModal/Menu.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Button from '~/components/Button';
 import { useAppContext } from '~/contexts';
 
 Menu.propTypes = {
   content: PropTypes.string.isRequired,
+  loadingType: PropTypes.string,
+  onSetLoadingType: PropTypes.func.isRequired,
+  onSetResponse: PropTypes.func.isRequired,
   style: PropTypes.object
 };
 
-export default function Menu({ content, style }) {
+export default function Menu({
+  content,
+  style,
+  loadingType,
+  onSetLoadingType,
+  onSetResponse
+}) {
   const getZerosReview = useAppContext((v) => v.requestHelpers.getZerosReview);
-  const [loadingType, setLoadingType] = useState(null);
   return (
     <div style={style}>
       <Button
@@ -34,15 +41,15 @@ export default function Menu({ content, style }) {
         style={{ marginTop: '1rem' }}
         onClick={() => handleButtonClick('grammar')}
       >
-        Check grammar
+        Grammar (Remember, Zero is not always right)
       </Button>
     </div>
   );
 
   async function handleButtonClick(type) {
-    setLoadingType(type);
-    const data = await getZerosReview({ type, content });
-    console.log(data);
-    setLoadingType(null);
+    onSetLoadingType(type);
+    const response = await getZerosReview({ type, content });
+    onSetLoadingType(null);
+    onSetResponse(response);
   }
 }
