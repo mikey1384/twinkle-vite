f2a4b14f63c9de7921fd19cae2f865483092934e
mikey1384 <mikey1384@gmail.com>
2023-03-25 20:49:12 +0700

    more updates

---
+++ src/containers/Home/AIStoriesModal/ContentContainer/Questions/index.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import Question from './Question';
+
+Questions.propTypes = {
+  questions: PropTypes.array.isRequired
+};
+
+export default function Questions({ questions }) {
+  const [userChoiceObj, setUserChoiceObj] = useState({});
+  return (
+    <div>
+      {questions.map((question, index) => (
+        <Question
+          key={question.id}
+          style={{ marginTop: index === 0 ? 0 : '3rem' }}
+          question={question.question}
+          choices={question.choices}
+          selectedChoiceIndex={userChoiceObj[question.id]}
+          onSelectChoice={(index) =>
+            setUserChoiceObj((obj) => ({
+              ...obj,
+              [question.id]: index
+            }))
+          }
+        />
+      ))}
+    </div>
+  );
+}
