e54b70f2e28c8586a6bff2aa79f969c3e685cdc4
mikey1384 <mikey1384@gmail.com>
2023-03-27 11:03:22 +0700

    more updates

--- src/containers/Home/AIStoriesModal/ContentContainer/SuccessModal/index.jsx
+++ src/containers/Home/AIStoriesModal/ContentContainer/SuccessModal/index.jsx
@@ -2,6 +2,8 @@ import PropTypes from 'prop-types';
 import Modal from '~/components/Modal';
 import Button from '~/components/Button';
 import SuccessText from './SuccessText';
+import { Color } from '~/constants/css';
+import { useKeyContext } from '~/contexts';
 import { addCommasToNumber } from '~/helpers/stringHelpers';
 
 SuccessModal.propTypes = {
@@ -17,6 +19,10 @@ export default function SuccessModal({
   numQuestions,
   rewardTable
 }) {
+  const {
+    xpNumber: { color: xpNumberColor }
+  } = useKeyContext((v) => v.theme);
+
   return (
     <Modal closeWhenClickedOutside={false} onHide={onHide}>
       <header>Reading Cleared</header>
@@ -26,9 +32,21 @@ export default function SuccessModal({
           You answered {numQuestions} out of {numQuestions} question
           {numQuestions === 1 ? '' : 's'} correctly!
         </div>
-        <div style={{ marginTop: '1rem', marginBottom: '3rem' }}>
-          You earned {addCommasToNumber(rewardTable[difficulty].xp)} XP and{' '}
-          {addCommasToNumber(rewardTable[difficulty].coins)} coins
+        <div
+          style={{
+            marginTop: '1rem',
+            marginBottom: '3rem',
+            fontSize: difficulty > 3 ? '1.7rem' : '1.5rem'
+          }}
+        >
+          You earned{' '}
+          <b style={{ color: Color[xpNumberColor]() }}>
+            {addCommasToNumber(rewardTable[difficulty].xp)}
+          </b>{' '}
+          <b style={{ color: Color.gold() }}>XP</b> and{' '}
+          <b style={{ color: Color.brownOrange() }}>
+            {addCommasToNumber(rewardTable[difficulty].coins)} coins
+          </b>
         </div>
       </main>
       <footer>
