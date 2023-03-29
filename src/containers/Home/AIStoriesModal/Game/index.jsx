b20e04a7edde2240de905d1659058117a3f2d503
mikey1384 <mikey1384@gmail.com>
2023-03-29 15:51:06 +0700

    minor fix

--- src/containers/Home/AIStoriesModal/Game/index.jsx
+++ src/containers/Home/AIStoriesModal/Game/index.jsx
@@ -67,7 +67,15 @@ export default function Game({
   const [questionsLoadError, setQuestionsLoadError] = useState(false);
 
   return (
-    <div style={{ width: '100%', height: '100%' }}>
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}
+    >
       {hasError ? (
         <div
           style={{
