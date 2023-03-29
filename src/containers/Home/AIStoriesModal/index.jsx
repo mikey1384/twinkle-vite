edc2d34a167ecd7a9cb63cbde69908ba89cffb7e
mikey1384 <mikey1384@gmail.com>
2023-03-29 08:44:57 +0700

    more refactoring

--- src/containers/Home/AIStoriesModal/index.jsx
+++ src/containers/Home/AIStoriesModal/index.jsx
@@ -2,50 +2,29 @@ import { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import Modal from '~/components/Modal';
 import { useAppContext } from '~/contexts';
-import Button from '~/components/Button';
-import ContentContainer from './ContentContainer';
 import FilterBar from '~/components/FilterBar';
-import DropdownButton from '~/components/Buttons/DropdownButton';
-import GradientButton from '~/components/Buttons/GradientButton';
+import Game from './Game';
 
 AIStoriesModal.propTypes = {
   onHide: PropTypes.func.isRequired
-};
-
-const levelHash = {
-  1: 'Level 1',
-  2: 'Level 2',
-  3: 'Level 3',
-  4: 'Level 4',
-  5: 'Level 5'
 };
 
 export default function AIStoriesModal({ onHide }) {
   const MainRef = useRef();
   const [resetNumber, setResetNumber] = useState(0);
   const [activeTab, setActiveTab] = useState('game');
-  const [hasError, setHasError] = useState(false);
-  const [questionsLoadError, setQuestionsLoadError] = useState(false);
-  const [loading, setLoading] = useState(true);
   const [attemptId, setAttemptId] = useState(null);
   const loadedDifficulty = localStorage.getItem('story-difficulty');
   const [difficulty, setDifficulty] = useState(loadedDifficulty || 3);
-  const [loadComplete, setLoadComplete] = useState(false);
+  const [loadStoryComplete, onSetLoadStoryComplete] = useState(false);
   const [dropdownShown, setDropdownShown] = useState(false);
   const [generateButtonPressed, setGenerateButtonPressed] = useState(false);
   const loadAIStoryTopic = useAppContext(
     (v) => v.requestHelpers.loadAIStoryTopic
   );
-  const loadAIStoryQuestions = useAppContext(
-    (v) => v.requestHelpers.loadAIStoryQuestions
-  );
-  const loadAIStory = useAppContext((v) => v.requestHelpers.loadAIStory);
-  const [storyObj, setStoryObj] = useState({});
   const [topic, setTopic] = useState('');
   const [storyType, setStoryType] = useState('');
   const [loadingTopic, setLoadingTopic] = useState(false);
-  const [questions, setQuestions] = useState([]);
-  const [questionsLoaded, setQuestionsLoaded] = useState(false);
   const [topicLoadError, setTopicLoadError] = useState(false);
 
   useEffect(() => {
@@ -56,7 +35,9 @@ export default function AIStoriesModal({ onHide }) {
 
   return (
     <Modal
-      closeWhenClickedOutside={!dropdownShown && !loadComplete}
+      closeWhenClickedOutside={
+        !dropdownShown && (!loadStoryComplete || activeTab === 'rankings')
+      }
       modalStyle={{
         height: '80vh'
       }}
@@ -95,151 +76,31 @@ export default function AIStoriesModal({ onHide }) {
         }}
         ref={MainRef}
       >
-        {hasError ? (
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              width: '100%',
-              height: '100%',
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}
-          >
-            <p style={{ fontWeight: 'bold', fontSize: '1.7rem' }}>
-              Oops, something went wrong. Try again
-            </p>
-            <GradientButton
-              style={{ marginTop: '5rem' }}
-              onClick={handleGenerateStory}
-            >
-              Retry
-            </GradientButton>
-          </div>
-        ) : generateButtonPressed ? (
-          <ContentContainer
+        {activeTab === 'game' && (
+          <Game
             attemptId={attemptId}
-            difficulty={Number(difficulty)}
-            loading={loading}
+            difficulty={difficulty}
             loadingTopic={loadingTopic}
-            loadComplete={loadComplete}
-            storyObj={storyObj}
-            questions={questions}
-            questionsLoadError={questionsLoadError}
-            onLoadQuestions={handleLoadQuestions}
-            onScrollToTop={() => (MainRef.current.scrollTop = 0)}
-            questionsLoaded={questionsLoaded}
-            onReset={handleReset}
+            generateButtonPressed={generateButtonPressed}
+            loadStoryComplete={loadStoryComplete}
+            onHide={onHide}
+            onLoadTopic={handleLoadTopic}
+            onSetAttemptId={setAttemptId}
+            onSetDropdownShown={setDropdownShown}
+            onSetResetNumber={setResetNumber}
+            onSetDifficulty={setDifficulty}
+            onSetGenerateButtonPressed={setGenerateButtonPressed}
+            onSetLoadStoryComplete={onSetLoadStoryComplete}
+            onSetTopicLoadError={setTopicLoadError}
+            MainRef={MainRef}
+            storyType={storyType}
+            topic={topic}
+            topicLoadError={topicLoadError}
           />
-        ) : (
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              width: '100%',
-              height: '100%',
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}
-          >
-            <DropdownButton
-              skeuomorphic
-              color="darkerGray"
-              icon="caret-down"
-              text={levelHash[difficulty]}
-              onDropdownShown={setDropdownShown}
-              menuProps={[
-                {
-                  label: levelHash[1],
-                  onClick: () => setDifficulty(1)
-                },
-                {
-                  label: levelHash[2],
-                  onClick: () => setDifficulty(2)
-                },
-                {
-                  label: levelHash[3],
-                  onClick: () => setDifficulty(3)
-                },
-                {
-                  label: levelHash[4],
-                  onClick: () => setDifficulty(4)
-                },
-                {
-                  label: levelHash[5],
-                  onClick: () => setDifficulty(5)
-                }
-              ]}
-            />
-            {topicLoadError ? (
-              <div
-                style={{
-                  marginTop: '5rem',
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'center'
-                }}
-              >
-                <p>There was an error initializing AI Story</p>
-                <GradientButton
-                  style={{ marginTop: '3rem' }}
-                  onClick={() => {
-                    setTopicLoadError(false);
-                    handleLoadTopic(difficulty);
-                  }}
-                >
-                  Retry
-                </GradientButton>
-              </div>
-            ) : (
-              <GradientButton
-                style={{ marginTop: '2rem' }}
-                onClick={handleGenerateStory}
-                loading={loadingTopic}
-              >
-                Generate a Story
-              </GradientButton>
-            )}
-          </div>
-        )}
-        {generateButtonPressed && (
-          <div
-            style={{
-              marginTop: hasError ? '3rem' : '15rem',
-              display: 'flex',
-              justifyContent: 'center'
-            }}
-          >
-            <Button transparent onClick={onHide}>
-              close
-            </Button>
-          </div>
         )}
       </main>
     </Modal>
   );
-
-  async function handleGenerateStory() {
-    setHasError(false);
-    setGenerateButtonPressed(true);
-    setLoading(true);
-    try {
-      const { attemptId: newAttemptId, storyObj } = await loadAIStory({
-        difficulty,
-        topic,
-        type: storyType
-      });
-      setAttemptId(newAttemptId);
-      setStoryObj(storyObj);
-      setLoadComplete(true);
-      await new Promise((resolve) => setTimeout(resolve, 100));
-    } catch (error) {
-      console.error(error);
-      setHasError(true);
-    } finally {
-      setLoading(false);
-    }
-  }
 
   async function handleLoadTopic(difficulty) {
     setLoadingTopic(true);
@@ -271,30 +132,5 @@ export default function AIStoriesModal({ onHide }) {
         return new Promise((resolve) => setTimeout(resolve, ms));
       }
     }
-  }
-
-  async function handleLoadQuestions() {
-    setQuestionsLoadError(false);
-    if (questionsLoaded) return;
-    try {
-      const questions = await loadAIStoryQuestions({
-        difficulty,
-        story: storyObj.story,
-        storyId: storyObj.id
-      });
-      setQuestions(questions);
-      setQuestionsLoaded(true);
-    } catch (error) {
-      console.error(error);
-      setQuestionsLoadError(true);
-    }
-  }
-
-  function handleReset() {
-    setResetNumber((prevNumber) => prevNumber + 1);
-    setLoadComplete(false);
-    setQuestionsLoaded(false);
-    setQuestions([]);
-    setGenerateButtonPressed(false);
   }
 }
