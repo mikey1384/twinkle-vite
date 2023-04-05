cf3fa9142d4e779c56cb2d756afa1be7560820a5
mikey1384 <mikey1384@gmail.com>
2023-04-05 08:41:09 +0700

    more refactoring

---
+++ src/components/ContentPanel/Body/MainContent/XPVideoAdditionalInfo.jsx
@@ -0,0 +1,54 @@
+import PropTypes from 'prop-types';
+import AlreadyPosted from '~/components/AlreadyPosted';
+import TagStatus from '~/components/TagStatus';
+
+XPVideoAdditionalInfo.propTypes = {
+  contentType: PropTypes.string.isRequired,
+  uploader: PropTypes.object,
+  contentId: PropTypes.number.isRequired,
+  content: PropTypes.string.isRequired,
+  onAddTags: PropTypes.func.isRequired,
+  onAddTagToContents: PropTypes.func.isRequired,
+  onLoadTags: PropTypes.func.isRequired,
+  rewardLevel: PropTypes.number,
+  tags: PropTypes.array,
+  theme: PropTypes.string
+};
+
+export default function XPVideoAdditionalInfo({
+  contentType,
+  uploader,
+  contentId,
+  content,
+  onAddTags,
+  onAddTagToContents,
+  onLoadTags,
+  rewardLevel,
+  tags,
+  theme
+}) {
+  if (contentType !== 'video') return null;
+  return (
+    <>
+      <AlreadyPosted
+        style={{
+          marginTop: rewardLevel ? '0.5rem' : '-0.5rem',
+          marginBottom: '0.5rem'
+        }}
+        uploaderId={(uploader || {}).id}
+        contentId={contentId}
+        contentType={contentType}
+        url={content}
+        videoCode={contentType === 'video' ? content : undefined}
+      />
+      <TagStatus
+        onAddTags={onAddTags}
+        onAddTagToContents={onAddTagToContents}
+        onLoadTags={onLoadTags}
+        tags={tags || []}
+        contentId={contentId}
+        theme={theme}
+      />
+    </>
+  );
+}
