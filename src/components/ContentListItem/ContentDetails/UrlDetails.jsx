a320094bad53aa7d27cc5d7eca6897bfb61fbb23
mikey1384 <mikey1384@gmail.com>
2023-04-08 09:39:20 +0700

    more refactoring

---
+++ src/components/ContentListItem/ContentDetails/UrlDetails.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Embedly from '~/components/Embedly';
+
+UrlDetails.propTypes = {
+  contentId: PropTypes.number.isRequired,
+  title: PropTypes.string
+};
+
+export default function UrlDetails({ contentId, title }) {
+  return (
+    <div>
+      <span
+        style={{
+          fontWeight: 'bold',
+          fontSize: '2rem'
+        }}
+        className="label"
+      >
+        {title}
+      </span>
+      <Embedly
+        small
+        noLink
+        style={{ marginTop: '0.5rem' }}
+        contentId={contentId}
+      />
+    </div>
+  );
+}
