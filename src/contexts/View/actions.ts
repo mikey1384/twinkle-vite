842c8436f99e3aad79aad174e9b8060a67ccf840
mikey1384 <mikey1384@gmail.com>
2023-04-13 14:58:17 +0700

    more updates

--- src/contexts/View/actions.ts
+++ src/contexts/View/actions.ts
@@ -1,49 +1,57 @@
-export default function ViewActions(dispatch) {
+import { Dispatch } from '../types';
+
+export default function ViewActions(dispatch: Dispatch) {
   return {
-    onChangePageVisibility(visible) {
+    onChangePageVisibility(visible: boolean) {
       return dispatch({
         type: 'CHANGE_PAGE_VISIBILITY',
         visible
       });
     },
-    onRecordScrollPosition({ section, position }) {
+    onRecordScrollPosition({
+      section,
+      position
+    }: {
+      section: string;
+      position: number;
+    }) {
       return dispatch({
         type: 'RECORD_SCROLL_POSITION',
         section,
         position
       });
     },
-    onSetExploreCategory(category) {
+    onSetExploreCategory(category: string) {
       return dispatch({
         type: 'SET_EXPLORE_CATEGORY',
         category
       });
     },
-    onSetContentNav(nav) {
+    onSetContentNav(nav: string) {
       return dispatch({
         type: 'SET_CONTENT_NAV',
         nav
       });
     },
-    onSetContentPath(path) {
+    onSetContentPath(path: string) {
       return dispatch({
         type: 'SET_CONTENT_PATH',
         path
       });
     },
-    onSetHomeNav(nav) {
+    onSetHomeNav(nav: string) {
       return dispatch({
         type: 'SET_HOME_NAV',
         nav
       });
     },
-    onSetPageTitle(title) {
+    onSetPageTitle(title: string) {
       return dispatch({
         type: 'SET_PAGE_TITLE',
         title
       });
     },
-    onSetProfileNav(nav) {
+    onSetProfileNav(nav: string) {
       return dispatch({
         type: 'SET_PROFILE_NAV',
         nav
