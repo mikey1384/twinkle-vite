a3433ebf700ee8c5186b5553aa8636e94c052b9e
mikey1384 <mikey1384@gmail.com>
2023-04-28 21:24:26 +0700

    more ts conversions

--- src/containers/Signin/RestoreAccount/EmailSection/index.tsx
+++ src/containers/Signin/RestoreAccount/EmailSection/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import EmailExists from './EmailExists';
 import AskForHelp from '~/components/AskForHelp';
@@ -6,7 +7,15 @@ EmailSection.propTypes = {
   account: PropTypes.object.isRequired
 };
 
-export default function EmailSection({ account }) {
+export default function EmailSection({
+  account
+}: {
+  account: {
+    email: string;
+    verifiedEmail: string;
+    id: number;
+  };
+}) {
   return (
     <div>
       {account.email || account.verifiedEmail ? (
