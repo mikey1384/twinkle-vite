97a1e4c9104884fd4270909516f5bbbd3677d88c
mikey1384 <mikey1384@gmail.com>
2023-05-02 20:47:25 +0700

    more updates

--- src/components/CardThumb/Detailed.tsx
+++ src/components/CardThumb/Detailed.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Color, mobileMaxWidth } from '~/constants/css';
 import { css } from '@emotion/css';
 import { cloudFrontURL, cardProps } from '~/constants/defaultValues';
+import { Card } from '~/types';
 
+CardThumb.propTypes = {
+  card: PropTypes.object.isRequired,
+  displayedBurnXP: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+    .isRequired,
+  borderColor: PropTypes.string,
+  cardColor: PropTypes.string,
+  style: PropTypes.object,
+  onClick: PropTypes.func,
+  xpNumberColor: PropTypes.string
+};
 export default function CardThumb({
   card,
   displayedBurnXP,
@@ -12,11 +24,11 @@ export default function CardThumb({
   onClick,
   xpNumberColor
 }: {
-  card: any;
+  card: Card;
   displayedBurnXP: string | number;
   borderColor?: string;
   cardColor?: string;
-  style?: any;
+  style?: React.CSSProperties;
   onClick?: () => void;
   xpNumberColor: string;
 }) {
