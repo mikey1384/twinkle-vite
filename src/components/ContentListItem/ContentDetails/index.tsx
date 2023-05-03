580acaf5630de4dc8e081e6cacf0ccbbaf589247
mikey1384 <mikey1384@gmail.com>
2023-05-03 21:13:35 +0700

    more ts conversions

--- src/components/ContentListItem/ContentDetails/index.tsx
+++ src/components/ContentListItem/ContentDetails/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import VideoDetails from './VideoDetails';
 import SubjectDetails from './SubjectDetails';
 import UrlDetails from './UrlDetails';
 import AIStoryDetails from './AIStoryDetails';
 
+ContentDetails.propTypes = {
+  contentType: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  story: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  uploader: PropTypes.object.isRequired,
+  contentId: PropTypes.number.isRequired,
+  topic: PropTypes.string
+};
 export default function ContentDetails({
   contentType,
   description,
