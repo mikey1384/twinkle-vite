ec129c31235f65e3eb315c8a661e8c571263000c
mikey1384 <mikey1384@gmail.com>
2023-05-04 08:52:25 +0700

    more ts conversions

--- src/components/ContentPanel/Heading/index.tsx
+++ src/components/ContentPanel/Heading/index.tsx
@@ -1,20 +1,34 @@
 import React, { memo } from 'react';
+import PropTypes from 'prop-types';
 import ProfilePic from '~/components/ProfilePic';
 import { useNavigate } from 'react-router-dom';
 import { timeSince } from '~/helpers/timeStampHelpers';
 import { css } from '@emotion/css';
 import { useContentState } from '~/helpers/hooks';
 import useHeadingText from './useHeadingText';
+import { Content } from '~/types';
 
+Heading.propTypes = {
+  action: PropTypes.string.isRequired,
+  theme: PropTypes.string.isRequired,
+  contentObj: PropTypes.object.isRequired
+};
 function Heading({
   action,
   theme,
   contentObj,
-  contentObj: { contentType, id, rootType, rootId, timeStamp, uploader = {} }
+  contentObj: {
+    contentType,
+    id,
+    rootType = '',
+    rootId = 0,
+    timeStamp,
+    uploader = { username: '', id: 0 }
+  }
 }: {
   action: string;
   theme: string;
-  contentObj: any;
+  contentObj: Content;
 }) {
   const rootObj = useContentState({
     contentType: rootType,
@@ -34,7 +48,7 @@ function Heading({
         <ProfilePic
           style={{ width: '6rem' }}
           userId={uploader.id}
-          profilePicUrl={uploader.profilePicUrl}
+          profilePicUrl={uploader.profilePicUrl || ''}
         />
       </div>
       <div
