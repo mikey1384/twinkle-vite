aa84d2bdbc5f042e1421411ea388b2190bbc9d17
mikey1384 <mikey1384@gmail.com>
2023-05-04 07:54:13 +0700

    more ts conversions

--- src/components/ContentPanel/Body/MainContent/RewardLevelDisplay.tsx
+++ src/components/ContentPanel/Body/MainContent/RewardLevelDisplay.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { css } from '@emotion/css';
 import RewardLevelBar from '~/components/RewardLevelBar';
 import { mobileMaxWidth } from '~/constants/css';
+import { Content } from '~/types';
 
+RewardLevelDisplay.propTypes = {
+  contentType: PropTypes.string.isRequired,
+  rootObj: PropTypes.object.isRequired,
+  byUser: PropTypes.bool.isRequired,
+  rewardLevel: PropTypes.number,
+  rootType: PropTypes.string
+};
 export default function RewardLevelDisplay({
   contentType,
   rootObj,
@@ -11,10 +20,10 @@ export default function RewardLevelDisplay({
   rootType
 }: {
   contentType: string;
-  rootObj: any;
+  rootObj: Content;
   byUser: boolean;
-  rewardLevel: number;
-  rootType: string;
+  rewardLevel?: number;
+  rootType?: string;
 }) {
   if (contentType !== 'subject' || !!rootObj.id || byUser || !rewardLevel)
     return null;
