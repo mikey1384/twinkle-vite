dd19c413c5540f69eafdeb338df59b44cf5f9bfe
mikey1384 <mikey1384@gmail.com>
2023-05-04 08:26:29 +0700

    more ts conversions

--- src/components/ContentPanel/Body/MainContent/XPVideoAdditionalInfo.tsx
+++ src/components/ContentPanel/Body/MainContent/XPVideoAdditionalInfo.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import AlreadyPosted from '~/components/AlreadyPosted';
 import TagStatus from '~/components/TagStatus';
+import ErrorBoundary from '~/components/ErrorBoundary';
 
+XPVideoAdditionalInfo.propTypes = {
+  contentType: PropTypes.string.isRequired,
+  uploader: PropTypes.object.isRequired,
+  contentId: PropTypes.number.isRequired,
+  content: PropTypes.string,
+  loggedIn: PropTypes.bool.isRequired,
+  onAddTags: PropTypes.func.isRequired,
+  onAddTagToContents: PropTypes.func.isRequired,
+  onLoadTags: PropTypes.func.isRequired,
+  rewardLevel: PropTypes.number,
+  tags: PropTypes.array.isRequired,
+  theme: PropTypes.string.isRequired
+};
 export default function XPVideoAdditionalInfo({
   contentType,
   uploader,
@@ -18,18 +33,18 @@ export default function XPVideoAdditionalInfo({
   contentType: string;
   uploader: any;
   contentId: number;
-  content: string;
+  content?: string;
   loggedIn: boolean;
   onAddTags: any;
   onAddTagToContents: any;
   onLoadTags: any;
-  rewardLevel: number;
+  rewardLevel?: number;
   tags: any;
   theme: string;
 }) {
   if (contentType !== 'video') return null;
   return (
-    <>
+    <ErrorBoundary componentPath="ContentPanel/Body/MainContent/XPVideoAdditionalInfo">
       <AlreadyPosted
         style={{
           marginTop: loggedIn && rewardLevel ? '0.5rem' : '-0.5rem',
@@ -49,6 +64,6 @@ export default function XPVideoAdditionalInfo({
         contentId={contentId}
         theme={theme}
       />
-    </>
+    </ErrorBoundary>
   );
 }
