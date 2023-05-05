ce27623a07829b2cc26657139f1a51321ef7d8d6
mikey1384 <mikey1384@gmail.com>
2023-05-05 15:43:07 +0700

    added mention feature to react-markdown

---
+++ src/components/Texts/LongText/plugins.ts
@@ -0,0 +1,45 @@
+import { visit } from 'unist-util-visit';
+
+const isFakeMention: {
+  [key: string]: boolean;
+} = {};
+
+export function remarkMentions() {
+  return (tree: any) => {
+    visit(tree, 'text', (node, index, parent) => {
+      if (typeof node.value !== 'string') return;
+      const mentions = node.value.split(/(@\w+|＠\w+)/g);
+
+      if (mentions.length <= 1) return;
+
+      const newNodes = mentions
+        .filter((text: string) => text)
+        .map((text: string) => {
+          if (/^＠\w+$/.test(text)) {
+            isFakeMention[text.replace('＠', '')] = true;
+            return {
+              type: 'text',
+              value: text.replace('＠', '@')
+            };
+          }
+          const match = /^@\w+$/.test(text);
+          if (match && !isFakeMention[text.slice(1)]) {
+            const mentionUrl = `/users/${text.slice(1)}`;
+            return {
+              type: 'link',
+              url: mentionUrl,
+              data: {
+                hProperties: {
+                  className: 'mention'
+                }
+              },
+              children: [{ type: 'text', value: text }]
+            };
+          }
+          return { type: 'text', value: text };
+        });
+
+      parent.children.splice(index, 1, ...newNodes);
+    });
+  };
+}
