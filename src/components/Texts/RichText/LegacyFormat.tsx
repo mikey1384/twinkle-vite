1e21d59007066d0ebbced7c7b40dfc3572b98a4f
mikey1384 <mikey1384@gmail.com>
2023-05-10 14:03:48 +0700

    more updates

--- src/components/Texts/RichText/LegacyFormat.tsx
+++ src/components/Texts/RichText/LegacyFormat.tsx
@@ -1,9 +1,7 @@
 import React, { useMemo } from 'react';
-import {
-  limitBrs,
-  processMentionLink,
-  processedStringWithURL
-} from '~/helpers/stringHelpers';
+import { limitBrs, processedStringWithURL } from '~/helpers/stringHelpers';
+import { Link } from 'react-router-dom';
+import parse from 'html-react-parser';
 
 export default function LegacyFormat({
   text,
@@ -26,9 +24,24 @@ export default function LegacyFormat({
         processedText = finalTextArray.join('<a');
       }
     }
-    const finalText = processMentionLink(limitBrs(processedText));
+    const finalText = convertStringToJSX(limitBrs(processedText));
     return finalText;
   }, [fullText, isOverflown, text]);
-
   return <>{processedText}</>;
+}
+
+function convertStringToJSX(text: string): React.ReactNode {
+  const result = parse(limitBrs(text), {
+    replace: (domNode) => {
+      if (
+        domNode.type === 'tag' &&
+        domNode.name === 'a' &&
+        domNode.attribs?.class === 'mention'
+      ) {
+        const node = domNode.children?.[0];
+        return <Link to={domNode.attribs.href || ''}>{node?.data}</Link>;
+      }
+    }
+  });
+  return result;
 }
