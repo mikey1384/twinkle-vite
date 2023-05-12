14ce3b33adae6682c93889943edcdf78d6e5ed86
mikey1384 <mikey1384@gmail.com>
2023-05-12 08:51:07 +0700

    more fix

--- src/components/Texts/RichText/MediaComponent/YouTubeVideo.tsx
+++ src/components/Texts/RichText/MediaComponent/YouTubeVideo.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactPlayer from 'react-player';
+import { isMobile } from '~/helpers';
+import { css } from '@emotion/css';
+import { fetchedVideoCodeFromURL } from '~/helpers/stringHelpers';
+import playButtonImg from '~/assets/play-button-image.png';
+
+const displayIsMobile = isMobile(navigator);
 
 export default function YouTubeVideo({
   src,
@@ -9,5 +15,29 @@ export default function YouTubeVideo({
   src: string;
   onReady: () => void;
 }) {
-  return <ReactPlayer {...commonProps} url={src} onReady={onReady} />;
+  const [isStarted, setIsStarted] = useState(!displayIsMobile);
+  return isStarted ? (
+    <ReactPlayer {...commonProps} url={src} onReady={onReady} />
+  ) : (
+    <div
+      onClick={() => setIsStarted(true)}
+      className={css`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        top: 0;
+        left: 0;
+        z-index: 1;
+        width: 100%;
+        height: 100%;
+        background: url(https://i.ytimg.com/vi/${fetchedVideoCodeFromURL(
+            src
+          )}/mqdefault.jpg)
+          no-repeat center;
+        background-size: 100% auto;
+      `}
+    >
+      <img style={{ width: '45px', height: '45px' }} src={playButtonImg} />
+    </div>
+  );
 }
