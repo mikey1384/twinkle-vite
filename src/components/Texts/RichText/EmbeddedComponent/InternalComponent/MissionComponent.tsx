4e94f69cc8f3fc492cd1661fbdc2989ceb1c9241
mikey1384 <mikey1384@gmail.com>
2023-05-13 18:22:36 +0700

    more fix

--- src/components/Texts/RichText/EmbeddedComponent/InternalComponent/MissionComponent.tsx
+++ src/components/Texts/RichText/EmbeddedComponent/InternalComponent/MissionComponent.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function MissionComponent() {
+export default function MissionComponent({ src }: { src: string }) {
   return (
     <div>
-      <div>this is a mission component</div>
+      <div>this is a mission component {src}</div>
     </div>
   );
 }
--- src/components/Texts/RichText/EmbeddedComponent/InternalComponent/index.tsx
+++ src/components/Texts/RichText/EmbeddedComponent/InternalComponent/index.tsx
@@ -7,16 +7,16 @@ export default function InternalComponent({ src }: { src: string }) {
   const InnerComponent = useMemo(() => {
     const urlParts = src.split('/');
     const linkType = urlParts[1];
+    const contentId = urlParts[2]?.split('?')?.[0];
     const mainContentTypes = ['videos', 'links', 'subjects'];
-    if (mainContentTypes.includes(linkType)) {
-      const contentId = urlParts[2]?.split('?')[0];
+    if (mainContentTypes.includes(linkType) && contentId) {
       const contentType = linkType.slice(0, -1);
       return (
         <MainContentComponent contentType={contentType} contentId={contentId} />
       );
     }
     if (linkType === 'missions') {
-      return <MissionComponent />;
+      return <MissionComponent src={src} />;
     }
     return (
       <div>
