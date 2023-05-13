ceed424cbd701ee5398f69af0fe7937bd6af6226
mikey1384 <mikey1384@gmail.com>
2023-05-13 18:54:16 +0700

    more updates

--- src/components/Texts/RichText/EmbeddedComponent/InternalComponent/MissionComponent.tsx
+++ src/components/Texts/RichText/EmbeddedComponent/InternalComponent/MissionComponent.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
+import { useAppContext, useMissionContext } from '~/contexts';
 
 export default function MissionComponent({ src }: { src: string }) {
+  const missionTypeIdHash = useMissionContext((v) => v.state.missionTypeIdHash);
+  const loadMissionTypeIdHash = useAppContext(
+    (v) => v.requestHelpers.loadMissionTypeIdHash
+  );
+  const onLoadMissionTypeIdHash = useMissionContext(
+    (v) => v.actions.onLoadMissionTypeIdHash
+  );
+
+  const missionType = useMemo(() => {
+    const srcParts = src.split('/');
+    return srcParts[srcParts.length - 1] || srcParts[srcParts.length - 2];
+  }, [src]);
+
+  const missionId = useMemo(() => {
+    return missionTypeIdHash?.[missionType];
+  }, [missionTypeIdHash, missionType]);
+
+  useEffect(() => {
+    if (!missionId) {
+      getMissionId();
+    }
+    async function getMissionId() {
+      const data = await loadMissionTypeIdHash();
+      onLoadMissionTypeIdHash(data);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [missionId, missionTypeIdHash]);
+
   return (
     <div>
-      <div>this is a mission component {src}</div>
+      <div>this is a mission component {missionId}</div>
     </div>
   );
 }
