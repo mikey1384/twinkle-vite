86f058fd30fde336e2e4165e7d237e329c15547f
mikey1384 <mikey1384@gmail.com>
2023-05-14 08:45:39 +0700

    more fix

--- src/components/Texts/RichText/EmbeddedComponent/InternalComponent/UserComponent.tsx
+++ src/components/Texts/RichText/EmbeddedComponent/InternalComponent/UserComponent.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
 export default function UserComponent({ src }: { src: string }) {
+  const parts = src.split('/');
+  const pageType = parts[3];
+  const subPageType = parts[4];
+
+  if (pageType === 'watched') {
+    return (
+      <div>
+        <div>watched page</div>
+        <div>{src}</div>
+      </div>
+    );
+  }
+  if (pageType === 'all' && subPageType === 'byuser') {
+    return (
+      <div>
+        <div>made by page</div>
+        <div>{src}</div>
+      </div>
+    );
+  }
   return (
     <div>
-      <div>this is a user profile! {src}</div>
+      <div>user page</div>
+      <div>{src}</div>
     </div>
   );
 }
