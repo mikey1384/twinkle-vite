bb14e3f325b01de489f74d175bf5e170af27b50f
mikey1384 <mikey1384@gmail.com>
2023-05-14 08:55:33 +0700

    more updates

--- src/components/Texts/RichText/EmbeddedComponent/InternalComponent/UserComponent.tsx
+++ src/components/Texts/RichText/EmbeddedComponent/InternalComponent/UserComponent.tsx
@@ -13,10 +13,82 @@ export default function UserComponent({ src }: { src: string }) {
       </div>
     );
   }
+  if (pageType === 'likes') {
+    return (
+      <div>
+        <div>likes page</div>
+        <div>{src}</div>
+      </div>
+    );
+  }
   if (pageType === 'all' && subPageType === 'byuser') {
     return (
       <div>
         <div>made by page</div>
+        <div>{src}</div>
+      </div>
+    );
+  }
+  if (pageType === 'all') {
+    return (
+      <div>
+        <div>all posts by page</div>
+        <div>{src}</div>
+      </div>
+    );
+  }
+  if (pageType === 'comments') {
+    return (
+      <div>
+        <div>comments by page</div>
+        <div>{src}</div>
+      </div>
+    );
+  }
+  if (pageType === 'subjects' && subPageType === 'byuser') {
+    return (
+      <div>
+        <div>subjects made by page</div>
+        <div>{src}</div>
+      </div>
+    );
+  }
+  if (pageType === 'subjects') {
+    return (
+      <div>
+        <div>subjects by page</div>
+        <div>{src}</div>
+      </div>
+    );
+  }
+  if (pageType === 'ai-stories') {
+    return (
+      <div>
+        <div>ai stories by page</div>
+        <div>{src}</div>
+      </div>
+    );
+  }
+  if (pageType === 'videos' && subPageType === 'byuser') {
+    return (
+      <div>
+        <div>videos made by page</div>
+        <div>{src}</div>
+      </div>
+    );
+  }
+  if (pageType === 'videos') {
+    return (
+      <div>
+        <div>videos by page</div>
+        <div>{src}</div>
+      </div>
+    );
+  }
+  if (pageType === 'links' && subPageType === 'byuser') {
+    return (
+      <div>
+        <div>links made by page</div>
         <div>{src}</div>
       </div>
     );
