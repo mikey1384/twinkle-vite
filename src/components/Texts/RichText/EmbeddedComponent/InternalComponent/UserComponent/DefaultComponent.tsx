b6c4cbc469b5c4bd98fa6e589ae258a9b664ccb8
mikey1384 <mikey1384@gmail.com>
2023-05-14 10:34:38 +0700

    more updates

--- src/components/Texts/RichText/EmbeddedComponent/InternalComponent/UserComponent/DefaultComponent.tsx
+++ src/components/Texts/RichText/EmbeddedComponent/InternalComponent/UserComponent/DefaultComponent.tsx
@@ -1,17 +1,43 @@
 import React from 'react';
+import UserDetails from '~/components/UserDetails';
+import ProfilePic from '~/components/ProfilePic';
+import { useChatContext } from '~/contexts';
 
 export default function DefaultComponent({
   src,
-  profileId
+  profile
 }: {
   src: string;
+  profile: {
+    id: number;
+    username: string;
+    profilePicUrl: string;
+  };
   profileId: number;
 }) {
+  const chatStatus = useChatContext((v) => v.state.chatStatus);
   return (
     <div>
-      <div>default page</div>
-      <div>
-        {src} {profileId}
+      <div style={{ fontWeight: 'bold' }}>default page {src}</div>
+      <div style={{ display: 'flex', marginTop: '2rem' }}>
+        <div style={{ width: '10rem' }}>
+          <ProfilePic
+            userId={profile.id}
+            profilePicUrl={profile.profilePicUrl || ''}
+            online={chatStatus[profile.id]?.isOnline}
+            statusShown
+            large
+          />
+        </div>
+        <div style={{ marginLeft: '2rem' }}>
+          <UserDetails
+            noLink
+            small
+            unEditable
+            profile={profile}
+            userId={profile.id}
+          />
+        </div>
       </div>
     </div>
   );
