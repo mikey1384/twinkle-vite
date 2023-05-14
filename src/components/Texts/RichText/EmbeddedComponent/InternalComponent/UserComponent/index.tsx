a77b48b8298ca7c1d065e3ed8097129331b04525
mikey1384 <mikey1384@gmail.com>
2023-05-14 09:16:50 +0700

    more fix

--- src/components/Texts/RichText/EmbeddedComponent/InternalComponent/UserComponent/index.tsx
+++ src/components/Texts/RichText/EmbeddedComponent/InternalComponent/UserComponent/index.tsx
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useProfileState, useTheme } from '~/helpers/hooks';
 
 export default function UserComponent({ src }: { src: string }) {
   const parts = src.split('/');
+  const username = parts[2];
   const pageType = parts[3];
   const subPageType = parts[4];
+  const { notExist, profileId } = useProfileState(username || '');
+
+  useEffect(() => {
+    let retries = 0;
+    const maxRetries = 3;
+
+    if (!notExist && !profile.loaded) {
+      loadProfile();
+    }
+
+    async function loadProfile() {
+      setLoading(true);
+      try {
+        const { pageNotExists, user } = await loadProfileViaUsername(
+          params.username
+        );
+        if (pageNotExists) {
+          setLoading(false);
+          return onUserNotExist(params.username);
+        }
+        onSetProfileId({ username: params.username, profileId: user.id });
+        onSetUserState({
+          userId: user.id,
+          newState: {
+            userId: user.id,
+            contentId: user.id,
+            username: params.username,
+            ...user,
+            loaded: true
+          }
+        });
+        onInitContent({
+          contentId: user.id,
+          contentType: 'user',
+          ...user
+        });
+        setLoading(false);
+      } catch (error) {
+        if (retries < maxRetries) {
+          retries++;
+          setTimeout(loadProfile, 500);
+        } else {
+          onUserNotExist(params.username);
+          setLoading(false);
+        }
+      }
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [params.username, notExist, profile.loaded]);
 
   if (pageType === 'watched') {
     return (
