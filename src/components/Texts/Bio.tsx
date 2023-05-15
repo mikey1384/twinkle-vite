fab74de3bee2e0ae9528676835433baf36e0cb7c
mikey1384 <mikey1384@gmail.com>
2023-05-15 15:34:59 +0700

    more fix

--- src/components/Texts/Bio.tsx
+++ src/components/Texts/Bio.tsx
@@ -27,26 +27,56 @@ export default function Bio({
         paddingLeft: '2rem',
         lineHeight: 1.6,
         listStyleType: 'disc',
+        width: '100%',
         fontSize: small ? '1.5rem' : '1.7rem',
         ...style
       }}
     >
       {firstRow && (
-        <nav style={{ display: 'flex', alignItems: 'center' }}>
-          <span style={{ paddingRight: '5px' }}>•</span>
-          <RichText>{firstRow}</RichText>
+        <nav style={{ display: 'flex', alignItems: 'center', width: '100%' }}>
+          <div
+            style={{
+              padding: '0 1.5rem',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
+            •
+          </div>
+          <RichText style={{ width: 'CALC(100% - 2rem)' }}>{firstRow}</RichText>
         </nav>
       )}
       {secondRow && (
-        <nav style={{ display: 'flex', alignItems: 'center' }}>
-          <span style={{ paddingRight: '5px' }}>•</span>
-          <RichText>{secondRow}</RichText>
+        <nav style={{ display: 'flex', alignItems: 'center', width: '100%' }}>
+          <div
+            style={{
+              padding: '0 1.5rem',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
+            •
+          </div>
+          <RichText style={{ width: 'CALC(100% - 2rem)' }}>
+            {secondRow}
+          </RichText>
         </nav>
       )}
       {thirdRow && (
-        <nav style={{ display: 'flex', alignItems: 'center' }}>
-          <span style={{ paddingRight: '5px' }}>•</span>
-          <RichText>{thirdRow}</RichText>
+        <nav style={{ display: 'flex', alignItems: 'center', width: '100%' }}>
+          <div
+            style={{
+              padding: '0 1.5rem',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
+            •
+          </div>
+          <RichText style={{ width: 'CALC(100% - 2rem)' }}>{thirdRow}</RichText>
         </nav>
       )}
     </section>
