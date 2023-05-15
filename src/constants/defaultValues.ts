0a30de239a35dae2ad759b60890ab258687ee618
mikey1384 <mikey1384@gmail.com>
2023-05-15 16:02:26 +0700

    bug fix

--- src/constants/defaultValues.ts
+++ src/constants/defaultValues.ts
@@ -1,7 +1,7 @@
 import localize from '~/constants/localize';
 import { Color } from '~/constants/css';
 
-export const clientVersion = '1.7.80';
+export const clientVersion = '1.7.81';
 export const defaultChatSubject = 'Welcome!';
 export const defaultContentState = {
   isEditing: false,
