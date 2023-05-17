a18aeebf8aa05d85b632eab8e2b1b15c7058bfcd
mikey1384 <mikey1384@gmail.com>
2023-05-17 08:24:42 +0700

    more fix

--- src/components/Texts/RichText/EmbeddedComponent/InternalComponent/AICardComponent/index.tsx
+++ src/components/Texts/RichText/EmbeddedComponent/InternalComponent/AICardComponent/index.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 export default function AICardComponent({ src }: { src: string }) {
+  const queryParams = useMemo(() => {
+    const url = new URL(`https:/${src}`);
+    const params = new URLSearchParams(url.search || '');
+    const cardId = params.get('cardId');
+    const color = params.get('search[color]');
+    const isBuyNow = params.get('search[isBuyNow]');
+    const quality = params.get('search[quality]');
+    const owner = params.get('search[owner]');
+    const word = params.get('search[word]');
+    return { cardId, color, isBuyNow, quality, owner, word };
+  }, [src]);
+
   return (
     <div>
       <div>AI Card!!</div>
       <div>{src}</div>
+      <div>{JSON.stringify(queryParams)}</div>
     </div>
   );
 }
