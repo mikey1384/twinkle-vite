8dc76147bbaa92f759445d02115f1bd7ca43ba97
mikey1384 <mikey1384@gmail.com>
2023-06-01 09:16:34 +0700

    version up

--- src/constants/defaultValues.ts
+++ src/constants/defaultValues.ts
@@ -1,7 +1,7 @@
 import localize from '~/constants/localize';
 import { Color } from '~/constants/css';
 
-export const clientVersion = '1.7.94';
+export const clientVersion = '1.7.95';
 export const defaultChatSubject = 'Welcome!';
 export const defaultContentState = {
   isEditing: false,
