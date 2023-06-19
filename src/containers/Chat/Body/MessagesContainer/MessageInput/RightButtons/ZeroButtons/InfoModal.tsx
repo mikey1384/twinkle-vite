a3d5eec68e38d1345f299a90f3fc101e1df4db20
mikey1384 <mikey1384@gmail.com>
2023-06-19 20:16:39 +0700

    more updates

--- src/containers/Chat/Body/MessagesContainer/MessageInput/RightButtons/ZeroButtons/InfoModal.tsx
+++ src/containers/Chat/Body/MessagesContainer/MessageInput/RightButtons/ZeroButtons/InfoModal.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import Modal from '~/components/Modal';
 import Button from '~/components/Button';
 import { useKeyContext } from '~/contexts';
-import localize from '~/constants/localize';
-
-const cancelLabel = localize('cancel');
 
 export default function ConfirmModal({ onHide }: { onHide: () => void }) {
   const {
@@ -15,17 +12,10 @@ export default function ConfirmModal({ onHide }: { onHide: () => void }) {
       <header>Under Construction</header>
       <main>Still working on this feature. Please check back later.</main>
       <footer>
-        <Button transparent style={{ marginRight: '0.7rem' }} onClick={onHide}>
-          {cancelLabel}
-        </Button>
-        <Button color={doneColor} onClick={handleConfirm}>
+        <Button color={doneColor} onClick={onHide}>
           Okay
         </Button>
       </footer>
     </Modal>
   );
-
-  function handleConfirm() {
-    onHide();
-  }
 }
