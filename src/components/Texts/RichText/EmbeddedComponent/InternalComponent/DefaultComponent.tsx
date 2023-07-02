043388f339c50e9c1f8f210b8fdaac09309c1045
mikey1384 <mikey1384@gmail.com>
2023-07-02 21:35:42 +0700

    more fix

--- src/components/Texts/RichText/EmbeddedComponent/InternalComponent/DefaultComponent.tsx
+++ src/components/Texts/RichText/EmbeddedComponent/InternalComponent/DefaultComponent.tsx
@@ -1,27 +1,105 @@
 import React, { useMemo } from 'react';
 import { Link } from 'react-router-dom';
-import { capitalize } from '~/helpers/stringHelpers';
+
+type LinkType = 'users' | 'ai-cards' | 'missions' | 'chat' | 'explore';
+
+const getDescriptionForLinkType: { [K in LinkType]: (src: string) => string } =
+  {
+    users: (src) => {
+      const parts = src.split('/');
+      const username = parts[2];
+      const type = parts[3];
+      const subtype = parts[4];
+      const byUser = subtype === 'byuser' ? 'Created' : 'Posted';
+      const contentByUser = type === 'byuser' ? 'Content Created' : 'Posts';
+
+      switch (type) {
+        case 'all':
+          if (subtype === 'byuser') {
+            return `All Content Created by ${username}`;
+          } else {
+            return `All ${contentByUser} by ${username}`;
+          }
+        case 'watched':
+          return `Videos Watched by ${username}`;
+        case 'likes':
+          if (subtype) {
+            return `${
+              subtype === 'all' ? 'All Posts' : capitalize(subtype)
+            } Liked by ${username}`;
+          } else {
+            return `All Liked Posts by ${username}`;
+          }
+        case 'comments':
+          return `Comments ${byUser} by ${username}`;
+        case 'videos':
+          return `Videos ${byUser} by ${username}`;
+        case 'links':
+          if (subtype === 'byuser') {
+            return `Webpages Created by ${username}`;
+          } else {
+            return `Links ${byUser} by ${username}`;
+          }
+        case 'subjects':
+          return `Subjects ${byUser} by ${username}`;
+        case 'ai-stories':
+          return `AI Stories ${byUser} by ${username}`;
+        default:
+          return `${username}'s Profile`;
+      }
+    },
+    'ai-cards': () => 'Explore AI Cards',
+    missions: (src) => {
+      const mission = src.split('/missions/')[1];
+      if (!mission) {
+        return `Browse Missions`;
+      }
+      return `Mission: ${mission.replace(/-/g, ' ')}`;
+    },
+    chat: (src) => {
+      const chatType = src.split('/chat/')[1];
+      if (chatType === 'vocabulary') {
+        return 'Collect Vocabulary';
+      } else if (chatType === 'ai-cards') {
+        return 'Collect AI Cards';
+      } else {
+        return 'Chat';
+      }
+    },
+    explore: (src) => {
+      const exploreType = src.split('/')[1];
+      return `Explore ${capitalize(exploreType)}`;
+    }
+  };
 
 export default function DefaultComponent({
   linkType,
   src
 }: {
-  linkType: string;
+  linkType: LinkType;
   src: string;
 }) {
+  const appliedLinkType = useMemo(() => {
+    const exploreType = ['subjects', 'videos', 'links'];
+    if (exploreType.includes(linkType)) {
+      return 'explore';
+    }
+    return linkType;
+  }, [linkType]);
   const linkLabel = useMemo(
-    () =>
-      capitalize(
-        linkType === 'ai-cards' ? 'AI Cards' : linkType ? linkType : 'Home'
-      ),
-    [linkType]
+    () => getDescriptionForLinkType[appliedLinkType]?.(src) || 'Home',
+    [appliedLinkType, src]
   );
 
   return (
     <div>
       <Link to={src} style={{ fontWeight: 'bold' }}>
-        <u>Go to {linkLabel}</u>
+        <u>{linkLabel}</u>
       </Link>
     </div>
   );
+}
+
+function capitalize(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
