eba798df59d4d7c10fc6250243f8242a22d5343b
mikey1384 <mikey1384@gmail.com>
2023-07-09 19:23:16 +0700

    more updates

--- src/containers/Home/Achievements/index.tsx
+++ src/containers/Home/Achievements/index.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { css } from '@emotion/css';
+import { Color, borderRadius, mobileMaxWidth } from '~/constants/css';
 
 export default function Achievements() {
   return (
-    <div>
-      <h1>Achievements</h1>
+    <div style={{ paddingBottom: '15rem' }}>
+      <div
+        className={css`
+          margin-bottom: 2rem;
+          background: #fff;
+          padding: 1rem;
+          border: 1px solid ${Color.borderGray()};
+          border-radius: ${borderRadius};
+          @media (max-width: ${mobileMaxWidth}) {
+            border-radius: 0;
+            border-top: 0;
+            border-left: 0;
+            border-right: 0;
+          }
+        `}
+      >
+        <p
+          className={css`
+            font-size: 2rem;
+            font-weight: bold;
+            line-height: 1.5;
+            color: ${Color.darkerGray()};
+          `}
+          style={{ fontWeight: 'bold', fontSize: '2.5rem' }}
+        >
+          Achievements
+        </p>
+      </div>
     </div>
   );
 }
