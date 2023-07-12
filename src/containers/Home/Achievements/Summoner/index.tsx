a23aafa9fb5eace4c120ebaed69b7713044bd1b4
mikey1384 <mikey1384@gmail.com>
2023-07-12 20:10:32 +0700

    more updates

--- src/containers/Home/Achievements/Summoner/index.tsx
+++ src/containers/Home/Achievements/Summoner/index.tsx
@@ -6,7 +6,7 @@ export default function Summoner({ style }: { style?: React.CSSProperties }) {
   return (
     <ItemPanel
       style={style}
-      itemName="The Cybernetic Summoner"
+      itemName="Cybernetic Summoner"
       description="Bestowed upon the select few who have unraveled the enigmas of the AI Card Conjuring. Your odyssey into the realms of AI wizardry has not only earned you this esteemed recognition but also transformed you into a revered Summoner of the Cybernetic Realm. Step forth, intrepid explorer of digital dimensions, your journey has just begun."
       requirements={['Unlock the AI Card Summoner License']}
       badgeSrc={SummonerBadge}
