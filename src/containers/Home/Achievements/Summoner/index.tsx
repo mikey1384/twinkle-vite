a42fa99293c098e23efebcfbb43b0ebc1c5fe05f
mikey1384 <mikey1384@gmail.com>
2023-07-12 10:40:17 +0700

    more refactoring

---
+++ src/containers/Home/Achievements/Summoner/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SummonerBadge from './summoner.png';
+import ItemPanel from '../ItemPanel';
+
+export default function Summoner({ style }: { style?: React.CSSProperties }) {
+  return (
+    <ItemPanel
+      style={style}
+      itemName="The Cybernetic Summoner"
+      description="Bestowed upon the select few who have unraveled the enigmas of the AI Card Conjuring. Your odyssey into the realms of AI wizardry has not only earned you this esteemed recognition but also transformed you into a revered Summoner of the Cybernetic Realm. Step forth, intrepid explorer of digital dimensions, your journey has just begun."
+      requirements={['Unlock the AI Card Summoner License']}
+      badgeSrc={SummonerBadge}
+    />
+  );
+}
--- src/containers/Home/Achievements/index.tsx
+++ src/containers/Home/Achievements/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { css } from '@emotion/css';
 import { Color, borderRadius, mobileMaxWidth } from '~/constants/css';
-import SummonerBadge from './summoner.png';
-import MentorBadge from './mentor.png';
-import SageBadge from './sage.png';
-import FounderBadge from './founder.png';
-import ItemPanel from './ItemPanel';
 import Mission from './Mission';
+import Summoner from './Summoner';
+import Mentor from './Mentor';
+import Sage from './Sage';
+import Founder from './Founder';
 
 export default function Achievements() {
   return (
@@ -38,34 +37,10 @@ export default function Achievements() {
         </p>
       </div>
       <Mission />
-      <ItemPanel
-        style={{ marginTop: '1rem' }}
-        itemName="The Cybernetic Summoner"
-        description="Bestowed upon the select few who have unraveled the enigmas of the AI Card Conjuring. Your odyssey into the realms of AI wizardry has not only earned you this esteemed recognition but also transformed you into a revered Summoner of the Cybernetic Realm. Step forth, intrepid explorer of digital dimensions, your journey has just begun."
-        requirements={['Unlock the AI Card Summoner License']}
-        badgeSrc={SummonerBadge}
-      />
-      <ItemPanel
-        style={{ marginTop: '1rem' }}
-        itemName="Starlight Mentor"
-        description="This honor is bestowed upon the exceptional individuals at Twinkle Academy who have taken up the noble task of guiding young minds towards knowledge. As a Starlight Mentor, your passion and wisdom illuminate the path of learning, inspiring those around you. Wear this badge with pride, for you are a beacon in the vast expanse of education."
-        requirements={['Take a full-time teaching position at Twinkle Academy']}
-        badgeSrc={MentorBadge}
-      />
-      <ItemPanel
-        style={{ marginTop: '1rem' }}
-        itemName="The Sage of Twinkle"
-        description="This highest honor is bestowed only upon those who have risen to the rank of Head Teacher or above at Twinkle Academy. As a Sage of Twinkle, you are a leader, an innovator, and a beacon of knowledge. Stand tall, for you light the path of learning for all."
-        requirements={['Rise to the rank of Head Teacher']}
-        badgeSrc={SageBadge}
-      />
-      <ItemPanel
-        style={{ marginTop: '1rem' }}
-        itemName="The Entrepreneur"
-        description="This badge is a testament to the daring innovators who have taken the leap to start their own business. As an Entrepreneur, you've done more than just create a company - you've realized a dream, transformed a vision into reality, and carved your own path in the business world. This badge celebrates your bold journey of entrepreneurship, symbolizing your resilience, your inventive spirit, and your steadfast dedication to bringing your unique business idea to life."
-        requirements={['Found a new business']}
-        badgeSrc={FounderBadge}
-      />
+      <Summoner style={{ marginTop: '1rem' }} />
+      <Mentor style={{ marginTop: '1rem' }} />
+      <Sage style={{ marginTop: '1rem' }} />
+      <Founder style={{ marginTop: '1rem' }} />
     </div>
   );
 }
