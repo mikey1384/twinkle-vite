528d1d70b813c85134b361580d8ab316f4526390
mikey1384 <mikey1384@gmail.com>
2023-07-17 20:40:45 +0700

    more refactoring

--- src/containers/Home/Achievements/Founder/index.tsx
+++ src/containers/Home/Achievements/Founder/index.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import FounderBadge from './founder.png';
 import ItemPanel from '../ItemPanel';
 
-export default function Founder({ style }: { style?: React.CSSProperties }) {
+export default function Founder({
+  data: { title, description },
+  style
+}: {
+  data: {
+    title: string;
+    description: string;
+  };
+  style?: React.CSSProperties;
+}) {
   return (
     <ItemPanel
       style={style}
-      itemName="The Founder"
-      description="This badge is a testament to the daring innovators who have taken the leap to start their own business. As an entrepreneur, you've done more than just create a company - you've realized a dream, transformed a vision into reality, and carved your own path in the business world. This badge celebrates your bold journey of entrepreneurship, symbolizing your resilience, your inventive spirit, and your steadfast dedication to bringing your unique business idea to life."
+      itemName={title}
+      description={description}
       requirements={['Found a new business']}
       badgeSrc={FounderBadge}
     />
--- src/containers/Home/Achievements/Summoner/index.tsx
+++ src/containers/Home/Achievements/Summoner/index.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import SummonerBadge from './summoner.png';
 import ItemPanel from '../ItemPanel';
 
-export default function Summoner({ style }: { style?: React.CSSProperties }) {
+export default function Summoner({
+  data: { title, description },
+  style
+}: {
+  data: {
+    title: string;
+    description: string;
+  };
+  style?: React.CSSProperties;
+}) {
   return (
     <ItemPanel
       style={style}
-      itemName="Cybernetic Summoner"
-      description="Bestowed upon the select few who have unraveled the enigmas of the AI Card Conjuring. Your odyssey into the realms of AI wizardry has not only earned you this esteemed recognition but also transformed you into a revered Summoner of the Cybernetic Realm. Step forth, intrepid explorer of digital dimensions, your journey has just begun."
+      itemName={title}
+      description={description}
       requirements={['Unlock the AI Card Summoner License']}
       badgeSrc={SummonerBadge}
     />
