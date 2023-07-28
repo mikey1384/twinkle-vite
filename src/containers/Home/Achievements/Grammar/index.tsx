e0f6cc12e72b441689224dfaadfe94695c9842b3
mikey1384 <mikey1384@gmail.com>
2023-07-28 09:05:58 +0700

    more updates

---
+++ src/containers/Home/Achievements/Grammar/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import FounderBadge from './founder.png';
+import ItemPanel from '../ItemPanel';
+
+export default function Grammar({
+  data: { isUnlocked, ap, title, description },
+  style
+}: {
+  data: {
+    ap: number;
+    isUnlocked: boolean;
+    title: string;
+    description: string;
+  };
+  style?: React.CSSProperties;
+}) {
+  return (
+    <ItemPanel
+      style={style}
+      ap={ap}
+      isUnlocked={isUnlocked}
+      itemName={title}
+      description={description}
+      requirement="Found a new business"
+      badgeSrc={FounderBadge}
+    />
+  );
+}
