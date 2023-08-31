4ef0ff8b0baeed7c8dd1fce9db37e0ebd8ba7b79
mikey1384 <mikey1384@gmail.com>
2023-08-31 20:38:39 +0700

    more updates

--- src/components/AchievementItems/Teenager/FormModal.tsx
+++ src/components/AchievementItems/Teenager/FormModal.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from '~/components/Modal';
 import Button from '~/components/Button';
 
 export default function FormModal({ onHide }: { onHide: () => void }) {
+  const [dob, setDob] = useState('');
   return (
     <Modal onHide={onHide}>
       <header>Additional Profile Details</header>
       <main>
-        <p>Form</p>
+        Enter Your Birthdate
+        <div style={{ marginTop: '1rem' }}>
+          <input
+            type="date"
+            value={dob}
+            onChange={(e) => setDob(e.target.value)}
+          />
+        </div>
       </main>
       <footer>
         <Button transparent onClick={onHide}>
