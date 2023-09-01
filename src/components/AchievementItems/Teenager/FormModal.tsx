c476a2194cbede98b2f9c4f08efb7b7a19a6f715
mikey1384 <mikey1384@gmail.com>
2023-09-01 10:07:17 +0700

    more updates

--- src/components/AchievementItems/Teenager/FormModal.tsx
+++ src/components/AchievementItems/Teenager/FormModal.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import Modal from '~/components/Modal';
 import Button from '~/components/Button';
+import { useKeyContext } from '~/contexts';
 
 export default function FormModal({ onHide }: { onHide: () => void }) {
+  const {
+    done: { color: doneColor }
+  } = useKeyContext((v) => v.theme);
   const [dob, setDob] = useState('');
   return (
     <Modal onHide={onHide}>
@@ -18,10 +22,18 @@ export default function FormModal({ onHide }: { onHide: () => void }) {
         </div>
       </main>
       <footer>
-        <Button transparent onClick={onHide}>
+        <Button transparent style={{ marginRight: '0.7rem' }} onClick={onHide}>
           Close
+        </Button>
+        <Button color={doneColor} onClick={handleSubmit}>
+          Submit
         </Button>
       </footer>
     </Modal>
   );
+
+  async function handleSubmit() {
+    console.log(dob);
+    onHide();
+  }
 }
