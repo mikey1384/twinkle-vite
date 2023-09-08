09ec25321fc3112914f90857d3c1ae0d6e596892
mikey1384 <mikey1384@gmail.com>
2023-09-08 09:25:01 +0700

    more updates

--- src/containers/Chat/Message/ApprovalRequest.tsx
+++ src/containers/Chat/Message/ApprovalRequest.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Button from '~/components/Button';
-import { useAppContext, useManagementContext } from '~/contexts';
-import { Color, liftedEffect } from '~/constants/css';
+import { useAppContext, useKeyContext, useManagementContext } from '~/contexts';
+import { borderRadius, Color, liftedEffect } from '~/constants/css';
 import { getAge } from '~/helpers';
 
 export default function ApprovalRequest({
@@ -14,6 +14,7 @@ export default function ApprovalRequest({
   username: string;
 }) {
   const content = '1984-01-03';
+  const { userId: myId } = useKeyContext((v) => v.myState);
   const approveDob = useAppContext((v) => v.requestHelpers.approveDob);
   const onApproveDob = useManagementContext((v) => v.actions.onApproveDob);
   const [submitting, setSubmitting] = useState(false);
@@ -22,80 +23,94 @@ export default function ApprovalRequest({
     <div
       style={{
         width: '100%',
-        padding: '2rem 1rem',
+        padding: '1rem',
         display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
         flexDirection: 'column',
-        marginTop: '3rem',
-        marginBottom: '3rem',
-        backgroundColor: Color.whiteGray(),
-        ...liftedEffect
+        alignItems: 'center'
       }}
     >
-      <div
-        style={{
-          marginTop: '1rem',
-          fontWeight: 'bold',
-          fontSize: '2rem',
-          color: Color.logoBlue()
-        }}
-      >
-        {username} {requestId}
+      <div style={{ borderRadius, color: Color.darkGray() }}>
+        {userId === myId ? 'requested approval' : 'requests your approval'}
       </div>
       <div
-        style={{ marginTop: '1.5rem', textAlign: 'center', lineHeight: 1.7 }}
+        style={{
+          width: '100%',
+          padding: '2rem 0',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          marginTop: '1.5rem',
+          marginBottom: '3rem',
+          backgroundColor: Color.whiteGray(),
+          ...liftedEffect
+        }}
       >
-        <p
+        <div
           style={{
-            color: Color.black(),
             fontWeight: 'bold',
-            fontSize: '1.6rem'
+            fontSize: '2rem',
+            color: Color.logoBlue()
           }}
         >
-          {content}
-        </p>
-        <p style={{ fontSize: '1.2rem', color: Color.darkerGray() }}>
-          ({getAge(content)} years old)
-        </p>
-      </div>
-      <div
-        style={{
-          marginTop: '3rem',
-          width: '100%',
-          display: 'flex',
-          justifyContent: 'center'
-        }}
-      >
-        <Button
-          color="rose"
-          filled
-          disabled={submitting}
-          loading={submitting && !isApproved}
-          onClick={() =>
-            handleSubmit({
-              isApproved: false,
-              userId
-            })
-          }
+          {username} {requestId}
+        </div>
+        <div
+          style={{ marginTop: '1.5rem', textAlign: 'center', lineHeight: 1.7 }}
         >
-          Reject
-        </Button>
-        <Button
-          style={{ marginLeft: '1.5rem' }}
-          filled
-          color="green"
-          disabled={submitting}
-          loading={submitting && isApproved}
-          onClick={() =>
-            handleSubmit({
-              isApproved: true,
-              userId
-            })
-          }
-        >
-          Approve
-        </Button>
+          <p
+            style={{
+              color: Color.black(),
+              fontWeight: 'bold',
+              fontSize: '1.6rem'
+            }}
+          >
+            {content}
+          </p>
+          <p style={{ fontSize: '1.2rem', color: Color.darkerGray() }}>
+            ({getAge(content)} years old)
+          </p>
+        </div>
+        {userId !== myId && (
+          <div
+            style={{
+              marginTop: '3rem',
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'center'
+            }}
+          >
+            <Button
+              color="rose"
+              filled
+              disabled={submitting}
+              loading={submitting && !isApproved}
+              onClick={() =>
+                handleSubmit({
+                  isApproved: false,
+                  userId
+                })
+              }
+            >
+              Reject
+            </Button>
+            <Button
+              style={{ marginLeft: '1.5rem' }}
+              filled
+              color="green"
+              disabled={submitting}
+              loading={submitting && isApproved}
+              onClick={() =>
+                handleSubmit({
+                  isApproved: true,
+                  userId
+                })
+              }
+            >
+              Approve
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   );
