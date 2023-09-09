10b2ba0128b060aba7031c0bef3946c98cc1bd59
mikey1384 <mikey1384@gmail.com>
2023-09-09 10:07:29 +0700

    more updates

---
+++ src/containers/Chat/Message/ApprovalRequest/Details/ApprovalResult.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { css } from '@emotion/css';
+import { Color, borderRadius } from '~/constants/css';
+import { useAppContext, useManagementContext } from '~/contexts';
+import Icon from '~/components/Icon';
+
+export default function ApprovalResult({
+  status,
+  userId
+}: {
+  status: string;
+  userId: number;
+}) {
+  const revertDobApproval = useAppContext(
+    (v) => v.requestHelpers.revertDobApproval
+  );
+  const onApproveDob = useManagementContext((v) => v.actions.onApproveDob);
+  const [reverting, setReverting] = useState(false);
+
+  return (
+    <div>
+      <div
+        style={{
+          marginTop: '2.5rem',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding: '1rem',
+          borderRadius,
+          boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
+          background:
+            Color[status === 'approved' ? 'limeGreen' : 'redOrange'](),
+          color: '#fff',
+          fontWeight: 'bold'
+        }}
+      >
+        <Icon
+          icon={status === 'approved' ? 'check' : 'times'}
+          style={{ marginRight: '0.7rem' }}
+        />
+        {status.toUpperCase()}
+      </div>
+      <div
+        className={css`
+          margin-top: 1rem;
+          font-size: 1.3rem;
+          color: ${reverting ? Color.lightGray() : Color.darkerGray()};
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        `}
+      >
+        <Icon icon="undo" style={{ marginRight: '0.5rem' }} />
+        <span
+          onClick={handleRevert}
+          className={css`
+            cursor: ${reverting ? 'default' : 'pointer'};
+            ${reverting ? '' : '&:hover { text-decoration: underline; }'}
+          `}
+        >
+          revert
+        </span>
+      </div>
+    </div>
+  );
+
+  async function handleRevert() {
+    setReverting(true);
+    const status = await revertDobApproval(userId);
+    onApproveDob({ userId, status });
+    setReverting(false);
+  }
+}
