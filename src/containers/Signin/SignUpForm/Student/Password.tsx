85b5048b58f0df2ef348792ce174c176c9cdbbed
mikey1384 <mikey1384@gmail.com>
2023-09-15 21:45:03 +0700

    more updates

--- src/containers/Signin/SignUpForm/Student/Password.tsx
+++ src/containers/Signin/SignUpForm/Student/Password.tsx
@@ -1,10 +1,5 @@
 import React, { useState } from 'react';
 import Input from '~/components/Texts/Input';
-import localize from '~/constants/localize';
-
-const passwordLabel = localize('password');
-const passwordsNeedToBeAtLeastLabel = localize('passwordsNeedToBeAtLeast');
-const setUpPasswordLabel = localize('setUpPassword');
 
 export default function Password({ onSubmit }: { onSubmit: () => void }) {
   const [password, setPassword] = useState('');
@@ -12,26 +7,37 @@ export default function Password({ onSubmit }: { onSubmit: () => void }) {
 
   return (
     <div>
-      <section>
-        <label>{passwordLabel}</label>
-        <Input
-          value={password}
-          hasError={errorMessage === 'password'}
-          placeholder={setUpPasswordLabel}
-          onChange={(text) => {
-            setErrorMessage('');
-            setPassword(text.trim());
-          }}
-          onKeyPress={(event: any) => {
-            if (event.key === 'Enter') {
-              onSubmit();
-            }
-          }}
-          type="password"
-        />
-        {errorMessage === 'password' && (
-          <p style={{ color: 'red' }}>{passwordsNeedToBeAtLeastLabel}</p>
-        )}
+      <section style={{ display: 'flex', justifyContent: 'center' }}>
+        <div style={{ textAlign: 'center' }}>
+          <div>
+            <label>Password (5 characters or longer)</label>
+          </div>
+          <Input
+            autoFocus
+            value={password}
+            style={{ width: 'auto' }}
+            hasError={errorMessage === 'password'}
+            placeholder="Password..."
+            onChange={(text) => {
+              setErrorMessage('');
+              setPassword(text.trim());
+            }}
+            onKeyPress={(event: any) => {
+              if (event.key === 'Enter') {
+                onSubmit();
+              }
+            }}
+            type="password"
+          />
+          <p style={{ marginTop: '1rem', fontSize: '1.3rem' }}>
+            You MUST remember your password. Write it down somewhere!
+          </p>
+          {errorMessage === 'password' && (
+            <p style={{ color: 'red' }}>
+              Passwords need to be at least 5 characters long
+            </p>
+          )}
+        </div>
       </section>
     </div>
   );
