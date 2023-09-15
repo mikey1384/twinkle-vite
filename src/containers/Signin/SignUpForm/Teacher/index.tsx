f644fec5bce2dabd5609af4c4894d71073838d3e
mikey1384 <mikey1384@gmail.com>
2023-09-15 20:58:14 +0700

    more fix

--- src/containers/Signin/SignUpForm/Teacher/index.tsx
+++ src/containers/Signin/SignUpForm/Teacher/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 export default function TeacherForm({
-  onSetUsername
+  onSetUsername,
+  onSetIsTeacherFormComplete
 }: {
   onSetUsername: (username: string) => void;
+  onSetIsTeacherFormComplete: (value: boolean) => void;
 }) {
-  console.log(onSetUsername);
+  console.log(onSetUsername, onSetIsTeacherFormComplete);
   return (
     <div>
       <div>Teacher Form</div>
