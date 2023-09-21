26b1c7ab3db35ad4231024bbac4a948c8d38845e
mikey1384 <mikey1384@gmail.com>
2023-09-21 20:44:46 +0700

    more updates

--- src/containers/Chat/Message/ApprovalRequest/Container/Details/Teacher.tsx
+++ src/containers/Chat/Message/ApprovalRequest/Container/Details/Teacher.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function Teacher({ content }: { content: string }) {
-  return <div>{content}</div>;
+  return (
+    <div>
+      <div>{content}</div>
+      {`Hi, my name is XXX and I'm a teacher at XXX, XXX. I would like to request a teacher account.`}
+    </div>
+  );
 }
