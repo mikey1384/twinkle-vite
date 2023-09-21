b7e02c567459bcb09dff5a8bd55aeb1d8b6e2c64
mikey1384 <mikey1384@gmail.com>
2023-09-21 17:05:28 +0700

    more updates

---
+++ src/containers/Chat/Message/ApprovalRequest/Container/Details/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Color } from '~/constants/css';
+import { getAge } from '~/helpers';
+
+export default function Details({
+  content,
+  type
+}: {
+  content: string;
+  type: string;
+}) {
+  return (
+    <div style={{ width: '100%' }}>
+      <div
+        style={{ marginTop: '1.5rem', textAlign: 'center', lineHeight: 1.7 }}
+      >
+        <p
+          style={{
+            color: Color.black(),
+            fontWeight: 'bold',
+            fontSize: '1.6rem'
+          }}
+        >
+          {content} {type}
+        </p>
+        <p style={{ fontSize: '1.2rem', color: Color.darkerGray() }}>
+          ({getAge(content)} years old)
+        </p>
+      </div>
+    </div>
+  );
+}
