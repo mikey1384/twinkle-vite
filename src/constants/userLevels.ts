37145fbc1340e11938c223858aa8de7310fe0ebf
mikey1384 <mikey1384@gmail.com>
2023-09-26 11:14:55 +0700

    more updates

---
+++ src/constants/userLevels.ts
@@ -0,0 +1,62 @@
+export const levels = [
+  {
+    ap: 0,
+    labels: [],
+    level: 0,
+    canEdit: false,
+    canDelete: false,
+    canReward: false,
+    canEditDictionary: false,
+    canPinPlaylists: false,
+    canEditPlaylists: false,
+    canEditRewardLevel: false
+  },
+  {
+    ap: 0,
+    labels: [],
+    level: 1,
+    canEdit: false,
+    canDelete: false,
+    canReward: false,
+    canEditDictionary: false,
+    canPinPlaylists: false,
+    canEditPlaylists: false,
+    canEditRewardLevel: false
+  },
+  {
+    ap: 150,
+    labels: ['Moderator'],
+    level: 2,
+    canEdit: false,
+    canDelete: true,
+    canReward: true,
+    canEditDictionary: false,
+    canPinPlaylists: false,
+    canEditPlaylists: false,
+    canEditRewardLevel: false
+  },
+  {
+    ap: 300,
+    labels: ['Moderator', 'Mod II'],
+    level: 3,
+    canEdit: false,
+    canDelete: true,
+    canReward: true,
+    canEditDictionary: false,
+    canPinPlaylists: false,
+    canEditPlaylists: false,
+    canEditRewardLevel: false
+  },
+  {
+    ap: 1000,
+    labels: ['Teacher', 'Moderator', 'Mod III'],
+    level: 4,
+    canEdit: false,
+    canDelete: true,
+    canReward: true,
+    canEditDictionary: true,
+    canPinPlaylists: false,
+    canEditPlaylists: false,
+    canEditRewardLevel: true
+  }
+];
