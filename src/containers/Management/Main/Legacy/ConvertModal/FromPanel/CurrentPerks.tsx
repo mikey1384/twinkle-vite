db191a7a80172893aab60ccad433df350219cf93
mikey1384 <mikey1384@gmail.com>
2023-10-04 18:06:15 +0700

    more updates

--- src/containers/Management/Main/Legacy/ConvertModal/FromPanel/CurrentPerks.tsx
+++ src/containers/Management/Main/Legacy/ConvertModal/FromPanel/CurrentPerks.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Table from '../../../Table';
-import Check from '../../../Check';
+import Table from '../../../../Table';
+import Check from '../../../../Check';
 import { Color } from '~/constants/css';
 import { User } from '~/types';
 
---
+++ src/containers/Management/Main/Legacy/ConvertModal/FromPanel/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import CurrentPerks from './CurrentPerks';
+import AchievementStatus from './AchievementStatus';
+import { css } from '@emotion/css';
+import { User } from '~/types';
+
+export default function FromPanel({ target }: { target: User }) {
+  return (
+    <div
+      className={css`
+        width: 100%;
+      `}
+    >
+      <div
+        className={css`
+          font-weight: bold;
+          font-family: Roboto, sans-serif;
+          margin: 1rem 0;
+        `}
+      >
+        From
+      </div>
+      <div
+        className={css`
+          width: 100%;
+          border-radius: 8px;
+          border: 1px solid #ccc;
+          padding: 1rem;
+          margin-bottom: 1rem;
+        `}
+      >
+        <CurrentPerks target={target} />
+        <AchievementStatus target={target} />
+      </div>
+    </div>
+  );
+}
