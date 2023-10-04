092606d8abd7eeaf9ca802baf34e16b1a33393fb
mikey1384 <mikey1384@gmail.com>
2023-10-04 18:16:59 +0700

    minor fix

---
+++ src/containers/Management/Main/Legacy/ConvertModal/ToPanel/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { css } from '@emotion/css';
+import { User } from '~/types';
+
+export default function ToPanel({ target }: { target: User }) {
+  return (
+    <div
+      className={css`
+        width: 100%;
+      `}
+    >
+      <div
+        className={css`
+          margin: 2rem 0rem 1rem 0;
+          font-weight: bold;
+          font-family: Roboto, sans-serif;
+        `}
+      >
+        To
+      </div>
+      <div
+        className={css`
+          width: 100%;
+          border-radius: 8px;
+          border: 1px solid #ccc;
+          padding: 1rem;
+          margin-bottom: 1rem;
+        `}
+      >
+        <div>{target.username}</div>
+      </div>
+    </div>
+  );
+}
