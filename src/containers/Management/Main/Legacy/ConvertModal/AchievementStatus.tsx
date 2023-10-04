629f05729b436becb3ca5b5da3427bc76a64712a
mikey1384 <mikey1384@gmail.com>
2023-10-04 16:54:20 +0700

    more updates

--- src/containers/Management/Main/Legacy/ConvertModal/AchievementStatus.tsx
+++ src/containers/Management/Main/Legacy/ConvertModal/AchievementStatus.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Loading from '~/components/Loading';
 import AchievementItem from '~/components/AchievementItem';
 import { useAppContext } from '~/contexts';
-import { User } from '~/types';
+import { Content, User } from '~/types';
 
 export default function AchievementStatus({ target }: { target: User }) {
   const loadAchievementsByUserId = useAppContext(
@@ -10,20 +10,28 @@ export default function AchievementStatus({ target }: { target: User }) {
   );
   const [achievements, setAchievements] = useState([]);
   const [loading, setLoading] = useState(false);
+
   useEffect(() => {
-    init();
     async function init() {
       setLoading(true);
       const data = await loadAchievementsByUserId(target.id);
       setAchievements(data);
       setLoading(false);
     }
+
+    init();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return loading ? (
     <Loading />
   ) : (
-    <AchievementItem isSmall achievement={achievements[0]} />
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      {achievements.map((achievement: Content) => (
+        <div style={{ flex: '0 0 50%' }} key={achievement.id}>
+          <AchievementItem isSmall achievement={achievement} />
+        </div>
+      ))}
+    </div>
   );
 }
