bc99bd1b4390841bc9491f03a5cb9f43eeace005
mikey1384 <mikey1384@gmail.com>
2023-10-05 09:37:05 +0700

    more updates

--- src/containers/Management/Main/Legacy/ConvertModal/ToPanel/NewStats.tsx
+++ src/containers/Management/Main/Legacy/ConvertModal/ToPanel/NewStats.tsx
@@ -42,11 +42,14 @@ const newStatsPerUserTypes: {
 };
 
 const achievementAP: Record<string, number> = {
+  grammar: 70,
+  mission: 70,
+  summoner: 70,
+  teenager: 100,
+  adult: 100,
   mentor: 800,
   sage: 500,
-  twinkle_founder: 1500,
-  teenager: 100,
-  adult: 100
+  twinkle_founder: 1500
 };
 
 export default function NewStats({ target }: { target: User }) {
