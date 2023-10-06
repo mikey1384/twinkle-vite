7362b4694717831e44f82c1e914975c4342c1e5d
mikey1384 <mikey1384@gmail.com>
2023-10-06 07:59:47 +0700

    more updates

---
+++ src/containers/Management/Main/Legacy/ConvertModal/ToPanel/NewAchievementStatus.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Loading from '~/components/Loading';
+import AchievementItem from '~/components/AchievementItem';
+import { Content } from '~/types';
+
+export default function NewAchievementStatus({
+  achievements,
+  loading
+}: {
+  achievements: Content[];
+  loading: boolean;
+}) {
+  return loading ? (
+    <Loading />
+  ) : (
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      {achievements.map((achievement: Content) => (
+        <div style={{ flex: '0 0 50%' }} key={achievement.id}>
+          <AchievementItem isSmall achievement={achievement} />
+        </div>
+      ))}
+    </div>
+  );
+}
