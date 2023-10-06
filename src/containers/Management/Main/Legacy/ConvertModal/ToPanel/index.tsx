93ffc7ebdc2c7a33df687e6c662b03ee1f07a56d
mikey1384 <mikey1384@gmail.com>
2023-10-06 08:05:16 +0700

    more updates

--- src/containers/Management/Main/Legacy/ConvertModal/ToPanel/index.tsx
+++ src/containers/Management/Main/Legacy/ConvertModal/ToPanel/index.tsx
@@ -1,17 +1,121 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import NewStats from './NewStats';
+import NewAchievementStatus from './NewAchievementStatus';
 import { css } from '@emotion/css';
 import { Content, User } from '~/types';
+import { levels } from '~/constants/userLevels';
+import { StatsProp } from './types';
+
+const newStatsPerUserTypes: {
+  [role: string]: {
+    title: string | null;
+    achievements: string[];
+  };
+} = {
+  moderator: {
+    title: 'moderator',
+    achievements: ['teenager']
+  },
+  programmer: {
+    title: 'programmer',
+    achievements: ['teenager']
+  },
+  ['senior programmer']: {
+    title: null,
+    achievements: ['teenager', 'adult']
+  },
+  ["mikey's friend"]: {
+    title: null,
+    achievements: ['teenager', 'adult']
+  },
+  teacher: {
+    title: 'teacher',
+    achievements: ['teenager', 'adult', 'mentor']
+  },
+  headteacher: {
+    title: 'headteacher',
+    achievements: ['teenager', 'adult', 'mentor', 'sage']
+  },
+  headmaster: {
+    title: 'headmaster',
+    achievements: ['teenager', 'adult', 'mentor', 'sage', 'twinkle_founder']
+  }
+};
 
 export default function ToPanel({
   achievementAP,
   achievements,
+  loading,
   target
 }: {
   achievementAP: Record<string, number>;
   achievements: Content[];
+  loading: boolean;
   target: User;
 }) {
+  const newStats: StatsProp = useMemo(() => {
+    const currentAchievementTypes = achievements.map(
+      (achievement) => achievement.type
+    );
+
+    // Get the achievements for the given user type
+    const unlockableAchievementTypes =
+      newStatsPerUserTypes[target.userType]?.achievements || [];
+
+    // Combine current and unlockable achievement types and remove duplicates
+    const uniqueAchievementTypes = Array.from(
+      new Set([...currentAchievementTypes, ...unlockableAchievementTypes])
+    );
+
+    // Calculate total achievement points (AP)
+    let totalAP = 0;
+    for (const uniqueAchievementType of uniqueAchievementTypes) {
+      totalAP += achievementAP[uniqueAchievementType] || 0;
+    }
+
+    // Determine the user's level based on total AP
+    let userLevel = levels[0].level; // Default to the first level
+    for (let i = levels.length - 1; i >= 0; i--) {
+      if (totalAP >= levels[i].ap) {
+        userLevel = levels[i].level;
+        break;
+      }
+    }
+
+    // Get the perks for the determined level
+    const perks: {
+      level: number;
+      canEdit: boolean;
+      canDelete: boolean;
+      canReward: boolean;
+      canPinPlaylists: boolean;
+      canEditPlaylists: boolean;
+      canEditRewardLevel: boolean;
+    } = levels.find((level) => level.level === userLevel) || {
+      level: 0,
+      canEdit: false,
+      canDelete: false,
+      canReward: false,
+      canPinPlaylists: false,
+      canEditPlaylists: false,
+      canEditRewardLevel: false
+    };
+
+    // Create the new stats object
+    return {
+      ...perks,
+      username: target.username,
+      realName: target.realName,
+      title: newStatsPerUserTypes[target.userType]?.title
+    };
+  }, [
+    achievementAP,
+    achievements,
+    target.realName,
+    target.userType,
+    target.username
+  ]);
+
   return (
     <div
       className={css`
@@ -36,11 +140,8 @@ export default function ToPanel({
           margin-bottom: 1rem;
         `}
       >
-        <NewStats
-          achievementAP={achievementAP}
-          achievements={achievements}
-          target={target}
-        />
+        <NewStats newStats={newStats} target={target} />
+        <NewAchievementStatus achievements={achievements} loading={loading} />
       </div>
     </div>
   );
