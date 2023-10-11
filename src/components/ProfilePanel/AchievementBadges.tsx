dabf4d54e68c5565791e7150a27b72a516c0a8e8
mikey1384 <mikey1384@gmail.com>
2023-10-11 09:49:02 +0700

    more updates

--- src/components/ProfilePanel/AchievementBadges.tsx
+++ src/components/ProfilePanel/AchievementBadges.tsx
@@ -1,13 +1,21 @@
 import React, { useEffect } from 'react';
 import ErrorBoundary from '~/components/ErrorBoundary';
+import { useAppContext } from '~/contexts';
+import { achievementIdToType } from '~/constants/defaultValues';
 
 export default function AchievementBadges({
   unlockedAchievementIds = []
 }: {
   unlockedAchievementIds: number[];
 }) {
+  const achievementsObj = useAppContext((v) => v.user.state.achievementsObj);
   useEffect(() => {
-    console.log(unlockedAchievementIds);
+    console.log(achievementsObj);
+    console.log(
+      unlockedAchievementIds.map(
+        (achievementId) => achievementIdToType[achievementId]
+      )
+    );
   }, []);
 
   return (
