53b62d5cb211192243ee20b4d46251edff918002
mikey1384 <mikey1384@gmail.com>
2023-10-14 18:32:53 +0700

    more updates

---
+++ src/containers/Management/ModActivities/FileAttachment/MediaPlayer.tsx
@@ -0,0 +1,140 @@
+import React, { useEffect, useMemo, useRef } from 'react';
+import ExtractedThumb from '~/components/ExtractedThumb';
+import ReactPlayer from 'react-player';
+import { v1 as uuidv1 } from 'uuid';
+import { useAppContext, useContentContext } from '~/contexts';
+import { useContentState } from '~/helpers/hooks';
+import { isMobile, returnImageFileFromUrl } from '~/helpers';
+
+const deviceIsMobile = isMobile(navigator);
+
+export default function MediaPlayer({
+  messageId,
+  fileType,
+  onPause = () => null,
+  onPlay = () => null,
+  src,
+  thumbUrl
+}: {
+  messageId: number;
+  fileType: string;
+  onPause?: () => void;
+  onPlay?: () => void;
+  src: string;
+  thumbUrl: string;
+}) {
+  const uploadThumb = useAppContext((v) => v.requestHelpers.uploadThumb);
+  const onSetThumbUrl = useContentContext((v) => v.actions.onSetThumbUrl);
+  const onSetVideoCurrentTime = useContentContext(
+    (v) => v.actions.onSetVideoCurrentTime
+  );
+  const { currentTime = 0 } = useContentState({
+    contentType: 'chat',
+    contentId: messageId
+  });
+  const timeAtRef = useRef(0);
+  const PlayerRef: React.RefObject<any> = useRef(null);
+
+  useEffect(() => {
+    if (currentTime > 0) {
+      PlayerRef.current?.seekTo(currentTime);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    return function setCurrentTimeBeforeUnmount() {
+      if (timeAtRef.current > 0) {
+        onSetVideoCurrentTime({
+          contentType: 'chat',
+          contentId: messageId,
+          currentTime: timeAtRef.current
+        });
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const isNotLight = useMemo(
+    () => fileType === 'audio' || currentTime || (!deviceIsMobile && !thumbUrl),
+    [currentTime, fileType, thumbUrl]
+  );
+
+  const light = useMemo(() => {
+    if (isNotLight) {
+      return false;
+    }
+    return thumbUrl;
+  }, [isNotLight, thumbUrl]);
+
+  return (
+    <div
+      style={{
+        marginTop: '1rem',
+        width: '100%',
+        height: 'CALC(100% - 3rem)',
+        position: 'relative'
+      }}
+    >
+      {fileType !== 'audio' && !thumbUrl && (
+        <ExtractedThumb
+          isHidden
+          src={src}
+          style={{ width: '100%' }}
+          onThumbnailLoad={handleThumbnailLoad}
+          thumbUrl={thumbUrl}
+        />
+      )}
+      <ReactPlayer
+        light={light}
+        ref={PlayerRef}
+        playsinline
+        onPlay={onPlay}
+        onPause={onPause}
+        onProgress={handleVideoProgress}
+        onReady={handleReady}
+        style={{
+          width: '100%',
+          height: '100%',
+          paddingBottom:
+            fileType === 'audio' || fileType === 'video' ? '1rem' : 0
+        }}
+        width="100%"
+        height={fileType === 'video' ? '100%' : '5rem'}
+        url={src}
+        controls
+      />
+    </div>
+  );
+
+  function handleReady() {
+    if (light) {
+      PlayerRef.current?.getInternalPlayer?.()?.play?.();
+    }
+  }
+
+  function handleThumbnailLoad(thumb: string) {
+    const file = returnImageFileFromUrl({
+      imageUrl: thumb
+    });
+    handleUploadThumb();
+
+    async function handleUploadThumb() {
+      const thumbUrl = await uploadThumb({
+        contentType: 'chat',
+        contentId: messageId,
+        file,
+        path: uuidv1()
+      });
+      onSetThumbUrl({
+        contentId: messageId,
+        contentType: 'chat',
+        thumbUrl
+      });
+    }
+  }
+
+  function handleVideoProgress() {
+    timeAtRef.current = PlayerRef.current.getCurrentTime();
+  }
+}
---
+++ src/containers/Management/ModActivities/FileAttachment/index.tsx
@@ -0,0 +1,117 @@
+import React, { useMemo, useState } from 'react';
+import FileInfo from './FileInfo';
+import ImagePreview from './ImagePreview';
+import MediaPlayer from './MediaPlayer';
+import { css } from '@emotion/css';
+import { Color, mobileMaxWidth } from '~/constants/css';
+import { getFileInfoFromFileName } from '~/helpers/stringHelpers';
+import { cloudFrontURL } from '~/constants/defaultValues';
+
+export default function FileAttachment({
+  messageId,
+  fileName,
+  filePath,
+  fileSize,
+  thumbUrl
+}: {
+  messageId: number;
+  fileName: string;
+  filePath: string;
+  fileSize: number;
+  thumbUrl: string;
+}) {
+  const isImageOrVideo = useMemo(
+    () =>
+      getFileInfoFromFileName(fileName)?.fileType === 'image' ||
+      getFileInfoFromFileName(fileName)?.fileType === 'video',
+    [fileName]
+  );
+  const { fileType } = useMemo(
+    () => getFileInfoFromFileName(fileName),
+    [fileName]
+  );
+  const src = useMemo(
+    () =>
+      `${cloudFrontURL}/attachments/chat/${filePath}/${encodeURIComponent(
+        fileName
+      )}`,
+    [fileName, filePath]
+  );
+  const [imageWorks, setImageWorks] = useState(true);
+
+  return (
+    <div
+      className={css`
+        margin-top: 1rem;
+        width: 100%;
+        min-height: 9rem;
+        height: ${isImageOrVideo && imageWorks ? '41rem' : 'auto'};
+        @media (max-width: ${mobileMaxWidth}) {
+          min-height: 8rem;
+          height: ${isImageOrVideo && imageWorks ? '23rem' : 'auto'};
+        }
+      `}
+    >
+      {fileType === 'image' ? (
+        imageWorks ? (
+          <ImagePreview
+            src={src}
+            fileName={fileName}
+            onSetImageWorks={setImageWorks}
+          />
+        ) : (
+          <FileInfo
+            fileName={fileName}
+            fileType={fileType}
+            fileSize={fileSize}
+            src={src}
+          />
+        )
+      ) : fileType === 'video' || fileType === 'audio' ? (
+        <div
+          style={{
+            width: '100%',
+            height: '100%'
+          }}
+        >
+          <div
+            style={{
+              width: '100%',
+              height: 'auto'
+            }}
+          >
+            <a
+              style={{
+                width: '100%',
+                fontWeight: 'bold',
+                color: Color.logoBlue(),
+                overflow: 'hidden',
+                display: '-webkit-box',
+                WebkitLineClamp: 2,
+                WebkitBoxOrient: 'vertical'
+              }}
+              href={src}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {fileName}
+            </a>
+          </div>
+          <MediaPlayer
+            messageId={messageId}
+            fileType={fileType}
+            src={src}
+            thumbUrl={thumbUrl}
+          />
+        </div>
+      ) : (
+        <FileInfo
+          fileName={fileName}
+          fileType={fileType}
+          fileSize={fileSize}
+          src={src}
+        />
+      )}
+    </div>
+  );
+}
