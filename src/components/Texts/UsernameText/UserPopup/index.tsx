6041ed198e2a3444303246e5dd7d3d05bb274754
mikey1384 <mikey1384@gmail.com>
2023-11-03 18:04:39 +0700

    more fixes

--- src/components/Texts/UsernameText/UserPopup/index.tsx
+++ src/components/Texts/UsernameText/UserPopup/index.tsx
@@ -45,61 +45,65 @@ export default function UserPopup({
       onMouseLeave={onMouseLeave}
       style={{ minWidth: '10rem' }}
     >
-      <li
-        style={{
-          color: Color.darkerGray()
-        }}
-        onClick={() => navigate(`/users/${username}`)}
-      >
-        <Icon icon="user" />
-        <span style={{ marginLeft: '1rem' }}>{profileLabel}</span>
-      </li>
-      {userId !== myId && (
-        <li
-          style={{
-            color: Color.darkerGray()
-          }}
-          onClick={onLinkClick}
-        >
-          <Icon icon="comment" />
-          <span style={{ marginLeft: '1rem' }}>{chatLabel}</span>
-        </li>
-      )}
-      {userXP && (
-        <li
-          style={{
-            padding: '5px',
-            background:
-              !!userRank && userRank < 4
-                ? Color.darkerGray()
-                : Color.highlightGray(),
-            color: !!userRank && userRank < 4 ? '#fff' : Color.darkerGray(),
-            fontSize: '1rem',
-            textAlign: 'center',
-            fontWeight: 'bold'
-          }}
-        >
-          {userXP} XP
-          {!!userRank && userRank < 4 ? (
-            <span
+      <div style={{ width: '30rem' }}>
+        <div style={{ padding: '1rem' }}>
+          <div
+            style={{
+              color: Color.darkerGray()
+            }}
+            onClick={() => navigate(`/users/${username}`)}
+          >
+            <Icon icon="user" />
+            <span style={{ marginLeft: '1rem' }}>{profileLabel}</span>
+          </div>
+          {userId !== myId && (
+            <div
               style={{
-                fontWeight: 'bold',
-                color:
-                  userRank === 1
-                    ? Color.gold()
-                    : userRank === 2
-                    ? '#fff'
-                    : Color.orange()
+                color: Color.darkerGray()
+              }}
+              onClick={onLinkClick}
+            >
+              <Icon icon="comment" />
+              <span style={{ marginLeft: '1rem' }}>{chatLabel}</span>
+            </div>
+          )}
+          {userXP && (
+            <div
+              style={{
+                padding: '5px',
+                background:
+                  !!userRank && userRank < 4
+                    ? Color.darkerGray()
+                    : Color.highlightGray(),
+                color: !!userRank && userRank < 4 ? '#fff' : Color.darkerGray(),
+                fontSize: '1rem',
+                textAlign: 'center',
+                fontWeight: 'bold'
               }}
             >
-              {' '}
-              (#{userRank})
-            </span>
-          ) : (
-            ''
+              {userXP} XP
+              {!!userRank && userRank < 4 ? (
+                <span
+                  style={{
+                    fontWeight: 'bold',
+                    color:
+                      userRank === 1
+                        ? Color.gold()
+                        : userRank === 2
+                        ? '#fff'
+                        : Color.orange()
+                  }}
+                >
+                  {' '}
+                  (#{userRank})
+                </span>
+              ) : (
+                ''
+              )}
+            </div>
           )}
-        </li>
-      )}
+        </div>
+      </div>
     </Popup>
   );
 }
