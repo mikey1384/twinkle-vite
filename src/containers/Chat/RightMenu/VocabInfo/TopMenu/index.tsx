9f76af563d52c2549b00b4b74cc11c4c08f24abb
mikey1384 <mikey1384@gmail.com>
2023-11-09 15:24:34 +0700

    more fix

--- src/containers/Chat/RightMenu/VocabInfo/TopMenu/index.tsx
+++ src/containers/Chat/RightMenu/VocabInfo/TopMenu/index.tsx
@@ -4,6 +4,7 @@ import { useChatContext, useKeyContext } from '~/contexts';
 import Collector from './Collector';
 import FilterBar from '~/components/FilterBar';
 import localize from '~/constants/localize';
+import Loading from '~/components/Loading';
 
 const collectorsOfHighLevelWordsLabel = localize('collectorsOfHighLevelWords');
 const rankingsLabel = localize('rankings');
@@ -12,6 +13,7 @@ const top30Label = localize('top30');
 export default function TopMenu() {
   const { numWordsCollected } = useKeyContext((v) => v.myState);
   const { all, top30s } = useChatContext((v) => v.state.wordCollectors);
+  const loadingVocabulary = useChatContext((v) => v.state.loadingVocabulary);
   const [allSelected, setAllSelected] = useState(numWordsCollected > 0);
   const wordCollectors = useMemo(
     () => (allSelected ? all : top30s),
@@ -34,56 +36,62 @@ export default function TopMenu() {
         overflow: 'scroll'
       }}
     >
-      <div
-        style={{
-          fontSize: '1.7rem',
-          padding: '1rem',
-          textAlign: 'center',
-          fontWeight: 'bold',
-          background: Color.brownOrange(),
-          color: '#fff'
-        }}
-      >
-        {collectorsOfHighLevelWordsLabel}
-      </div>
-      {numWordsCollected > 0 && (
-        <FilterBar style={{ fontSize: '1.5rem', height: '4rem' }}>
-          <nav
-            onClick={() => setAllSelected(true)}
-            className={allSelected ? 'active' : ''}
+      {loadingVocabulary ? (
+        <Loading style={{ height: '100%' }} />
+      ) : (
+        <>
+          <div
+            style={{
+              fontSize: '1.7rem',
+              padding: '1rem',
+              textAlign: 'center',
+              fontWeight: 'bold',
+              background: Color.brownOrange(),
+              color: '#fff'
+            }}
           >
-            {rankingsLabel}
-          </nav>
-          <nav
-            onClick={() => setAllSelected(false)}
-            className={!allSelected ? 'active' : ''}
-          >
-            {top30Label}
-          </nav>
-        </FilterBar>
-      )}
-      <div style={{ marginTop: '1rem' }}>
-        {wordCollectors
-          .filter(
-            (collector: { numWordsCollected: number }) =>
-              collector.numWordsCollected > 0
-          )
-          .map(
-            (collector: {
-              id: number;
-              username: string;
-              rank: number;
-              profilePicUrl: string;
-              numWordsCollected: number;
-            }) => (
-              <Collector
-                key={collector.username}
-                style={{ padding: '1rem' }}
-                user={collector}
-              />
-            )
+            {collectorsOfHighLevelWordsLabel}
+          </div>
+          {numWordsCollected > 0 && (
+            <FilterBar style={{ fontSize: '1.5rem', height: '4rem' }}>
+              <nav
+                onClick={() => setAllSelected(true)}
+                className={allSelected ? 'active' : ''}
+              >
+                {rankingsLabel}
+              </nav>
+              <nav
+                onClick={() => setAllSelected(false)}
+                className={!allSelected ? 'active' : ''}
+              >
+                {top30Label}
+              </nav>
+            </FilterBar>
           )}
-      </div>
+          <div style={{ marginTop: '1rem' }}>
+            {wordCollectors
+              .filter(
+                (collector: { numWordsCollected: number }) =>
+                  collector.numWordsCollected > 0
+              )
+              .map(
+                (collector: {
+                  id: number;
+                  username: string;
+                  rank: number;
+                  profilePicUrl: string;
+                  numWordsCollected: number;
+                }) => (
+                  <Collector
+                    key={collector.username}
+                    style={{ padding: '1rem' }}
+                    user={collector}
+                  />
+                )
+              )}
+          </div>
+        </>
+      )}
     </div>
   );
 }
