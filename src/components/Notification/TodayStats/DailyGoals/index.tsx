2af8f220fb3f2b883fed0be72985b70414f39a86
mikey1384 <mikey1384@gmail.com>
2023-12-09 10:15:36 +0700

    more updates

--- src/components/Notification/TodayStats/DailyGoals/index.tsx
+++ src/components/Notification/TodayStats/DailyGoals/index.tsx
@@ -1,59 +1,11 @@
 import React from 'react';
+import Badge from './Badge';
 import { css } from '@emotion/css';
 
 export default function DailyGoals() {
-  const achievedGoals = ['W', 'G', 'A'];
+  const achievedGoals = [];
 
   const isAchieved = (goal: any) => achievedGoals.includes(goal);
-
-  const badgeStyle = css`
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    width: 40px;
-    height: 40px;
-    margin: 0.5rem;
-    border-radius: 50%;
-    color: white;
-    font-weight: bold;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    cursor: pointer;
-    transition: background-color 0.3s ease, transform 0.3s ease;
-
-    &.W {
-      &:hover {
-        transform: scale(1.1);
-        background: ${isAchieved('W')
-          ? ''
-          : `linear-gradient(135deg, rgba(255, 140, 0, 0.3) 0%, rgba(255, 192, 64, 0.3) 100%)`};
-      }
-      background: ${isAchieved('W')
-        ? `linear-gradient(135deg, #ff8c00 0%, #ffc040 100%)`
-        : 'var(--color-not-achieved)'};
-    }
-    &.G {
-      &:hover {
-        transform: scale(1.1);
-        background: ${isAchieved('G')
-          ? ''
-          : `linear-gradient(135deg, rgba(219, 0, 118, 0.3) 0%, rgba(255, 64, 136, 0.3) 100%)`};
-      }
-      background: ${isAchieved('G')
-        ? `linear-gradient(135deg, #db0076 0%, #ff4088 100%)`
-        : 'var(--color-not-achieved)'};
-    }
-    &.A {
-      &:hover {
-        transform: scale(1.1);
-        background: ${isAchieved('A')
-          ? ''
-          : `linear-gradient(135deg, rgba(0, 71, 171, 0.3) 0%, rgba(64, 140, 255, 0.3) 100%)`};
-      }
-      background: ${isAchieved('A')
-        ? `linear-gradient(135deg, #0047ab 0%, #408cff 100%)`
-        : 'var(--color-not-achieved)'};
-    }
-  `;
 
   const containerStyle = css`
     display: flex;
@@ -68,9 +20,9 @@ export default function DailyGoals() {
           --color-not-achieved: #b3b3b3;
         }`}
       </style>
-      <div className={`${badgeStyle} W`}>W</div>
-      <div className={`${badgeStyle} G`}>G</div>
-      <div className={`${badgeStyle} A`}>A</div>
+      <Badge isAchieved={isAchieved('W')}>W</Badge>
+      <Badge isAchieved={isAchieved('G')}>G</Badge>
+      <Badge isAchieved={isAchieved('A')}>A</Badge>
     </div>
   );
 }
