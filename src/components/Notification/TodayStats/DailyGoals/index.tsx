b10db8e5028daf8a25f966ef040fff4ccca669cd
mikey1384 <mikey1384@gmail.com>
2023-12-10 09:24:22 +0700

    more updates

--- src/components/Notification/TodayStats/DailyGoals/index.tsx
+++ src/components/Notification/TodayStats/DailyGoals/index.tsx
@@ -13,20 +13,78 @@ export default function DailyGoals({
     (goal: any) => achievedGoals.includes(goal),
     [achievedGoals]
   );
+  const allGoalsAchieved = achievedGoals.length === badgeItems.length;
+
   return (
-    <div
-      style={{ marginTop: '0.5rem' }}
-      className={css`
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      `}
-    >
-      {badgeItems.map((item) => (
-        <Badge key={item} isAchieved={isAchieved(item)}>
-          {item}
-        </Badge>
-      ))}
+    <div>
+      <div
+        style={{ marginTop: '0.5rem' }}
+        className={css`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        `}
+      >
+        {badgeItems.map((item) => (
+          <Badge key={item} isAchieved={isAchieved(item)}>
+            {item}
+          </Badge>
+        ))}
+      </div>
+      {allGoalsAchieved && (
+        <div
+          className={css`
+            text-align: center;
+            margin-top: 1rem;
+          `}
+        >
+          <p
+            className={css`
+              font-size: 1.2rem;
+              color: #333;
+              margin-bottom: 0.5rem;
+            `}
+          >
+            {`Great job! You've completed your daily goals!`}
+          </p>
+          <button
+            className={css`
+              background-image: linear-gradient(
+                to right,
+                #6a11cb 0%,
+                #2575fc 100%
+              );
+              color: white;
+              padding: 10px 20px;
+              border: none;
+              border-radius: 20px;
+              cursor: pointer;
+              font-weight: bold;
+              font-size: 1rem;
+              transition: transform 0.3s;
+              animation: pulse 2s infinite;
+
+              &:hover {
+                transform: scale(1.05);
+              }
+
+              @keyframes pulse {
+                0% {
+                  transform: scale(1);
+                }
+                50% {
+                  transform: scale(1.05);
+                }
+                100% {
+                  transform: scale(1);
+                }
+              }
+            `}
+          >
+            Collect Rewards
+          </button>
+        </div>
+      )}
     </div>
   );
 }
