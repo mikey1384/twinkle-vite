e12a32099a0c530437a74be6d439244e1f23ceb8
mikey1384 <mikey1384@gmail.com>
2023-12-10 10:51:38 +0700

    more updates

--- src/components/Notification/DailyRewardModal.tsx
+++ src/components/Notification/DailyRewardModal.tsx
@@ -1,28 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from '~/components/Modal';
 import Button from '~/components/Button';
-import { useKeyContext } from '~/contexts';
+import { css } from '@emotion/css';
 
-export default function DailyRewardModal({
-  onHide,
-  modalOverModal
-}: {
-  onHide: () => void;
-  modalOverModal?: boolean;
-}) {
-  const {
-    done: { color: doneColor }
-  } = useKeyContext((v) => v.theme);
+interface Word {
+  word: string;
+  ranking: number;
+  color: string;
+}
+
+// Example word list with rankings
+const words: Word[] = [
+  { word: 'Basic', ranking: 1, color: '#ffebcd' },
+  { word: 'Elementary', ranking: 2, color: '#c0f2d1' },
+  { word: 'Intermediate', ranking: 3, color: '#b3d9ff' },
+  { word: 'Advanced', ranking: 4, color: '#ff9999' },
+  { word: 'Epic', ranking: 5, color: '#c6b2fe' }
+];
+
+export default function DailyRewardModal({ onHide }: { onHide: () => void }) {
+  const [currentWord, setCurrentWord] = useState('');
+  const [chosenWord, setChosenWord] = useState<Word | null>(null);
+  const [isRevealing, setIsRevealing] = useState(false);
+
+  const startReveal = () => {
+    setIsRevealing(true);
+    const chosen = words[Math.floor(Math.random() * words.length)];
+    setChosenWord(chosen);
+
+    const allWords = [...words, chosen, chosen]; // Duplicate the chosen word for higher probability
+    let currentIndex = 0;
+    let speed = 300; // initial speed in ms
+
+    const interval = setInterval(() => {
+      if (allWords[currentIndex] === chosen && speed < 100) {
+        clearInterval(interval);
+        setIsRevealing(false);
+        setCurrentWord(chosen.word);
+      } else {
+        setCurrentWord(allWords[currentIndex].word);
+        currentIndex = (currentIndex + 1) % allWords.length;
+        speed *= 0.9; // Increase the speed
+      }
+    }, speed);
+  };
+
+  const wordDisplayStyle = css`
+    color: ${chosenWord?.color || '#000'};
+    font-size: 2.5rem;
+    font-weight: bold;
+    margin-top: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 60px; // Keeps the layout stable
+  `;
 
   return (
-    <Modal modalOverModal={modalOverModal} onHide={onHide}>
+    <Modal onHide={onHide}>
       <header>Daily Reward</header>
-      <main style={{ justifyContent: 'center', minHeight: '15rem' }}>
-        This is daily reward
+      <main>
+        {!isRevealing && !chosenWord && (
+          <button onClick={startReveal}>Reveal</button>
+        )}
+        <div className={wordDisplayStyle}>{currentWord}</div>
       </main>
       <footer>
-        <Button color={doneColor} onClick={onHide}>
-          OK
+        <Button transparent onClick={onHide}>
+          Close
         </Button>
       </footer>
     </Modal>
