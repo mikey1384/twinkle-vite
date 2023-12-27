a2369b14e65e3f01f09e00fa720eab5a1d0ac5c4
mikey1384 <mikey1384@gmail.com>
2023-12-27 18:54:30 +0700

    more updates

--- src/components/Notification/TodayStats/DailyGoals/index.tsx
+++ src/components/Notification/TodayStats/DailyGoals/index.tsx
@@ -66,7 +66,7 @@ export default function DailyGoals({
           ) : (
             <CollectRewardsButton
               isChecked={isChecked}
-              onCollectRewardButtonClick={onCollectRewardButtonClick}
+              onClick={onCollectRewardButtonClick}
               dailyRewardModalShown={dailyRewardModalShown}
             />
           )}
