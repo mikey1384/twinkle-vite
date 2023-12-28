fdca7f597bfcdf77666ce20ebc268f8ab0f617bd
mikey1384 <mikey1384@gmail.com>
2023-12-28 20:55:48 +0700

    more updates

--- src/components/Notification/DailyBonusModal.tsx
+++ src/components/Notification/DailyBonusModal.tsx
@@ -1,15 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Modal from '~/components/Modal';
 import Button from '~/components/Button';
+import Question from '~/components/Question';
+import Loading from '~/components/Loading';
 import { useAppContext } from '~/contexts';
 
 export default function DailyBonusModal({ onHide }: { onHide: () => void }) {
   const loadDailyBonus = useAppContext((v) => v.requestHelpers.loadDailyBonus);
+  const [questions, setQuestions] = useState<any>([]);
+  const [loading, setLoading] = useState(true);
+  const [selectedChoiceIndex, setSelectedChoiceIndex] = useState<number>();
   useEffect(() => {
     init();
     async function init() {
-      const questions = await loadDailyBonus();
-      console.log(questions);
+      try {
+        setLoading(true);
+        const questions = await loadDailyBonus();
+        setQuestions(questions);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -17,7 +29,32 @@ export default function DailyBonusModal({ onHide }: { onHide: () => void }) {
   return (
     <Modal wrapped onHide={onHide}>
       <header>Daily Reward</header>
-      <main>bonus reward</main>
+      <main>
+        {loading ? (
+          <Loading />
+        ) : (
+          <>
+            {questions.map(
+              (question: {
+                id: number;
+                question: string;
+                choices: string[];
+                answerIndex: number;
+              }) => (
+                <Question
+                  key={question.id}
+                  isGraded={false}
+                  question={question.question}
+                  choices={question.choices}
+                  selectedChoiceIndex={selectedChoiceIndex}
+                  answerIndex={question.answerIndex}
+                  onSelectChoice={(index) => setSelectedChoiceIndex(index)}
+                />
+              )
+            )}
+          </>
+        )}
+      </main>
       <footer>
         <Button transparent onClick={onHide}>
           Close
