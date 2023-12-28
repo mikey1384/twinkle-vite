80a61a4e26665cd0045be11e8f5f7b9b802ae26c
mikey1384 <mikey1384@gmail.com>
2023-12-28 18:34:13 +0700

    more updates

--- src/components/Notification/DailyBonusModal.tsx
+++ src/components/Notification/DailyBonusModal.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Modal from '~/components/Modal';
 import Button from '~/components/Button';
+import { useAppContext } from '~/contexts';
 
 export default function DailyBonusModal({ onHide }: { onHide: () => void }) {
+  const loadDailyBonus = useAppContext((v) => v.requestHelpers.loadDailyBonus);
+  useEffect(() => {
+    init();
+    async function init() {
+      const data = await loadDailyBonus();
+      console.log(data);
+    }
+  });
   return (
     <Modal wrapped onHide={onHide}>
       <header>Daily Reward</header>
