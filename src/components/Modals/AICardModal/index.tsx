a8dcdfaa207bee6beb2cd91c43837a68dc638645
mikey1384 <mikey1384@gmail.com>
2024-01-12 18:19:17 +0700

    more minor fix

--- src/components/Modals/AICardModal/index.tsx
+++ src/components/Modals/AICardModal/index.tsx
@@ -117,7 +117,7 @@ export default function AICardModal({
       setOffersLoadMoreShown(loadMoreShown);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [cardId]);
 
   useEffect(() => {
     socket.on('ai_card_offer_posted', handleAICardOfferPosted);
