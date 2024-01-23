c7e8a75f39929af29518a7cb6325aef48d21a86f
mikey1384 <mikey1384@gmail.com>
2024-01-23 09:54:21 +0700

    minor fix

--- src/containers/VideoPage/Details/index.tsx
+++ src/containers/VideoPage/Details/index.tsx
@@ -12,6 +12,7 @@ import Description from './Description';
 import RecommendationInterface from '~/components/RecommendationInterface';
 import RecommendationStatus from '~/components/RecommendationStatus';
 import TagStatus from '~/components/TagStatus';
+import ErrorBoundary from '~/components/ErrorBoundary';
 import {
   isMobile,
   determineUserCanRewardThis,
@@ -315,229 +316,237 @@ export default function Details({
   }, [videoViews]);
 
   return (
-    <div style={{ width: '100%' }}>
-      <AlreadyPosted
-        changingPage={changingPage}
-        style={{ marginBottom: '1rem' }}
-        contentId={Number(videoId)}
-        contentType="video"
-        url={content}
-        uploaderId={uploader.id}
-        videoCode={content}
-      />
-      <TagStatus
-        style={{ fontSize: '1.5rem' }}
-        onAddTags={addTags}
-        tags={tags}
-        contentId={Number(videoId)}
-      />
-      <div
-        style={{
-          padding: '0 1rem 1rem 1rem',
-          width: '100%'
-        }}
-      >
-        <div
-          style={{ display: 'flex', flexDirection: 'column', width: '100%' }}
-        >
-          <div
-            style={{
-              display: 'flex',
-              width: '100%',
-              alignItems: 'center',
-              justifyContent: 'space-between',
-              fontSize: '1.5rem'
-            }}
-          >
-            <BasicInfos
-              className={css`
-                width: CALC(100% - 25rem);
-                @media (max-width: ${mobileMaxWidth}) {
-                  width: CALC(100% - ${canReward ? '15rem' : '12rem'});
-                }
-              `}
-              style={{
-                marginRight: '1rem',
-                display: 'flex',
-                flexDirection: 'column'
-              }}
-              editedUrl={editedUrl}
-              editedTitle={editedTitle}
-              onTitleChange={handleTitleChange}
-              innerRef={TitleRef}
-              onTitleKeyUp={(event: any) => {
-                if (event.key === ' ') {
-                  handleTitleChange(addEmoji(event.target.value));
-                }
-              }}
-              onUrlChange={handleUrlChange}
-              onEdit={isEditing}
-              onMouseLeave={() => setTitleHovered(false)}
-              onMouseOver={onMouseOver}
-              onTitleClick={() => {
-                if (textIsOverflown(TitleRef.current)) {
-                  setTitleHovered((titleHovered) => !titleHovered);
-                }
-              }}
-              title={title}
-              titleExceedsCharLimit={titleExceedsCharLimit}
-              titleHovered={titleHovered}
-              timeStamp={timeStamp}
-              uploader={uploader}
-              urlExceedsCharLimit={urlExceedsCharLimit}
-            />
-            <SideButtons
-              className={css`
-                width: 25rem;
-                @media (max-width: ${mobileMaxWidth}) {
-                  width: ${canReward ? '15rem' : '12rem'};
-                }
-              `}
-              style={{
-                marginTop: canEditPlaylists ? 0 : '1rem',
-                display: 'flex',
-                flexDirection: 'column'
-              }}
-              byUser={byUser}
-              changeByUserStatus={changeByUserStatus}
-              rewardLevel={rewardLevel}
-              likes={likes}
-              onLikeVideo={handleLikeVideo}
-              onSetRewardLevel={onSetRewardLevel}
-              uploader={uploader}
-              userId={userId}
-              videoId={videoId}
-            />
-          </div>
-        </div>
+    <ErrorBoundary componentPath="VideoPage/Details">
+      <div style={{ width: '100%' }}>
+        <AlreadyPosted
+          changingPage={changingPage}
+          style={{ marginBottom: '1rem' }}
+          contentId={Number(videoId)}
+          contentType="video"
+          url={content}
+          uploaderId={uploader.id}
+          videoCode={content}
+        />
+        <TagStatus
+          style={{ fontSize: '1.5rem' }}
+          onAddTags={addTags}
+          tags={tags}
+          contentId={Number(videoId)}
+        />
         <div
           style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            width: '100%',
-            marginTop: '1rem',
-            position: 'relative'
+            padding: '0 1rem 1rem 1rem',
+            width: '100%'
           }}
         >
           <div
             style={{ display: 'flex', flexDirection: 'column', width: '100%' }}
           >
-            <Description
-              onChange={(event) => handleDescriptionChange(event.target.value)}
-              onEdit={isEditing}
-              onEditCancel={handleEditCancel}
-              onEditFinish={handleEditFinish}
-              onKeyUp={(event) => {
-                if (event.key === ' ') {
-                  handleDescriptionChange(addEmoji(event.target.value));
-                }
+            <div
+              style={{
+                display: 'flex',
+                width: '100%',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                fontSize: '1.5rem'
               }}
-              description={description}
-              editedDescription={editedDescription}
-              descriptionExceedsCharLimit={descriptionExceedsCharLimit}
-              determineEditButtonDoneStatus={determineEditButtonDoneStatus}
-            />
-            {!isEditing && videoViews > 10 && (
-              <div
+            >
+              <BasicInfos
+                className={css`
+                  width: CALC(100% - 25rem);
+                  @media (max-width: ${mobileMaxWidth}) {
+                    width: CALC(100% - ${canReward ? '15rem' : '12rem'});
+                  }
+                `}
                 style={{
-                  padding: '1rem 0',
-                  fontSize: '2rem',
-                  fontWeight: 'bold',
-                  color: Color.darkerGray()
+                  marginRight: '1rem',
+                  display: 'flex',
+                  flexDirection: 'column'
                 }}
-              >
-                {viewsLabel}
-              </div>
-            )}
-            <div style={{ display: 'flex', marginTop: '1rem' }}>
-              {editButtonShown && !isEditing && (
-                <DropdownButton
-                  skeuomorphic
-                  icon="pencil-alt"
-                  color="darkerGray"
-                  style={{ marginRight: '1rem' }}
-                  text={editOrDeleteLabel}
-                  menuProps={editMenuItems}
-                />
-              )}
+                editedUrl={editedUrl}
+                editedTitle={editedTitle}
+                onTitleChange={handleTitleChange}
+                innerRef={TitleRef}
+                onTitleKeyUp={(event: any) => {
+                  if (event.key === ' ') {
+                    handleTitleChange(addEmoji(event.target.value));
+                  }
+                }}
+                onUrlChange={handleUrlChange}
+                onEdit={isEditing}
+                onMouseLeave={() => setTitleHovered(false)}
+                onMouseOver={onMouseOver}
+                onTitleClick={() => {
+                  if (textIsOverflown(TitleRef.current)) {
+                    setTitleHovered((titleHovered) => !titleHovered);
+                  }
+                }}
+                title={title}
+                titleExceedsCharLimit={titleExceedsCharLimit}
+                titleHovered={titleHovered}
+                timeStamp={timeStamp}
+                uploader={uploader}
+                urlExceedsCharLimit={urlExceedsCharLimit}
+              />
+              <SideButtons
+                className={css`
+                  width: 25rem;
+                  @media (max-width: ${mobileMaxWidth}) {
+                    width: ${canReward ? '15rem' : '12rem'};
+                  }
+                `}
+                style={{
+                  marginTop: canEditPlaylists ? 0 : '1rem',
+                  display: 'flex',
+                  flexDirection: 'column'
+                }}
+                byUser={byUser}
+                changeByUserStatus={changeByUserStatus}
+                rewardLevel={rewardLevel}
+                likes={likes}
+                onLikeVideo={handleLikeVideo}
+                onSetRewardLevel={onSetRewardLevel}
+                uploader={uploader}
+                userId={userId}
+                videoId={videoId}
+              />
             </div>
           </div>
           <div
             style={{
-              position: 'absolute',
-              bottom: 0,
-              right: 0
+              display: 'flex',
+              justifyContent: 'space-between',
+              width: '100%',
+              marginTop: '1rem',
+              position: 'relative'
             }}
           >
-            <div style={{ display: 'flex' }}>
-              {rewardButtonShown && (
-                <RewardButton
-                  skeuomorphic
-                  contentId={videoId}
-                  contentType="video"
-                  disableReason={xpButtonDisabled}
-                />
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                width: '100%'
+              }}
+            >
+              <Description
+                onChange={(event) =>
+                  handleDescriptionChange(event.target.value)
+                }
+                onEdit={isEditing}
+                onEditCancel={handleEditCancel}
+                onEditFinish={handleEditFinish}
+                onKeyUp={(event) => {
+                  if (event.key === ' ') {
+                    handleDescriptionChange(addEmoji(event.target.value));
+                  }
+                }}
+                description={description}
+                editedDescription={editedDescription}
+                descriptionExceedsCharLimit={descriptionExceedsCharLimit}
+                determineEditButtonDoneStatus={determineEditButtonDoneStatus}
+              />
+              {!isEditing && videoViews > 10 && (
+                <div
+                  style={{
+                    padding: '1rem 0',
+                    fontSize: '2rem',
+                    fontWeight: 'bold',
+                    color: Color.darkerGray()
+                  }}
+                >
+                  {viewsLabel}
+                </div>
               )}
-              <Button
-                color={rewardColor}
-                style={{ marginLeft: '1rem' }}
-                skeuomorphic
-                filled={isRecommendedByUser}
-                disabled={recommendationInterfaceShown}
-                onClick={() => setRecommendationInterfaceShown(true)}
-              >
-                <Icon icon="heart" />
-              </Button>
+              <div style={{ display: 'flex', marginTop: '1rem' }}>
+                {editButtonShown && !isEditing && (
+                  <DropdownButton
+                    skeuomorphic
+                    icon="pencil-alt"
+                    color="darkerGray"
+                    style={{ marginRight: '1rem' }}
+                    text={editOrDeleteLabel}
+                    menuProps={editMenuItems}
+                  />
+                )}
+              </div>
+            </div>
+            <div
+              style={{
+                position: 'absolute',
+                bottom: 0,
+                right: 0
+              }}
+            >
+              <div style={{ display: 'flex' }}>
+                {rewardButtonShown && (
+                  <RewardButton
+                    skeuomorphic
+                    contentId={videoId}
+                    contentType="video"
+                    disableReason={xpButtonDisabled}
+                  />
+                )}
+                <Button
+                  color={rewardColor}
+                  style={{ marginLeft: '1rem' }}
+                  skeuomorphic
+                  filled={isRecommendedByUser}
+                  disabled={recommendationInterfaceShown}
+                  onClick={() => setRecommendationInterfaceShown(true)}
+                >
+                  <Icon icon="heart" />
+                </Button>
+              </div>
             </div>
           </div>
-        </div>
-        <RecommendationStatus
-          style={{
-            marginTop: '1rem',
-            marginBottom: 0,
-            marginLeft: '-1rem',
-            marginRight: '-1rem'
-          }}
-          contentType="video"
-          recommendations={recommendations}
-        />
-        {recommendationInterfaceShown && (
-          <RecommendationInterface
+          <RecommendationStatus
             style={{
               marginTop: '1rem',
-              fontSize: '1.7rem',
               marginBottom: 0,
               marginLeft: '-1rem',
               marginRight: '-1rem'
             }}
-            contentId={videoId}
             contentType="video"
-            onHide={() => setRecommendationInterfaceShown(false)}
             recommendations={recommendations}
-            rewardLevel={byUser ? 5 : 0}
-            content={description}
-            uploaderId={uploader.id}
           />
-        )}
-        {xpRewardInterfaceShown && (
-          <XPRewardInterface
-            innerRef={RewardInterfaceRef}
-            rewardLevel={byUser ? 5 : 0}
-            rewards={rewards}
-            contentType="video"
-            contentId={Number(videoId)}
-            noPadding
-            onReward={() =>
-              setRecommendationInterfaceShown(
-                !isRecommendedByUser && twinkleCoins > 0
-              )
-            }
-            uploaderLevel={uploader.level}
-            uploaderId={uploader.id}
-          />
-        )}
+          {recommendationInterfaceShown && (
+            <RecommendationInterface
+              style={{
+                marginTop: '1rem',
+                fontSize: '1.7rem',
+                marginBottom: 0,
+                marginLeft: '-1rem',
+                marginRight: '-1rem'
+              }}
+              contentId={videoId}
+              contentType="video"
+              onHide={() => setRecommendationInterfaceShown(false)}
+              recommendations={recommendations}
+              rewardLevel={byUser ? 5 : 0}
+              content={description}
+              uploaderId={uploader.id}
+            />
+          )}
+          {xpRewardInterfaceShown && (
+            <XPRewardInterface
+              innerRef={RewardInterfaceRef}
+              rewardLevel={byUser ? 5 : 0}
+              rewards={rewards}
+              contentType="video"
+              contentId={Number(videoId)}
+              noPadding
+              onReward={() =>
+                setRecommendationInterfaceShown(
+                  !isRecommendedByUser && twinkleCoins > 0
+                )
+              }
+              uploaderLevel={uploader.level}
+              uploaderId={uploader.id}
+            />
+          )}
+        </div>
       </div>
-    </div>
+    </ErrorBoundary>
   );
 
   function determineEditButtonDoneStatus() {
