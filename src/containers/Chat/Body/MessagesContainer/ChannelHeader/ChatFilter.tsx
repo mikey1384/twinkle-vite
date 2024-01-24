c2dc93c549c59510672742e1d27ec94c25ebf1b8
mikey1384 <mikey1384@gmail.com>
2024-01-24 20:09:46 +0700

    more updates

--- src/containers/Chat/Body/MessagesContainer/ChannelHeader/ChatFilter.tsx
+++ src/containers/Chat/Body/MessagesContainer/ChannelHeader/ChatFilter.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Button from '~/components/Button';
 import ErrorBoundary from '~/components/ErrorBoundary';
 
 export default function ChatFilter() {
   return (
     <ErrorBoundary componentPath="Chat/Body/MessageContainer/ChannelHeader/ChatFilter">
-      <div>this is chat filter</div>
+      <Button skeuomorphic onClick={() => console.log('clicked')}>
+        something something
+      </Button>
     </ErrorBoundary>
   );
 }
