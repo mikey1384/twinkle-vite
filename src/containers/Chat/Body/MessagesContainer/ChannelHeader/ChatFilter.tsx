f9763a91a95cb4acf2d9923634006a219618fb93
mikey1384 <mikey1384@gmail.com>
2024-01-25 09:30:09 +0700

    more fix

--- src/containers/Chat/Body/MessagesContainer/ChannelHeader/ChatFilter.tsx
+++ src/containers/Chat/Body/MessagesContainer/ChannelHeader/ChatFilter.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import Button from '~/components/Button';
+import Icon from '~/components/Icon';
 import ErrorBoundary from '~/components/ErrorBoundary';
 
-export default function ChatFilter() {
+export default function ChatFilter({
+  subjectObj
+}: {
+  subjectObj: {
+    content: string;
+    timeStamp: number;
+    reloadTimeStamp: number;
+    reloader: any;
+    uploader: any;
+  };
+}) {
+  const { content } = subjectObj;
+
   return (
     <ErrorBoundary componentPath="Chat/Body/MessageContainer/ChannelHeader/ChatFilter">
       <Button skeuomorphic onClick={() => console.log('clicked')}>
-        something something
+        <Icon icon="chevron-down" />
+        <span style={{ marginLeft: '0.7rem' }}>
+          {content || 'Select a topic'}
+        </span>
       </Button>
     </ErrorBoundary>
   );
