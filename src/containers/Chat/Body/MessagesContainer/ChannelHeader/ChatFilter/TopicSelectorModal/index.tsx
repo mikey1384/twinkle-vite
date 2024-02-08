128ee5681f938d3f62798b30c3cae1d6371d722a
mikey1384 <mikey1384@gmail.com>
2024-02-08 09:06:14 +0700

    minor fix

--- src/containers/Chat/Body/MessagesContainer/ChannelHeader/ChatFilter/TopicSelectorModal/index.tsx
+++ src/containers/Chat/Body/MessagesContainer/ChannelHeader/ChatFilter/TopicSelectorModal/index.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Modal from '~/components/Modal';
 import Button from '~/components/Button';
-import LoadMoreButton from '~/components/Buttons/LoadMoreButton';
-import TopicItem from './TopicItem';
 import TopicInput from './TopicInput';
-import Loading from '~/components/Loading';
+import Main from './Main';
 import { css } from '@emotion/css';
 import { Color } from '~/constants/css';
 import { useAppContext } from '~/contexts';
@@ -76,89 +74,7 @@ export default function TopicSelectorModal({
           topicSearchText={topicSearchText}
           onSetTopicSearchText={setTopicSearchText}
         />
-        {!loaded && <Loading />}
-        {myTopicObj.subjects.length > 0 && (
-          <div style={{ width: '100%', marginTop: '3rem' }}>
-            <h3
-              style={{
-                color: Color[displayedThemeColor](),
-                marginBottom: '1rem'
-              }}
-            >
-              My Topics
-            </h3>
-            {myTopicObj.subjects.map(
-              (subject: {
-                id: number;
-                content: string;
-                userId: number;
-                username: string;
-                timeStamp: number;
-                userIsOwner?: boolean;
-              }) => (
-                <TopicItem
-                  key={subject.id}
-                  currentSubjectId={currentTopicId}
-                  displayedThemeColor={displayedThemeColor}
-                  onSelectSubject={() => onSelectTopic(subject.id)}
-                  {...subject}
-                />
-              )
-            )}
-            {myTopicObj.loadMoreButton && (
-              <LoadMoreButton
-                filled
-                loading={myTopicObj.loading}
-                onClick={() => handleLoadMoreTopics(true)}
-              />
-            )}
-          </div>
-        )}
-        {loaded && allTopicObj.subjects.length > 0 && (
-          <div
-            style={{
-              margin: '1rem 0',
-              marginTop: '3rem',
-              width: '100%'
-            }}
-          >
-            <h3
-              style={{
-                color: Color[displayedThemeColor]()
-              }}
-            >
-              All Topics
-            </h3>
-          </div>
-        )}
-        {loaded && allTopicObj.subjects.length === 0 && (
-          <div>{`There aren't any subjects here, yet`}</div>
-        )}
-        {allTopicObj.subjects.map(
-          (subject: {
-            id: number;
-            content: string;
-            userId: number;
-            username: string;
-            timeStamp: number;
-            userIsOwner?: boolean;
-          }) => (
-            <TopicItem
-              key={subject.id}
-              currentSubjectId={currentTopicId}
-              displayedThemeColor={displayedThemeColor}
-              onSelectSubject={() => onSelectTopic(subject.id)}
-              {...subject}
-            />
-          )
-        )}
-        {allTopicObj.loadMoreButton && (
-          <LoadMoreButton
-            filled
-            loading={allTopicObj.loading}
-            onClick={() => handleLoadMoreTopics(false)}
-          />
-        )}
+        <Main />
       </main>
       <footer>
         <Button transparent onClick={onHide}>
