baeb58c98d003df5f2fe25d0f306c18d64a7c34a
mikey1384 <mikey1384@gmail.com>
2024-02-13 20:08:16 +0700

    more updates

---
+++ src/containers/Chat/Body/MessagesContainer/ChannelHeader/ChatFilter/TopicSelectorModal/NoTopicPosted.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function NoTopicPosted() {
+  return (
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '15rem',
+        fontSize: '1.5rem',
+        fontWeight: 'bold'
+      }}
+    >
+      No Topic Posted
+    </div>
+  );
+}
