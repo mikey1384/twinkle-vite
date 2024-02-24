58f505943d252c7556940a83fb67c61432c33720
mikey1384 <mikey1384@gmail.com>
2024-02-24 17:10:09 +0900

    more fix

--- src/components/AICardsPreview/index.tsx
+++ src/components/AICardsPreview/index.tsx
@@ -4,8 +4,8 @@ import CardThumb from '~/components/CardThumb';
 import ShowMoreCardsButton from '~/components/Buttons/ShowMoreCardsButton';
 import MoreAICardsModal from './MoreAICardsModal';
 import ErrorBoundary from '~/components/ErrorBoundary';
-import { isMobile } from '~/helpers';
 import { useChatContext } from '~/contexts';
+import { isMobile } from '~/helpers';
 
 const deviceIsMobile = isMobile(navigator);
 
@@ -32,36 +32,35 @@ export default function AICardsPreview({
   onSetAICardModalCardId?: (cardId: number) => void;
 }) {
   const cardObj = useChatContext((v) => v.state.cardObj);
-  const { numMore, cards, displayedCards } = useMemo(() => {
+  const { numMore, displayedCardIds, allCardIds } = useMemo(() => {
     const displayedCardIds = cardIds.slice(0, deviceIsMobile ? 3 : 5);
     const numMore = cardIds.length - displayedCardIds.length;
-    const cards = cardIds.map((cardId) => cardObj[cardId]);
-    const displayedCards = displayedCardIds
-      .filter((cardId) => !!cardObj[cardId])
-      .map((cardId) => cardObj[cardId]);
-    return { numMore, cards, displayedCards };
-  }, [cardIds, cardObj]);
+    const allCardIds = cardIds.map((cardId) => cardId);
+    return { numMore, allCardIds, displayedCardIds };
+  }, [cardIds]);
   const [moreAICardsModalShown, setMoreAICardsModalShown] = useState(false);
 
   return (
     <ErrorBoundary componentPath="components/AICardsPreview">
       <div style={{ display: 'flex', height: '100%' }}>
-        {displayedCards.map((card, index) => (
-          <div key={card.id} style={{ position: 'relative' }}>
-            <CardThumb
-              detailed
-              card={card}
-              style={{
-                marginLeft: index > 0 ? '1rem' : 0
-              }}
-              onClick={
-                onSetAICardModalCardId
-                  ? () => onSetAICardModalCardId(card.id)
-                  : undefined
-              }
-            />
-          </div>
-        ))}
+        {displayedCardIds.map((cardId, index) => {
+          return (
+            <div key={cardId} style={{ position: 'relative' }}>
+              <CardThumb
+                detailed
+                card={cardObj[cardId] || { id: cardId }}
+                style={{
+                  marginLeft: index > 0 ? '1rem' : 0
+                }}
+                onClick={
+                  onSetAICardModalCardId
+                    ? () => onSetAICardModalCardId(cardId)
+                    : undefined
+                }
+              />
+            </div>
+          );
+        })}
         {!!numMore && (
           <ShowMoreCardsButton
             onClick={
@@ -78,7 +77,9 @@ export default function AICardsPreview({
         {moreAICardsModalShown && (
           <MoreAICardsModal
             modalOverModal={isOnModal}
-            cards={cards}
+            cards={allCardIds.map(
+              (cardId) => cardObj[cardId] || { id: cardId }
+            )}
             moreAICardsModalTitle={moreAICardsModalTitle}
             onSetAICardModalCardId={onSetAICardModalCardId}
             onHide={() =>
