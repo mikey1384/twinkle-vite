d68b04387a44588e935aa24d334a48e7a77fc76a
mikey1384 <mikey1384@gmail.com>
2024-02-27 19:36:28 +0900

    minor fix

--- src/containers/Chat/Message/MessageBody/TopicStartNotification.tsx
+++ src/containers/Chat/Message/MessageBody/TopicStartNotification.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ErrorBoundary from '~/components/ErrorBoundary';
 import { css } from '@emotion/css';
 import { Color, mobileMaxWidth } from '~/constants/css';
 import { useTheme } from '~/helpers/hooks';
@@ -26,63 +27,65 @@ export default function TopicStartNotification({
   const onEnterTopic = useChatContext((v) => v.actions.onEnterTopic);
 
   return (
-    <div
-      className={css`
-        font-family: 'Roboto', sans-serif;
-        color: ${themeStyles.text};
-        background-color: ${themeStyles.bg};
-        border-top: 1px solid ${themeStyles.border};
-        border-bottom: 1px solid ${themeStyles.border};
-        cursor: pointer;
-        text-align: center;
-        padding: 1rem;
-        margin: 1rem 0;
-        transition: background 0.3s ease;
-
-        &:hover {
-          background-color: ${themeStyles.hoverBg};
-          border-color: ${themeStyles.hoverBorder};
-        }
-      `}
-      onClick={() => handleTopicClick(topicObj.id)}
-    >
+    <ErrorBoundary componentPath="Chat/Message/MessageBody/TopicStartNotification">
       <div
         className={css`
-          font-size: 1.6rem;
-          font-weight: bold;
-          width: 100%;
-          @media (max-width: ${mobileMaxWidth}) {
-            font-size: 1.5rem;
+          font-family: 'Roboto', sans-serif;
+          color: ${themeStyles.text};
+          background-color: ${themeStyles.bg};
+          border-top: 1px solid ${themeStyles.border};
+          border-bottom: 1px solid ${themeStyles.border};
+          cursor: pointer;
+          text-align: center;
+          padding: 1rem;
+          margin: 1rem 0;
+          transition: background 0.3s ease;
+
+          &:hover {
+            background-color: ${themeStyles.hoverBg};
+            border-color: ${themeStyles.hoverBorder};
           }
         `}
+        onClick={() => handleTopicClick(topicObj.id)}
       >
         <div
           className={css`
+            font-size: 1.6rem;
+            font-weight: bold;
             width: 100%;
-          `}
-        >
-          {username} started a new topic
-        </div>
-        <div
-          className={css`
-            width: 100%;
-            padding: 0 10rem;
-            overflow: hidden;
-            text-overflow: ellipsis;
-            white-space: nowrap;
-            color: ${Color[topicTextColor]()};
-            text-shadow: ${topicShadowColor
-              ? `0.05rem 0.05rem 0.05rem ${Color[topicShadowColor]()}`
-              : 'none'};
             @media (max-width: ${mobileMaxWidth}) {
-              padding: 0 3rem;
+              font-size: 1.5rem;
             }
           `}
         >
-          {topicObj.title}
+          <div
+            className={css`
+              width: 100%;
+            `}
+          >
+            {username} started a new topic
+          </div>
+          <div
+            className={css`
+              width: 100%;
+              padding: 0 10rem;
+              overflow: hidden;
+              text-overflow: ellipsis;
+              white-space: nowrap;
+              color: ${Color[topicTextColor]()};
+              text-shadow: ${topicShadowColor
+                ? `0.05rem 0.05rem 0.05rem ${Color[topicShadowColor]()}`
+                : 'none'};
+              @media (max-width: ${mobileMaxWidth}) {
+                padding: 0 3rem;
+              }
+            `}
+          >
+            {topicObj.title}
+          </div>
         </div>
       </div>
-    </div>
+    </ErrorBoundary>
   );
 
   function handleTopicClick(topicId: number) {
