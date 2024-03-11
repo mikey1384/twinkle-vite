316cf0f46974d2e30f5e4263a6230e59d50e26ef
mikey1384 <mikey1384@gmail.com>
2024-03-11 17:45:28 +0700

    minor fix

--- src/containers/Chat/Body/MessagesContainer/index.tsx
+++ src/containers/Chat/Body/MessagesContainer/index.tsx
@@ -1319,11 +1319,13 @@ function MessagesContainer({
         (MessagesRef.current || {}).scrollTop = 0;
         (MessagesRef.current || {}).scrollTop = -1000;
         setTimeout(() => {
-          if (userId === 9595) {
-            window.alert('herre');
-            window.alert((MessagesRef.current || {}).scrollTop);
-          }
-          (MessagesRef.current || {}).scrollTop = 0;
+          requestAnimationFrame(() => {
+            (MessagesRef.current || {}).scrollTop = 0;
+            if (userId === 9595) {
+              window.alert('herre');
+              window.alert((MessagesRef.current || {}).scrollTop);
+            }
+          });
         }, 500);
       }
       (MessagesRef.current || {}).scrollTop = 0;
