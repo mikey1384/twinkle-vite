d8fcba92c5cbd48416b94a61273e6ea08bba4e64
mikey1384 <mikey1384@gmail.com>
2024-03-24 09:01:44 +0700

    minor fix

--- src/containers/Explore/Subjects/Featured.tsx
+++ src/containers/Explore/Subjects/Featured.tsx
@@ -34,7 +34,8 @@ export default function Featured({
       return subjects;
     }
     return subjects[0] ? [subjects[0]] : [];
-  }, [subjects, expanded]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [subjects?.[0]?.id, expanded]);
 
   return (
     <ErrorBoundary componentPath="Explore/Subjects/Featured">
