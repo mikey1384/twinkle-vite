f1594b7d719959249f2af3bd8dc69ab83dcac120
mikey1384 <mikey1384@gmail.com>
2024-04-02 20:22:09 +0700

    more minor fix

--- src/components/ContentListItem/index.tsx
+++ src/components/ContentListItem/index.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useMemo } from 'react';
-import Main from './Main';
+import React, { useEffect, useMemo, useState } from 'react';
+import CommentContent from './CommentContent';
+import RootContent from './RootContent';
 import { useContentState } from '~/helpers/hooks';
 import { useNavigate } from 'react-router-dom';
-import { useKeyContext, useContentContext } from '~/contexts';
+import { useKeyContext } from '~/contexts';
 
 export default function ContentListItem({
   onClick = () => null,
@@ -33,17 +34,50 @@ export default function ContentListItem({
   const {
     itemSelected: { color: itemSelectedColor, opacity: itemSelectedOpacity }
   } = useKeyContext((v) => v.theme);
-  const onInitContent = useContentContext((v) => v.actions.onInitContent);
+  const [currentContent, setCurrentContent] = useState<any>(contentObj || {});
+  const [rootContent, setRootContent] = useState<any>(
+    contentObj?.rootObj || {}
+  );
+
+  const contentState = useContentState({ contentId, contentType });
+  const rootState = useContentState({
+    contentId: currentContent?.rootObj?.id,
+    contentType: currentContent?.rootObj?.contentType
+  });
+
+  useEffect(() => {
+    if (currentContent.isDeleted) {
+      onContentIsDeleted?.(contentId);
+    }
+  }, [contentId, currentContent.isDeleted, onContentIsDeleted]);
+
+  const isCommentItem = useMemo(() => {
+    return !!currentContent.notFound || !!currentContent.isDeleted
+      ? null
+      : contentType === 'comment';
+  }, [contentType, currentContent.isDeleted, currentContent.notFound]);
+
+  useEffect(() => {
+    if (contentState.loaded) {
+      setCurrentContent(contentState);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [contentState?.loaded]);
+
+  useEffect(() => {
+    if (currentContent?.rootObj?.id && rootState.loaded) {
+      setRootContent(rootState);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentContent?.rootObj?.id, rootState?.loaded]);
+
   const {
     content,
     description,
-    isDeleted,
     fileName,
     filePath,
     fileSize,
-    loaded,
     rewardLevel,
-    rootObj,
     secretAnswer,
     secretAttachment,
     story,
@@ -51,72 +85,57 @@ export default function ContentListItem({
     title,
     thumbUrl,
     uploader = {}
-  } = useContentState({ contentId, contentType });
-
-  const rootState = useContentState({
-    contentId: rootObj?.id,
-    contentType: rootObj?.contentType
-  });
-
-  useEffect(() => {
-    if (isDeleted) {
-      onContentIsDeleted?.(contentId);
-    }
-  }, [contentId, isDeleted, onContentIsDeleted]);
-
-  const isCommentItem = useMemo(() => {
-    return !!contentObj.notFound || !!isDeleted
-      ? null
-      : contentType === 'comment';
-  }, [contentObj.notFound, contentType, isDeleted]);
-
-  useEffect(() => {
-    if (!loaded) {
-      onInitContent({ contentId, ...contentObj });
-    }
-    if (rootObj?.id && !rootState?.loaded) {
-      onInitContent({
-        contentId: rootObj.id,
-        contentType: rootObj.contentType,
-        ...rootObj
-      });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [loaded, rootObj?.id, rootState?.loaded]);
+  } = currentContent;
 
   return (
-    <Main
-      content={content}
-      description={description}
-      fileName={fileName}
-      filePath={filePath}
-      fileSize={fileSize}
-      userId={userId}
-      contentId={contentId}
-      contentType={contentType}
-      expandable={expandable}
-      innerStyle={innerStyle}
-      isCommentItem={isCommentItem}
-      itemSelectedColor={itemSelectedColor}
-      itemSelectedOpacity={itemSelectedOpacity}
-      hideSideBordersOnMobile={hideSideBordersOnMobile}
-      modalOverModal={modalOverModal}
-      navigate={navigate}
-      onClick={onClick}
-      rewardLevel={rewardLevel}
-      rootId={rootState?.id}
-      rootContent={rootState?.content}
-      rootRewardLevel={rootState?.rewardLevel}
-      secretAnswer={secretAnswer}
-      secretAttachment={secretAttachment}
-      selectable={selectable}
-      selected={selected}
-      story={story}
-      thumbUrl={thumbUrl}
-      title={title}
-      topic={topic}
-      uploader={uploader}
-      style={style}
-    />
+    <div style={{ width: style?.width || '100%' }}>
+      {isCommentItem ? (
+        <CommentContent
+          contentId={contentId}
+          contentType={contentType}
+          uploader={uploader}
+          content={content}
+          fileName={fileName}
+          filePath={filePath}
+          fileSize={fileSize}
+          thumbUrl={thumbUrl}
+          style={style}
+        />
+      ) : (
+        <RootContent
+          content={content}
+          contentId={contentId}
+          contentType={contentType}
+          description={description}
+          fileName={fileName}
+          filePath={filePath}
+          fileSize={fileSize}
+          onClick={onClick}
+          rootType={rootContent.contentType}
+          expandable={expandable}
+          selected={selected}
+          hideSideBordersOnMobile={hideSideBordersOnMobile}
+          itemSelectedColor={itemSelectedColor}
+          itemSelectedOpacity={itemSelectedOpacity}
+          modalOverModal={modalOverModal}
+          navigate={navigate}
+          rewardLevel={rewardLevel}
+          rootId={rootContent.id}
+          rootContent={rootContent}
+          rootRewardLevel={rootContent.rewardLevel}
+          secretAnswer={secretAnswer}
+          secretAttachment={secretAttachment}
+          selectable={selectable}
+          story={story}
+          style={style}
+          innerStyle={innerStyle}
+          thumbUrl={thumbUrl}
+          title={title}
+          topic={topic}
+          uploader={uploader}
+          userId={userId}
+        />
+      )}
+    </div>
   );
 }
