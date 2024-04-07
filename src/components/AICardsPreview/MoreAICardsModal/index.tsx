1fc9912221c5c97888056dd4a596019eab1d4ef1
mikey1384 <mikey1384@gmail.com>
2024-04-07 09:34:16 +0700

    add burn value calculator for AI card modal

--- src/components/AICardsPreview/MoreAICardsModal/index.tsx
+++ src/components/AICardsPreview/MoreAICardsModal/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import Modal from '~/components/Modal';
 import Button from '~/components/Button';
 import CardItem from './CardItem';
+import { addCommasToNumber } from '~/helpers/stringHelpers';
+import { returnCardBurnXP } from '~/constants/defaultValues';
 import { Card } from '~/types';
 
 MoreAICardsModal.propTypes = {
@@ -25,9 +27,25 @@ export default function MoreAICardsModal({
   modalOverModal?: boolean;
   moreAICardsModalTitle?: string;
 }) {
+  const totalBv = useMemo(() => {
+    return cards.reduce(
+      (acc, card) =>
+        acc +
+        (card.level && card.quality
+          ? returnCardBurnXP({
+              cardLevel: card.level,
+              cardQuality: card.quality
+            })
+          : 0),
+      0
+    );
+  }, [cards]);
+
   return (
     <Modal modalOverModal={modalOverModal} onHide={onHide}>
-      <header>{moreAICardsModalTitle}</header>
+      <header>{`${moreAICardsModalTitle} (total burn value: ${addCommasToNumber(
+        totalBv
+      )} XP)`}</header>
       <main>
         <div style={{ display: 'flex', flexWrap: 'wrap', width: '100%' }}>
           {cards.map((card) => (
