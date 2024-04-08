86aa37f294bae666d9662a4effcb0529e86e2262
mikey1384 <mikey1384@gmail.com>
2024-04-08 20:38:11 +0700

    try to simplify useLazyLoad with resize observerref

--- src/helpers/hooks/index.tsx
+++ src/helpers/hooks/index.tsx
@@ -75,8 +75,6 @@ export function useLazyLoad({
 }) {
   const timerRef = useRef<any>(null);
   const inViewRef = useRef(inView);
-  const setHeightCountRef = useRef(0);
-  const cooldownRef = useRef(0);
   const resizeObserverRef = useRef<ResizeObserver | null>(null);
 
   useEffect(() => {
@@ -84,47 +82,15 @@ export function useLazyLoad({
   }, [inView]);
 
   useEffect(() => {
-    if (inView) {
-      if (!resizeObserverRef.current) {
-        resizeObserverRef.current = new ResizeObserver(
-          (entries: ResizeObserverEntry[]) => {
-            const clientHeight = entries[0].target.clientHeight;
-            if (inViewRef.current) {
-              if (cooldownRef.current === 0) {
-                onSetPlaceholderHeight(clientHeight);
-                setHeightCountRef.current += 1;
-                if (setHeightCountRef.current >= 5) {
-                  cooldownRef.current = 100;
-                }
-              } else {
-                setTimeout(() => {
-                  onSetPlaceholderHeight(clientHeight);
-                  setHeightCountRef.current = 0;
-                  cooldownRef.current = Math.min(
-                    cooldownRef.current + 100,
-                    1000
-                  );
-                }, cooldownRef.current);
-              }
-            }
-          }
-        );
-      }
-      if (PanelRef.current) {
-        resizeObserverRef.current.observe(PanelRef.current);
-      }
-    } else {
-      if (resizeObserverRef.current && PanelRef.current) {
-        resizeObserverRef.current.unobserve(PanelRef.current);
-      }
+    if (inView && !resizeObserverRef.current && PanelRef.current) {
+      resizeObserverRef.current = new ResizeObserver(
+        (entries: ResizeObserverEntry[]) => {
+          const clientHeight = entries[0].target.clientHeight;
+          onSetPlaceholderHeight(clientHeight);
+        }
+      );
+      resizeObserverRef.current.observe(PanelRef.current);
     }
-
-    return () => {
-      if (resizeObserverRef.current && PanelRef.current) {
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        resizeObserverRef.current.unobserve(PanelRef.current);
-      }
-    };
   }, [PanelRef, inView, onSetPlaceholderHeight]);
 
   useEffect(() => {
