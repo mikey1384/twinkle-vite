840573b27a0fe124d3931c7834b8a45f958cbb06
mikey1384 <mikey1384@gmail.com>
2024-04-22 14:56:51 +0700

    more fix

--- src/components/Texts/RichText/InvisibleTextContainer.tsx
+++ src/components/Texts/RichText/InvisibleTextContainer.tsx
@@ -7,7 +7,6 @@ export default function InvisibleTextContainer({
   contentType,
   isAIMessage,
   isProfileComponent,
-  isParsed,
   linkColor,
   markerColor,
   text,
@@ -19,7 +18,6 @@ export default function InvisibleTextContainer({
   contentType?: string;
   isAIMessage?: boolean;
   isProfileComponent?: boolean;
-  isParsed?: boolean;
   linkColor: string;
   markerColor: string;
   text: string;
@@ -45,7 +43,6 @@ export default function InvisibleTextContainer({
         word-break: break-word;
         line-height: 1.7;
         overflow: hidden;
-        ${isParsed ? 'display: none;' : ''}
       `}
     >
       <Markdown
