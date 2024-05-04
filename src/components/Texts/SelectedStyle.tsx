d09f28eae54b3d9416d682c94779bb5bcc160e62
mikey1384 <mikey1384@gmail.com>
2024-05-04 10:34:25 +0700

    more updates

---
+++ src/components/Texts/SelectedStyle.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import CloseButton from '~/components/Buttons/CloseButton';
+import { Color, mobileMaxWidth } from '~/constants/css';
+import { css } from '@emotion/css';
+
+export default function SelectedStyle({
+  selectedStyle,
+  onClear,
+  style
+}: {
+  selectedStyle: string;
+  onClear: () => void;
+  style?: React.CSSProperties;
+}) {
+  return (
+    <div
+      style={{
+        position: 'relative',
+        fontWeight: 'bold',
+        fontFamily: "'Roboto', sans-serif",
+        color: Color.logoBlue(),
+        display: 'flex',
+        ...style
+      }}
+      className={css`
+        font-size: 2rem;
+        @media (max-width: ${mobileMaxWidth}) {
+          font-size: 1.5rem;
+        }
+      `}
+    >
+      {selectedStyle || 'Any'}
+      {selectedStyle && (
+        <div
+          style={{
+            position: 'relative',
+            display: 'flex',
+            alignItems: 'center',
+            marginLeft: '0.7rem'
+          }}
+        >
+          <CloseButton
+            style={{
+              padding: 0,
+              margin: 0,
+              right: 0,
+              top: 0,
+              display: 'block',
+              position: 'static',
+              width: '1.7rem',
+              height: '1.7rem',
+              background: Color.logoBlue()
+            }}
+            onClick={onClear}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
