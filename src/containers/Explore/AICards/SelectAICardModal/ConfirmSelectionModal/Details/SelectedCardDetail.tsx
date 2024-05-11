1587e8203d3efaaf5d9db362de0cfe3c4ab937e2
mikey1384 <mikey1384@gmail.com>
2024-05-11 10:40:24 +0700

    more updates

--- src/containers/Explore/AICards/SelectAICardModal/ConfirmSelectionModal/Details/SelectedCardDetail.tsx
+++ src/containers/Explore/AICards/SelectAICardModal/ConfirmSelectionModal/Details/SelectedCardDetail.tsx
@@ -1,53 +1,16 @@
-import React, { useMemo } from 'react';
-import Icon from '~/components/Icon';
+import React from 'react';
 import AICardsPreview from '~/components/AICardsPreview';
-import { addCommasToNumber } from '~/helpers/stringHelpers';
-import { borderRadius, innerBorderRadius, Color } from '~/constants/css';
-import { User } from '~/types';
+import { borderRadius, Color } from '~/constants/css';
 
 export default function SelectedCardDetail({
   isAICardModalShown,
-  isShowing,
-  selectedOption,
   cardIds,
-  coins,
-  onSetAICardModalCardId,
-  partner
+  onSetAICardModalCardId
 }: {
   isAICardModalShown: boolean;
-  isShowing: boolean;
-  selectedOption: string;
   cardIds: number[];
-  coins: number;
   onSetAICardModalCardId: (v: number) => void;
-  partner: User;
 }) {
-  const actionLabel = useMemo(() => {
-    if (selectedOption === 'want') {
-      if (isShowing) {
-        return 'Show';
-      } else {
-        return 'Offer';
-      }
-    }
-    if (selectedOption === 'offer') {
-      return 'Show';
-    }
-    return 'Send';
-  }, [isShowing, selectedOption]);
-  const backgroundColor = useMemo(() => {
-    if (selectedOption === 'want') {
-      if (isShowing) {
-        return Color.pink();
-      }
-      return 'transparent';
-    }
-    if (selectedOption === 'offer') {
-      return Color.pink();
-    }
-    return Color.green();
-  }, [isShowing, selectedOption]);
-
   return (
     <div
       style={{
@@ -60,27 +23,6 @@ export default function SelectedCardDetail({
     >
       <div
         style={{
-          color: selectedOption === 'want' && !isShowing ? '#000' : '#fff',
-          borderTopLeftRadius: innerBorderRadius,
-          borderTopRightRadius: innerBorderRadius,
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          padding: '1rem',
-          background: backgroundColor,
-          fontWeight: 'bold'
-        }}
-      >
-        <span style={{ marginRight: '1rem' }}>
-          {actionLabel} {partner.username}...
-        </span>
-        <Icon
-          icon="arrow-up"
-          color={actionLabel !== 'Offer' ? '#fff' : Color.red()}
-        />
-      </div>
-      <div
-        style={{
           width: '100%',
           borderTop: `1px solid ${Color.borderGray()}`,
           padding: '1rem',
@@ -90,28 +32,6 @@ export default function SelectedCardDetail({
           flexDirection: 'column'
         }}
       >
-        {!!coins && (
-          <div
-            style={{
-              width: '100%',
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}
-          >
-            <div style={{ fontWeight: 'bold' }}>
-              <Icon
-                style={{ color: Color.brownOrange() }}
-                icon={['far', 'badge-dollar']}
-              />{' '}
-              <span style={{ color: Color.darkerGray() }}>
-                {addCommasToNumber(coins)}
-              </span>
-            </div>
-            {!!cardIds.length && <div style={{ padding: '1rem' }}>and</div>}
-          </div>
-        )}
         <AICardsPreview
           isOnModal
           isAICardModalShown={isAICardModalShown}
