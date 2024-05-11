ec833f99ce8816921d11d10d839ff1bd15a33dfb
mikey1384 <mikey1384@gmail.com>
2024-05-11 10:16:57 +0700

    more refactoring

--- src/containers/Explore/AICards/SelectAICardModal/ConfirmSelectionModal/index.tsx
+++ src/containers/Explore/AICards/SelectAICardModal/ConfirmSelectionModal/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import Modal from '~/components/Modal';
 import Button from '~/components/Button';
 import localize from '~/constants/localize';
@@ -11,9 +11,6 @@ const confirmLabel = localize('confirm');
 export default function ConfirmSelectionModal({
   isAICardModalShown,
   onHide,
-  onConfirm,
-  selectedOption,
-  coinAmountObj,
   onSetAICardModalCardId,
   offeredCardIds,
   wantedCardIds,
@@ -21,9 +18,6 @@ export default function ConfirmSelectionModal({
 }: {
   isAICardModalShown: boolean;
   onHide: () => void;
-  onConfirm: (v: any) => void;
-  selectedOption: string;
-  coinAmountObj: any;
   onSetAICardModalCardId: (v: number) => void;
   offeredCardIds: number[];
   wantedCardIds: number[];
@@ -34,74 +28,15 @@ export default function ConfirmSelectionModal({
   } = useKeyContext((v) => v.theme);
 
   const [submitting, setSubmitting] = useState(false);
-  const coinOffered = coinAmountObj.offer;
-  const coinWanted = coinAmountObj.want;
-  const effectiveCoinOffered = useMemo(() => {
-    if (selectedOption === 'want') {
-      return Math.max(coinOffered - coinWanted, 0);
-    }
-    return coinOffered;
-  }, [coinOffered, coinWanted, selectedOption]);
-
-  const effectiveCoinWanted = useMemo(() => {
-    if (selectedOption === 'want') {
-      return Math.max(coinWanted - coinOffered, 0);
-    }
-    return coinWanted;
-  }, [coinOffered, coinWanted, selectedOption]);
-
-  const title = useMemo(() => {
-    if (selectedOption === 'want') {
-      if (!effectiveCoinOffered && !offeredCardIds.length)
-        return 'Express Interest';
-      if (!effectiveCoinWanted && !wantedCardIds.length)
-        return `Show${
-          effectiveCoinOffered === 0
-            ? ''
-            : ` ${effectiveCoinOffered} coin${
-                effectiveCoinOffered === 1 ? '' : 's'
-              } ${offeredCardIds.length === 0 ? '' : 'and'}`
-        }${
-          offeredCardIds.length === 0
-            ? ''
-            : ` ${offeredCardIds.length} card${
-                offeredCardIds.length === 1 ? '' : 's'
-              }`
-        }`;
-      return 'Propose Trade';
-    }
-    return `${selectedOption === 'offer' ? 'Show' : 'Send'}${
-      effectiveCoinOffered === 0
-        ? ''
-        : ` ${effectiveCoinOffered} coin${
-            effectiveCoinOffered === 1 ? '' : 's'
-          } ${offeredCardIds.length === 0 ? '' : 'and'}`
-    }${
-      offeredCardIds.length === 0
-        ? ''
-        : ` ${offeredCardIds.length} card${
-            offeredCardIds.length === 1 ? '' : 's'
-          }`
-    }`;
-  }, [
-    effectiveCoinOffered,
-    effectiveCoinWanted,
-    offeredCardIds.length,
-    selectedOption,
-    wantedCardIds.length
-  ]);
 
   return (
     <Modal modalOverModal closeWhenClickedOutside={false} onHide={onHide}>
-      <header>{title}</header>
+      <header>Set Price</header>
       <main>
         <Details
-          coinsOffered={effectiveCoinOffered}
-          coinsWanted={effectiveCoinWanted}
           cardIdsOffered={offeredCardIds}
           cardIdsWanted={wantedCardIds}
           isAICardModalShown={isAICardModalShown}
-          selectedOption={selectedOption}
           partner={partner}
           onSetAICardModalCardId={onSetAICardModalCardId}
         />
@@ -119,11 +54,5 @@ export default function ConfirmSelectionModal({
 
   function handleConfirm() {
     setSubmitting(true);
-    onConfirm({
-      coinsWanted: effectiveCoinWanted,
-      coinsOffered: effectiveCoinOffered,
-      offeredCardIds,
-      wantedCardIds
-    });
   }
 }
