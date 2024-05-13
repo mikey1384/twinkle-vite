63d772e4d2902b9e4c7f90224a170ddd4a698f64
mikey1384 <mikey1384@gmail.com>
2024-05-13 09:35:09 +0700

    more updates

--- src/containers/Explore/AICards/SelectAICardModal/ConfirmSelectionModal/FinalConfirm.tsx
+++ src/containers/Explore/AICards/SelectAICardModal/ConfirmSelectionModal/FinalConfirm.tsx
@@ -5,6 +5,8 @@ import localize from '~/constants/localize';
 import AICardsPreview from '~/components/AICardsPreview';
 import { addCommasToNumber } from '~/helpers/stringHelpers';
 import { useKeyContext } from '~/contexts';
+import { css } from '@emotion/css';
+import { Color } from '~/constants/css';
 
 const cancelLabel = localize('cancel');
 
@@ -39,22 +41,49 @@ export default function ConfirmSelectionModal({
 
   return (
     <Modal modalOverModal closeWhenClickedOutside={false} onHide={onHide}>
-      <header>Confirm</header>
-      <main>
-        <div>
-          <p>
-            The following cards have bids higher than the price you are listing
-            the cards for and will be sold immediately
-          </p>
-          <AICardsPreview
-            isOnModal
-            isAICardModalShown={isAICardModalShown}
-            cardIds={higherBidCardIds}
-            onSetAICardModalCardId={onSetAICardModalCardId}
-          />
-          You will receive {totalCoinsReceivableFromSelling} coins
-        </div>
-        <div>
+      <header>Final Confirmation</header>
+      <main
+        className={css`
+          font-family: 'Helvetica Neue', Arial, sans-serif;
+          text-align: center;
+          color: #333;
+          background-color: #fff;
+          border-radius: 8px;
+          p {
+            margin-bottom: 1rem;
+          }
+          .card-section {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            margin-bottom: 3rem;
+          }
+        `}
+      >
+        {higherBidCardIds?.length && (
+          <div className="card-section">
+            <p>
+              The following cards have bids higher than the price you are
+              listing the cards for and will be sold immediately:
+            </p>
+            <AICardsPreview
+              isOnModal
+              isAICardModalShown={isAICardModalShown}
+              cardIds={higherBidCardIds}
+              onSetAICardModalCardId={onSetAICardModalCardId}
+            />
+            <div
+              className={css`
+                margin-top: 1rem;
+                font-weight: bold;
+                color: ${Color.green()};
+              `}
+            >
+              You will receive {totalCoinsReceivableFromSelling} coins
+            </div>
+          </div>
+        )}
+        <div className="card-section">
           <p>
             The following cards will be listed on the market for the price you
             have set
