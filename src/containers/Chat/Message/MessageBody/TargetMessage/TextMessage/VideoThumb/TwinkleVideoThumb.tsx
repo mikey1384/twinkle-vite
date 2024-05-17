659217df772b8f1b0232fbd822e15355ad8a344a
mikey1384 <mikey1384@gmail.com>
2024-05-17 21:41:11 +0700

    more fix

--- src/containers/Chat/Message/MessageBody/TargetMessage/TextMessage/VideoThumb/TwinkleVideoThumb.tsx
+++ src/containers/Chat/Message/MessageBody/TargetMessage/TextMessage/VideoThumb/TwinkleVideoThumb.tsx
@@ -1,7 +1,10 @@
-import React, { useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import VideoThumbImage from '~/components/VideoThumbImage';
 import Loading from '~/components/Loading';
 import TwinkleVideoModal from '../../../TwinkleVideoModal';
+import { css } from '@emotion/css';
+import { mobileMaxWidth } from '~/constants/css';
+import { useAppContext, useContentContext } from '~/contexts';
 import { useContentState } from '~/helpers/hooks';
 import { extractVideoIdFromTwinkleVideoUrl } from '~/helpers/stringHelpers';
 
@@ -12,29 +15,50 @@ export default function TwinkleVideoThumb({
   messageId: number;
   videoUrl: string;
 }) {
+  const onInitContent = useContentContext((v) => v.actions.onInitContent);
+  const loadContent = useAppContext((v) => v.requestHelpers.loadContent);
   const [modalShown, setModalShown] = useState(false);
   const videoId = useMemo(
     () => extractVideoIdFromTwinkleVideoUrl(videoUrl),
     [videoUrl]
   );
-  const { content, rewardLevel, notFound } = useContentState({
+  const { content, rewardLevel, notFound, loaded } = useContentState({
     contentId: Number(videoId),
     contentType: 'video'
   });
 
+  useEffect(() => {
+    if (!loaded) {
+      init();
+    }
+
+    async function init() {
+      const data = await loadContent({
+        contentId: videoId,
+        contentType: 'video'
+      });
+      onInitContent(data);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [loaded]);
+
   return notFound ? null : (
     <div
-      style={{
-        position: 'relative',
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        alignItems: 'center'
-      }}
+      className={css`
+        position: relative;
+        width: 100%;
+        height: 10rem;
+        display: flex;
+        align-items: center;
+        @media (max-width: ${mobileMaxWidth}) {
+          height: 5rem;
+        }
+      `}
     >
       {content ? (
         <VideoThumbImage
-          style={{ height: '5rem', cursor: 'pointer' }}
+          noPaddingBottom
+          style={{ height: '100%', cursor: 'pointer' }}
           rewardLevel={rewardLevel}
           videoId={Number(videoId)}
           src={`https://img.youtube.com/vi/${content}/mqdefault.jpg`}
