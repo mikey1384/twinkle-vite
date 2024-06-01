af882c57e64977ce4204814824a891b7f2494e80
mikey1384 <mikey1384@gmail.com>
2024-06-01 18:25:45 +0700

    implement group fetching

--- src/containers/Home/Groups/index.tsx
+++ src/containers/Home/Groups/index.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import GroupItem from './GroupItem';
 import ErrorBoundary from '~/components/ErrorBoundary';
+import { useAppContext } from '~/contexts/';
 
 export default function Groups() {
+  const loadPublicGroups = useAppContext(
+    (v) => v.requestHelpers.loadPublicGroups
+  );
+  const [groups, setGroups] = useState([]);
+  useEffect(() => {
+    init();
+    async function init() {
+      const groups = await loadPublicGroups();
+      setGroups(groups);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <ErrorBoundary componentPath="Home/Groups">
       <div>
-        <GroupItem />
+        {groups.map((group: { id: number }) => (
+          <GroupItem key={group.id} />
+        ))}
       </div>
     </ErrorBoundary>
   );
