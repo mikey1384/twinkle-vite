9675694a030ef456612d7592f88366620e504788
mikey1384 <mikey1384@gmail.com>
2024-06-01 17:53:57 +0700

    add GroupItem

---
+++ src/containers/Home/Groups/GroupItem.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function GroupItem() {
+  return <div>this is a group item</div>;
+}
--- src/containers/Home/Groups/index.tsx
+++ src/containers/Home/Groups/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import GroupItem from './GroupItem';
+import ErrorBoundary from '~/components/ErrorBoundary';
 
 export default function Groups() {
-  return <div>this is groups</div>;
+  return (
+    <ErrorBoundary componentPath="Home/Groups">
+      <div>
+        <GroupItem />
+      </div>
+    </ErrorBoundary>
+  );
 }
