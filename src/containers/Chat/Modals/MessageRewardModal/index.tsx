06321c40974ad2fbaf0c7bc4cfb9d1312b845eb9
mikey1384 <mikey1384@gmail.com>
2024-06-20 18:45:22 +0900

    more updates

--- src/containers/Chat/Modals/MessageRewardModal/index.tsx
+++ src/containers/Chat/Modals/MessageRewardModal/index.tsx
@@ -6,6 +6,8 @@ import RewardLevelForm from '~/components/Forms/RewardLevelForm';
 import { rewardReasons } from '~/constants/defaultValues';
 import { addCommasToNumber } from '~/helpers/stringHelpers';
 import { useKeyContext } from '~/contexts';
+import { Color } from '~/constants/css';
+import { css } from '@emotion/css';
 
 export default function MessageRewardModal({
   onHide,
@@ -16,48 +18,92 @@ export default function MessageRewardModal({
   userToReward: any;
   onSubmit: (v: { reasonId: number; amount: number }) => void;
 }) {
-  const { isAdmin } = useKeyContext((v) => v.myState);
   const {
     done: { color: doneColor }
   } = useKeyContext((v) => v.theme);
+  const { twinkleCoins } = useKeyContext((v) => v.myState);
   const [selectedReasonId, setSelectedReasonId] = useState(0);
   const [rewardAmount, setRewardAmount] = useState(0);
   const submitDisabled = useMemo(
-    () => !rewardAmount || !selectedReasonId,
-    [rewardAmount, selectedReasonId]
+    () =>
+      !rewardAmount || !selectedReasonId || rewardAmount * 200 > twinkleCoins,
+    [rewardAmount, selectedReasonId, twinkleCoins]
+  );
+  const displayedRewardAmount = useMemo(
+    () => addCommasToNumber(rewardAmount * 200),
+    [rewardAmount]
   );
 
   return (
     <Modal onHide={onHide}>
-      <header>Reward {userToReward.username}</header>
+      <header
+        className={css`
+          font-size: 1.5rem;
+          font-weight: bold;
+        `}
+      >
+        Reward {userToReward.username}
+      </header>
       <main>
         <div style={{ width: '100%' }}>
           <div
+            className={css`
+              text-align: center;
+            `}
+          >
+            <div
+              className={css`
+                font-size: 2rem;
+                font-weight: bold;
+              `}
+            >
+              {displayedRewardAmount} XP
+            </div>
+            {!!rewardAmount && (
+              <div
+                className={css`
+                  margin-left: 1rem;
+                  color: ${Color.darkerGray()};
+                  font-size: 1.3rem;
+                `}
+              >
+                ({displayedRewardAmount} coins)
+              </div>
+            )}
+          </div>
+          {rewardAmount * 200 > twinkleCoins && (
+            <div
+              className={css`
+                width: 100%;
+                text-align: center;
+                color: red;
+                margin-bottom: 1rem;
+                font-size: 1.2rem;
+              `}
+            >
+              You do not have enough coins to reward this amount.
+            </div>
+          )}
+          <RewardLevelForm
+            icon="certificate"
+            extendedRewardLevels
+            rewardLevel={rewardAmount}
+            onSetRewardLevel={setRewardAmount}
             style={{
               width: '100%',
               textAlign: 'center',
-              fontSize: '2rem',
-              fontWeight: 'bold',
-              marginBottom: '1.5rem'
+              marginTop: '1.5rem',
+              fontSize: '3rem'
             }}
-          >
-            <span>{addCommasToNumber(rewardAmount * 200)} XP</span>
-          </div>
-          <RewardLevelForm
-            icon="certificate"
-            extendedRewardLevels={!!isAdmin}
-            rewardLevel={rewardAmount}
-            onSetRewardLevel={setRewardAmount}
-            style={{ width: '100%', textAlign: 'center', fontSize: '3rem' }}
           />
         </div>
         <div
-          style={{
-            marginTop: '1rem',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center'
-          }}
+          className={css`
+            margin-top: 1rem;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+          `}
         >
           {Object.keys(rewardReasons).map((key) => (
             <RewardReason
