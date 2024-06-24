f8e9a1b091fcee0520085c3d782ea32e3d63d6b2
mikey1384 <mikey1384@gmail.com>
2024-06-24 21:34:19 +0900

    more updates

--- src/containers/Chat/Modals/TopicSettingsModal/AIChatMenu.tsx
+++ src/containers/Chat/Modals/TopicSettingsModal/AIChatMenu.tsx
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import ErrorBoundary from '~/components/ErrorBoundary';
 import SwitchButton from '~/components/Buttons/SwitchButton';
+import Textarea from '~/components/Texts/Textarea';
+import { exceedsCharLimit, addEmoji } from '~/helpers/stringHelpers';
 import { css } from '@emotion/css';
 
 export default function AIChatMenu() {
   const [isCustomInstructionsOn, setIsCustomInstructionsOn] = useState(false);
+  const [customInstructions, setCustomInstructions] = useState('');
+  const commentExceedsCharLimit = useMemo(
+    () =>
+      exceedsCharLimit({
+        contentType: 'comment',
+        text: customInstructions
+      }),
+    [customInstructions]
+  );
+
   return (
     <ErrorBoundary componentPath="Chat/Modals/TopicSettingsModal/AIChatMenu">
       <div
@@ -12,6 +24,8 @@ export default function AIChatMenu() {
           margin-top: 0.5rem;
           width: 100%;
           display: flex;
+          flex-direction: column;
+          align-items: center;
           justify-content: center;
         `}
       >
@@ -29,7 +43,31 @@ export default function AIChatMenu() {
           }}
           label="Custom Instructions"
         />
+        {isCustomInstructionsOn && (
+          <div style={{ width: '100%' }}>
+            <Textarea
+              placeholder="Enter instructions..."
+              style={{
+                width: '100%',
+                position: 'relative'
+              }}
+              hasError={!!commentExceedsCharLimit}
+              minRows={3}
+              value={customInstructions}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                setCustomInstructions(event.target.value)
+              }
+              onKeyUp={handleKeyUp}
+            />
+          </div>
+        )}
       </div>
     </ErrorBoundary>
   );
+
+  function handleKeyUp(event: { key: string; target: { value: string } }) {
+    if (event.key === ' ') {
+      setCustomInstructions(addEmoji(event.target.value));
+    }
+  }
 }
