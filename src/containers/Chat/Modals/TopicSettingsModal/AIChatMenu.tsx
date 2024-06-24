4d26bfd32ac22f5ce4b3b748bc58475242c57d11
mikey1384 <mikey1384@gmail.com>
2024-06-24 21:11:07 +0900

    add custom instructions button

--- src/containers/Chat/Modals/TopicSettingsModal/AIChatMenu.tsx
+++ src/containers/Chat/Modals/TopicSettingsModal/AIChatMenu.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ErrorBoundary from '~/components/ErrorBoundary';
+import SwitchButton from '~/components/Buttons/SwitchButton';
+import { css } from '@emotion/css';
 
 export default function AIChatMenu() {
+  const [isCustomInstructionsOn, setIsCustomInstructionsOn] = useState(false);
   return (
     <ErrorBoundary componentPath="Chat/Modals/TopicSettingsModal/AIChatMenu">
-      <div>
-        <div>AIChatMenu</div>
+      <div
+        className={css`
+          margin-top: 0.5rem;
+          width: 100%;
+          display: flex;
+          justify-content: center;
+        `}
+      >
+        <SwitchButton
+          checked={isCustomInstructionsOn}
+          onChange={() =>
+            setIsCustomInstructionsOn(
+              (isCustomInstructionsOn) => !isCustomInstructionsOn
+            )
+          }
+          labelStyle={{
+            fontWeight: 'bold',
+            fontSize: '1.3rem',
+            color: '#333'
+          }}
+          label="Custom Instructions"
+        />
       </div>
     </ErrorBoundary>
   );
