12d1ce1544e0a2b5e064855cf2b25f239cbdf3c2
mikey1384 <mikey1384@gmail.com>
2024-08-03 18:00:41 +0700

    minor fix

--- src/components/AchievementItem/FormModal/Submitted.tsx
+++ src/components/AchievementItem/FormModal/Submitted.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Color } from '~/constants/css';
+import { css } from '@emotion/css';
 import Button from '~/components/Button';
 
 export default function Submitted({
@@ -12,35 +13,73 @@ export default function Submitted({
   onTryAgain: () => void;
 }) {
   return (
-    <div style={{ marginBottom: '1rem' }}>
-      {status === 'pending' && (
-        <div>
-          <p>Your submission has been received</p>
-          <p>Please wait for verification</p>
-        </div>
-      )}
-      {status === 'approved' && (
-        <div>
-          <p>
-            Your submission has been{' '}
-            <span style={{ color: Color.limeGreen() }}>verified</span>
-          </p>
-        </div>
-      )}
-      {status === 'rejected' && (
-        <div>
-          <p>
-            Your submission was{' '}
-            <span style={{ color: Color.redOrange() }}>rejected</span>
-          </p>
-          <p>{value}</p>
-          <div style={{ marginTop: '1rem' }}>
-            <Button filled color="logoBlue" onClick={onTryAgain}>
-              Try again
-            </Button>
-          </div>
-        </div>
-      )}
+    <div
+      className={css`
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+      `}
+    >
+      <div
+        className={css`
+          font-size: 1.6rem;
+          font-weight: bold;
+          margin-bottom: 1rem;
+          text-align: center;
+          color: ${Color.darkerGray()};
+          p {
+            margin-bottom: 0.5rem;
+          }
+        `}
+      >
+        {renderStatusView()}
+      </div>
     </div>
   );
+
+  function renderStatusView() {
+    switch (status) {
+      case 'pending':
+        return (
+          <div>
+            <p>Your submission has been received</p>
+            <p>Please wait for verification</p>
+          </div>
+        );
+      case 'approved':
+        return (
+          <div>
+            <p>
+              Your submission has been{' '}
+              <span style={{ color: Color.limeGreen() }}>verified</span>
+            </p>
+          </div>
+        );
+      case 'rejected':
+        return (
+          <div>
+            <p>
+              Your submission was{' '}
+              <span style={{ color: Color.redOrange() }}>rejected</span>
+            </p>
+            <p>{value}</p>
+            <div
+              style={{
+                marginTop: '2rem',
+                display: 'flex',
+                justifyContent: 'center'
+              }}
+            >
+              <Button filled color="logoBlue" onClick={onTryAgain}>
+                Try again
+              </Button>
+            </div>
+          </div>
+        );
+      default:
+        return null;
+    }
+  }
 }
