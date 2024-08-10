0aa80ddab2b7c42ede170d155afcab5e2b6366ea
mikey1384 <mikey1384@gmail.com>
2024-08-10 09:31:06 +0700

    more fix

--- src/containers/Chat/LeftMenu/index.tsx
+++ src/containers/Chat/LeftMenu/index.tsx
@@ -6,8 +6,7 @@ import Icon from '~/components/Icon';
 import Tabs from './Tabs';
 import Subchannels from './Subchannels';
 import PinnedTopics from './PinnedTopics';
-import ciel from '~/assets/ciel.png';
-import zero from '~/assets/zero.png';
+import AIButton from './AIButton';
 import { Color, mobileMaxWidth } from '~/constants/css';
 import { css } from '@emotion/css';
 import { useAppContext, useChatContext, useKeyContext } from '~/contexts';
@@ -20,7 +19,7 @@ import {
   GENERAL_CHAT_ID,
   VOCAB_CHAT_TYPE
 } from '~/constants/defaultValues';
-import {isMobile, isTablet} from '~/helpers';
+import { isMobile, isTablet } from '~/helpers';
 import { matchPath, useNavigate, useLocation } from 'react-router-dom';
 import ErrorBoundary from '~/components/ErrorBoundary';
 
@@ -235,15 +234,15 @@ function LeftMenu({
               `}
             >
               <AIButton
+                aiName="ciel"
                 loading={cielChatLoading}
                 onClick={() => handleAIClick('ciel')}
-                src={ciel}
                 alt="Ciel"
               />
               <AIButton
+                aiName="zero"
                 loading={zeroChatLoading}
                 onClick={() => handleAIClick('zero')}
-                src={zero}
                 alt="Zero"
               />
             </div>
@@ -315,63 +314,6 @@ function LeftMenu({
       </div>
     </ErrorBoundary>
   );
-
-  function AIButton({
-    loading,
-    onClick,
-    src,
-    alt
-  }: {
-    loading: boolean;
-    onClick: () => void;
-    src: string;
-    alt: string;
-  }) {
-    return (
-      <button
-        className={css`
-          border: none;
-          cursor: pointer;
-          opacity: ${loading ? 0.5 : 1};
-          background: none;
-          padding: 0;
-        `}
-        onClick={onClick}
-        disabled={loading}
-      >
-        <div
-          className={css`
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            width: 4rem;
-            height: 4rem;
-            position: relative;
-          `}
-        >
-          <img
-            src={src}
-            alt={alt}
-            className={css`
-              width: 100%;
-              height: 100%;
-              background-size: cover;
-              border-radius: 4px;
-            `}
-          />
-          {loading && (
-            <Icon
-              icon="spinner"
-              pulse
-              className={css`
-                position: absolute;
-              `}
-            />
-          )}
-        </div>
-      </button>
-    );
-  }
 
   async function handleAIClick(type: 'ciel' | 'zero') {
     if (type === 'zero') {
