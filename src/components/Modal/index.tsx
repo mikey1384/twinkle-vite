0edf4dc9d92492dd397a5e81d93d7e1843cc8ecd
mikey1384 <mikey1384@gmail.com>
2024-08-21 07:14:05 +0700

    bug fix

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -2,11 +2,8 @@ import React, { RefObject, useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { css } from '@emotion/css';
 import { borderRadius, Color, mobileMaxWidth } from '~/constants/css';
-import { isMobile } from '~/helpers';
 import ErrorBoundary from '~/components/ErrorBoundary';
 import Content from './Content';
-
-const deviceIsMobile = isMobile(navigator);
 
 export default function Modal({
   className,
@@ -39,21 +36,21 @@ export default function Modal({
   style?: object;
   wrapped?: boolean;
 }) {
-  const [modalHeight, setModalHeight] = useState('auto');
+  const [maxHeight, setMaxHeight] = useState('100vh');
 
   useEffect(() => {
-    function updateModalDimensions() {
-      const windowHeight = window.innerHeight;
-
+    const handleResize = () => {
       if (!wrapped) {
-        const maxHeight = deviceIsMobile ? windowHeight : windowHeight - 160;
-        setModalHeight(`${maxHeight}px`);
+        setMaxHeight(`${window.innerHeight * 0.9}px`);
       }
-    }
+    };
 
-    updateModalDimensions();
-    window.addEventListener('resize', updateModalDimensions);
-    return () => window.removeEventListener('resize', updateModalDimensions);
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
   }, [wrapped]);
 
   const modalWidth = {
@@ -75,7 +72,6 @@ export default function Modal({
     : large
     ? 'large'
     : 'default';
-
   const Modal = (
     <ErrorBoundary componentPath="Modal/index">
       <div
@@ -98,7 +94,7 @@ export default function Modal({
             right: 0;
             left: 0;
             bottom: 0;
-            padding-bottom: ${deviceIsMobile ? '0' : '7rem'};
+            padding-bottom: 7rem;
             background: ${Color.black(0.5)};
             overflow-y: scroll;
             -webkit-overflow-scrolling: touch;
@@ -108,25 +104,20 @@ export default function Modal({
           <Content
             closeColor={closeColor}
             closeWhenClickedOutside={closeWhenClickedOutside}
-            style={{
-              height: wrapped ? 'auto' : modalHeight,
-              maxHeight: wrapped ? 'none' : '100%',
-              ...modalStyle
-            }}
+            style={modalStyle}
             className={css`
               position: relative;
-              border-radius: ${deviceIsMobile ? '0' : borderRadius};
+              border-radius: ${borderRadius};
               background: #fff;
-              width: ${deviceIsMobile ? '100%' : modalWidth[widthKey]};
-              top: ${deviceIsMobile ? '0' : '3rem'};
-              margin-left: ${deviceIsMobile ? '0' : marginLeft[widthKey]};
-              box-shadow: ${deviceIsMobile
-                ? 'none'
-                : `3px 4px 5px ${Color.black()}`};
+              width: ${modalWidth[widthKey]};
+              top: 3rem;
+              margin-left: ${marginLeft[widthKey]};
+              box-shadow: 3px 4px 5px ${Color.black()};
               display: flex;
               flex-direction: column;
               justify-content: space-between;
-              min-height: ${deviceIsMobile ? '100%' : '30vh'};
+              min-height: 30vh;
+              ${wrapped ? '' : `max-height: ${maxHeight};`}
               > header {
                 display: flex;
                 align-items: center;
@@ -138,7 +129,6 @@ export default function Modal({
                 margin-top: 0.5rem;
                 @media (max-width: ${mobileMaxWidth}) {
                   font-size: 1.7rem;
-                  padding: 1.5rem;
                 }
               }
               > main {
@@ -148,29 +138,24 @@ export default function Modal({
                 flex-direction: column;
                 justify-content: flex-start;
                 align-items: center;
-                overflow-y: ${wrapped ? 'visible' : 'auto'};
+                ${wrapped ? '' : 'overflow-y: auto;'}
                 flex-grow: 1;
                 @media (max-width: ${mobileMaxWidth}) {
-                  padding: 1rem;
-                  overflow-y: ${wrapped ? 'visible' : 'auto'};
+                  overflow-y: visible;
+                  flex-grow: 0;
                 }
               }
               > footer {
-                padding: 1.5rem;
+                padding: 1.5rem 1.5rem 1.5rem 1.5rem;
                 display: flex;
                 align-items: center;
                 justify-content: flex-end;
                 border-top: 1px solid ${Color.borderGray()};
-                @media (max-width: ${mobileMaxWidth}) {
-                  padding: 1rem;
-                }
               }
               @media (max-width: ${mobileMaxWidth}) {
                 width: 100% !important;
                 margin: 0;
-                height: 100% !important;
                 max-height: none;
-                border-radius: 0;
               }
             `}
             onHide={onHide}
