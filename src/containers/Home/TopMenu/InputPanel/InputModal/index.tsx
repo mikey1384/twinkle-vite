2fce8028b045634a2767d7447988dea570e0e190
mikey1384 <mikey1384@gmail.com>
2024-08-25 08:45:24 +0700

    more fix

--- src/containers/Home/TopMenu/InputPanel/InputModal/index.tsx
+++ src/containers/Home/TopMenu/InputPanel/InputModal/index.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ContentInput from './ContentInput';
 import SubjectInput from './SubjectInput';
 import ErrorBoundary from '~/components/ErrorBoundary';
 import Modal from '~/components/Modal';
 import Button from '~/components/Button';
+import { useAppContext } from '~/contexts';
 
 export default function InputModal({ onHide }: { onHide: () => void }) {
+  const checkDrafts = useAppContext((v) => v.requestHelpers.checkDrafts);
+  const [drafts, setDrafts] = useState([]);
+
+  useEffect(() => {
+    fetchDrafts();
+    async function fetchDrafts() {
+      const data = await checkDrafts({ contentType: 'subject' });
+      console.log('data', data);
+      setDrafts(data);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  console.log('drafts', drafts);
+
   return (
     <ErrorBoundary componentPath="Home/Stories/InputPanel/InputModal">
       <Modal wrapped closeWhenClickedOutside={false} onHide={onHide}>
