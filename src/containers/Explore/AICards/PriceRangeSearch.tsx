1f3a7fc1e793d8dae756abc44f89f81f5fed3660
mikey1384 <mikey1384@gmail.com>
2024-08-26 18:51:55 +0700

    refine css for price range field

--- src/containers/Explore/AICards/PriceRangeSearch.tsx
+++ src/containers/Explore/AICards/PriceRangeSearch.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Input from '~/components/Texts/Input';
+import { css } from '@emotion/css';
+import { Color } from '~/constants/css';
 
 interface PriceRangeSearchProps {
   priceRange: { min: string; max: string };
@@ -11,21 +13,62 @@ export default function PriceRangeSearch({
   onPriceRangeChange
 }: PriceRangeSearchProps) {
   return (
-    <div style={{ display: 'flex', alignItems: 'center', marginTop: '1rem' }}>
+    <div
+      className={css`
+        margin-top: 0.5rem;
+        display: flex;
+        align-items: center;
+        border-radius: 20px;
+        padding: 0.3rem;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        border: 1px solid ${Color.borderGray()};
+        background-color: #fff;
+      `}
+    >
       <Input
-        placeholder="Min Price"
+        placeholder="Min"
         type="number"
         value={priceRange.min}
         onChange={(text) => onPriceRangeChange({ ...priceRange, min: text })}
-        style={{ width: '100px', marginRight: '0.5rem' }}
+        className={css`
+          width: 80px;
+          font-size: 1.3rem;
+          padding: 0.5rem;
+          border: none;
+          background-color: white;
+          color: ${Color.darkerGray()};
+          border-radius: 15px;
+          &::placeholder {
+            color: ${Color.gray()};
+          }
+        `}
       />
-      <span>-</span>
+      <span
+        className={css`
+          margin: 0 0.5rem;
+          color: ${Color.gray()};
+          font-size: 1.3rem;
+        `}
+      >
+        -
+      </span>
       <Input
-        placeholder="Max Price"
+        placeholder="Max"
         type="number"
         value={priceRange.max}
         onChange={(text) => onPriceRangeChange({ ...priceRange, max: text })}
-        style={{ width: '100px', marginLeft: '0.5rem' }}
+        className={css`
+          width: 80px;
+          font-size: 1.3rem;
+          padding: 0.5rem;
+          border: none;
+          background-color: white;
+          color: ${Color.darkerGray()};
+          border-radius: 15px;
+          &::placeholder {
+            color: ${Color.gray()};
+          }
+        `}
       />
     </div>
   );
