f8b9a00db0d2b635e7ace497ef4bb341a0dceed5
mikey1384 <mikey1384@gmail.com>
2024-08-30 19:53:29 +0700

    more refactoring

--- src/containers/Chat/Modals/TransactionModal/SelectGroupsModal/index.tsx
+++ src/containers/Chat/Modals/TransactionModal/SelectGroupsModal/index.tsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import Modal from '~/components/Modal';
 import Button from '~/components/Button';
+import Loading from '~/components/Loading';
 import { css } from '@emotion/css';
-import { cloudFrontURL } from '~/constants/defaultValues';
-import { Color, borderRadius } from '~/constants/css';
-import Icon from '~/components/Icon';
 import { useAppContext, useKeyContext } from '~/contexts';
+import GroupItem from './GroupItem';
 
 export default function SelectGroupsModal({
   onHide,
@@ -48,7 +47,7 @@ export default function SelectGroupsModal({
       <header>Select Groups</header>
       <main>
         {loading ? (
-          <div>Loading...</div>
+          <Loading />
         ) : (
           <div
             className={css`
@@ -60,85 +59,12 @@ export default function SelectGroupsModal({
             `}
           >
             {groups.map((group) => (
-              <div
+              <GroupItem
                 key={group.id}
-                className={css`
-                  display: flex;
-                  align-items: center;
-                  padding: 1rem;
-                  cursor: pointer;
-                  border-radius: ${borderRadius};
-                  background-color: ${selectedGroupIds.includes(group.id)
-                    ? Color.highlightGray()
-                    : 'transparent'};
-                  &:hover {
-                    background-color: ${Color.highlightGray()};
-                  }
-                `}
-                onClick={() => handleGroupSelect(group.id)}
-              >
-                <div
-                  className={css`
-                    width: 4rem;
-                    height: 4rem;
-                    border-radius: 50%;
-                    margin-right: 1.5rem;
-                    flex-shrink: 0;
-                    background-color: ${Color.lightGray()};
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                    overflow: hidden;
-                  `}
-                >
-                  {group.thumbPath ? (
-                    <img
-                      src={`${cloudFrontURL}/thumbs/${group.thumbPath}/thumb.png`}
-                      alt={group.channelName}
-                      className={css`
-                        width: 100%;
-                        height: 100%;
-                        object-fit: cover;
-                      `}
-                    />
-                  ) : (
-                    <Icon icon="users" style={{ fontSize: '2rem' }} />
-                  )}
-                </div>
-                <div
-                  className={css`
-                    flex-grow: 1;
-                  `}
-                >
-                  <div
-                    className={css`
-                      font-size: 1.3rem;
-                      font-weight: bold;
-                      color: ${Color.darkerGray()};
-                    `}
-                  >
-                    {group.channelName}
-                  </div>
-                  <div
-                    className={css`
-                      font-size: 1.1rem;
-                      color: ${Color.gray()};
-                    `}
-                  >
-                    {group.members} members
-                  </div>
-                </div>
-                {selectedGroupIds.includes(group.id) && (
-                  <Icon
-                    icon="check"
-                    style={{
-                      marginLeft: '1rem',
-                      color: Color.green(),
-                      fontSize: '1.5rem'
-                    }}
-                  />
-                )}
-              </div>
+                group={group}
+                isSelected={selectedGroupIds.includes(group.id)}
+                onSelect={handleGroupSelect}
+              />
             ))}
           </div>
         )}
