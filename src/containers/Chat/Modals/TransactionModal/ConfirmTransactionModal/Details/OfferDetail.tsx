fb163308cfe83a0fd41972aa19f879bfe4411cf4
mikey1384 <mikey1384@gmail.com>
2024-09-01 16:44:54 +0700

    more updates

--- src/containers/Chat/Modals/TransactionModal/ConfirmTransactionModal/Details/OfferDetail.tsx
+++ src/containers/Chat/Modals/TransactionModal/ConfirmTransactionModal/Details/OfferDetail.tsx
@@ -58,6 +58,34 @@ export default function OfferDetail({
     [groupIds, groupObjs]
   );
 
+  const offerItems = [
+    coins > 0 && (
+      <div key="coins" style={{ fontWeight: 'bold' }}>
+        <Icon
+          style={{ color: Color.brownOrange() }}
+          icon={['far', 'badge-dollar']}
+        />{' '}
+        <span style={{ color: Color.darkerGray() }}>
+          {addCommasToNumber(coins)}
+        </span>
+      </div>
+    ),
+    cardIds.length > 0 && (
+      <AICardsPreview
+        key="cards"
+        isOnModal
+        isAICardModalShown={isAICardModalShown}
+        cardIds={cardIds}
+        onSetAICardModalCardId={onSetAICardModalCardId}
+      />
+    ),
+    selectedGroups.length > 0 && (
+      <div key="groups" style={{ width: '100%', marginTop: '1rem' }}>
+        <SelectedGroups selectedGroups={selectedGroups} isConfirmationView />
+      </div>
+    )
+  ].filter(Boolean);
+
   return (
     <div
       style={{
@@ -100,42 +128,14 @@ export default function OfferDetail({
           flexDirection: 'column'
         }}
       >
-        {!!coins && (
-          <div
-            style={{
-              width: '100%',
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}
-          >
-            <div style={{ fontWeight: 'bold' }}>
-              <Icon
-                style={{ color: Color.brownOrange() }}
-                icon={['far', 'badge-dollar']}
-              />{' '}
-              <span style={{ color: Color.darkerGray() }}>
-                {addCommasToNumber(coins)}
-              </span>
-            </div>
-            {!!cardIds.length && <div style={{ padding: '1rem' }}>and</div>}
-          </div>
-        )}
-        <AICardsPreview
-          isOnModal
-          isAICardModalShown={isAICardModalShown}
-          cardIds={cardIds}
-          onSetAICardModalCardId={onSetAICardModalCardId}
-        />
-        {!!selectedGroups.length && (
-          <div style={{ width: '100%', marginTop: '1rem' }}>
-            <SelectedGroups
-              selectedGroups={selectedGroups}
-              isConfirmationView
-            />
-          </div>
-        )}
+        {offerItems.map((item, index) => (
+          <React.Fragment key={index}>
+            {index > 0 && (
+              <div style={{ padding: '1rem', fontWeight: 'bold' }}>and</div>
+            )}
+            {item}
+          </React.Fragment>
+        ))}
       </div>
     </div>
   );
