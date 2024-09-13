da38e34fbf44519738482bd6066db912a5339c9f
mikey1384 <mikey1384@gmail.com>
2024-09-13 21:29:44 +0700

    more updates

--- src/containers/Build/index.tsx
+++ src/containers/Build/index.tsx
@@ -7,27 +7,54 @@ import { useTransition, animated } from 'react-spring';
 export default function Build() {
   const [isBuildScreenShown, setIsBuildScreenShown] = useState(false);
 
-  const transitions = useTransition(isBuildScreenShown, {
-    from: { opacity: 0, transform: 'translateX(0%)' },
+  const mainMenuTransitions = useTransition(!isBuildScreenShown, {
+    from: { opacity: 0, transform: 'translateX(-100%)' },
     enter: { opacity: 1, transform: 'translateX(0%)' },
-    leave: { opacity: 0, transform: 'translateX(-100%)' }
+    leave: { opacity: 0, transform: 'translateX(-100%)' },
+    config: { duration: 300 }
+  });
+
+  const projectTransitions = useTransition(isBuildScreenShown, {
+    from: { opacity: 0, transform: 'translateX(100%)' },
+    enter: { opacity: 1, transform: 'translateX(0%)' },
+    leave: { opacity: 0, transform: 'translateX(100%)' },
+    config: { duration: 300 }
   });
 
   return (
     <ErrorBoundary componentPath="App">
-      {transitions((styles, item) =>
-        item ? (
-          <animated.div style={{ ...styles, width: '100%', height: '100%' }}>
-            <Project onSetIsBuildScreenShown={setIsBuildScreenShown} />
-          </animated.div>
-        ) : (
-          <animated.div style={{ ...styles, width: '100%', height: '100%' }}>
-            <MainMenu
-              onOptionSelect={(option: string) => console.log(option)}
-              onSetIsBuildScreenShown={setIsBuildScreenShown}
-            />
-          </animated.div>
-        )
+      {mainMenuTransitions(
+        (styles, item) =>
+          item && (
+            <animated.div
+              style={{
+                ...styles,
+                width: '100%',
+                height: '100%',
+                position: 'absolute'
+              }}
+            >
+              <MainMenu
+                onOptionSelect={(option: string) => console.log(option)}
+                onSetIsBuildScreenShown={setIsBuildScreenShown}
+              />
+            </animated.div>
+          )
+      )}
+      {projectTransitions(
+        (styles, item) =>
+          item && (
+            <animated.div
+              style={{
+                ...styles,
+                width: '100%',
+                height: '100%',
+                position: 'absolute'
+              }}
+            >
+              <Project onSetIsBuildScreenShown={setIsBuildScreenShown} />
+            </animated.div>
+          )
       )}
     </ErrorBoundary>
   );
