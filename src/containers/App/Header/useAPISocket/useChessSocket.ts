9c63e2d1b94bc0cf7a6ef7b163597f8d0e1200ca
mikey1384 <mikey1384@gmail.com>
2024-10-08 22:36:45 +0700

    more updates

---
+++ src/containers/App/Header/useAPISocket/useChessSocket.ts
@@ -0,0 +1,136 @@
+import { useEffect } from 'react';
+import { socket } from '~/constants/sockets/api';
+import { useChatContext } from '~/contexts';
+
+export default function useChessSocket({
+  selectedChannelId
+}: {
+  selectedChannelId: number;
+}) {
+  const onSetChessGameState = useChatContext(
+    (v) => v.actions.onSetChessGameState
+  );
+  const onSetChessModalShown = useChatContext(
+    (v) => v.actions.onSetChessModalShown
+  );
+  const onSubmitMessage = useChatContext((v) => v.actions.onSubmitMessage);
+  const onUpdateRecentChessMessage = useChatContext(
+    (v) => v.actions.onUpdateRecentChessMessage
+  );
+
+  useEffect(() => {
+    socket.on('chess_move_made', handleChessMoveMade);
+    socket.on('chess_rewind_requested', handleChessRewindRequest);
+
+    socket.on('canceled_chess_rewind', handleChessRewindCanceled);
+    socket.on('declined_chess_rewind', handleChessRewindDeclined);
+    socket.on('rewound_chess_game', handleChessRewind);
+
+    return function cleanUp() {
+      socket.removeListener('chess_move_made', handleChessMoveMade);
+      socket.removeListener('chess_rewind_requested', handleChessRewindRequest);
+
+      socket.removeListener('canceled_chess_rewind', handleChessRewindCanceled);
+      socket.removeListener('declined_chess_rewind', handleChessRewindDeclined);
+      socket.removeListener('rewound_chess_game', handleChessRewind);
+    };
+
+    function handleChessMoveMade({ channelId }: { channelId: number }) {
+      if (channelId === selectedChannelId) {
+        onSetChessModalShown(false);
+      }
+    }
+
+    function handleChessRewindCanceled({
+      channelId,
+      messageId,
+      cancelMessage,
+      sender,
+      timeStamp
+    }: {
+      channelId: number;
+      messageId: number;
+      cancelMessage: string;
+      sender: any;
+      timeStamp: number;
+    }) {
+      onSubmitMessage({
+        message: {
+          channelId,
+          id: messageId,
+          content: cancelMessage,
+          userId: sender.userId,
+          username: sender.username,
+          profilePicUrl: sender.profilePicUrl,
+          isNotification: true,
+          timeStamp
+        },
+        messageId
+      });
+      onSetChessGameState({ channelId, newState: { rewindRequestId: null } });
+    }
+
+    function handleChessRewindDeclined({
+      channelId,
+      declineMessage,
+      messageId,
+      sender,
+      timeStamp
+    }: {
+      channelId: number;
+      declineMessage: string;
+      messageId: number;
+      sender: any;
+      timeStamp: number;
+    }) {
+      onSubmitMessage({
+        message: {
+          channelId,
+          id: messageId,
+          content: declineMessage,
+          userId: sender.userId,
+          username: sender.username,
+          profilePicUrl: sender.profilePicUrl,
+          isNotification: true,
+          timeStamp
+        },
+        messageId
+      });
+      onSetChessGameState({
+        channelId,
+        newState: { rewindRequestId: null }
+      });
+    }
+
+    function handleChessRewindRequest({
+      channelId,
+      messageId
+    }: {
+      channelId: number;
+      messageId: number;
+    }) {
+      onSetChessGameState({
+        channelId,
+        newState: { rewindRequestId: messageId }
+      });
+    }
+
+    function handleChessRewind({
+      channelId,
+      message
+    }: {
+      channelId: number;
+      message: any;
+    }) {
+      onUpdateRecentChessMessage({ channelId, message });
+      onSetChessGameState({
+        channelId,
+        newState: { rewindRequestId: null }
+      });
+      onSubmitMessage({
+        message,
+        messageId: message.id
+      });
+    }
+  });
+}
