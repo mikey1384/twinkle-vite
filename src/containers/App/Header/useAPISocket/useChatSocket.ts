5c4bf974f81e27ef00cfd9c8b5fabc3820751433
mikey1384 <mikey1384@gmail.com>
2024-10-08 23:02:55 +0700

    more refactoring

--- src/containers/App/Header/useAPISocket/useChatSocket.ts
+++ src/containers/App/Header/useAPISocket/useChatSocket.ts
@@ -1,13 +1,26 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { socket } from '~/constants/sockets/api';
-import { useChatContext, useKeyContext, useViewContext } from '~/contexts';
+import { GENERAL_CHAT_ID } from '~/constants/defaultValues';
+import {
+  useAppContext,
+  useChatContext,
+  useNotiContext,
+  useKeyContext,
+  useViewContext
+} from '~/contexts';
 
 export default function useChatSocket({
   channelsObj,
-  selectedChannelId
+  onUpdateMyXp,
+  selectedChannelId,
+  subchannelId,
+  usingChatRef
 }: {
   channelsObj: Record<number, any>;
+  onUpdateMyXp: () => void;
   selectedChannelId: number;
+  subchannelId: number;
+  usingChatRef: React.RefObject<boolean>;
 }) {
   const { userId } = useKeyContext((v) => v.myState);
 
@@ -16,6 +29,12 @@ export default function useChatSocket({
 
   const onAddReactionToMessage = useChatContext(
     (v) => v.actions.onAddReactionToMessage
+  );
+  const onChangeChannelOwner = useChatContext(
+    (v) => v.actions.onChangeChannelOwner
+  );
+  const onChangeChatSubject = useChatContext(
+    (v) => v.actions.onChangeChatSubject
   );
   const onDeleteMessage = useChatContext((v) => v.actions.onDeleteMessage);
   const onEditMessage = useChatContext((v) => v.actions.onEditMessage);
@@ -31,26 +50,48 @@ export default function useChatSocket({
   const onEnableChatSubject = useChatContext(
     (v) => v.actions.onEnableChatSubject
   );
+  const onNotifyChatSubjectChange = useNotiContext(
+    (v) => v.actions.onNotifyChatSubjectChange
+  );
   const onReceiveFirstMsg = useChatContext((v) => v.actions.onReceiveFirstMsg);
+  const onReceiveMessage = useChatContext((v) => v.actions.onReceiveMessage);
+  const onReceiveMessageOnDifferentChannel = useChatContext(
+    (v) => v.actions.onReceiveMessageOnDifferentChannel
+  );
   const onRemoveReactionFromMessage = useChatContext(
     (v) => v.actions.onRemoveReactionFromMessage
+  );
+  const onUpdateCurrentTransactionId = useChatContext(
+    (v) => v.actions.onUpdateCurrentTransactionId
+  );
+
+  const updateChatLastRead = useAppContext(
+    (v) => v.requestHelpers.updateChatLastRead
+  );
+  const updateSubchannelLastRead = useAppContext(
+    (v) => v.requestHelpers.updateSubchannelLastRead
   );
 
   useEffect(() => {
     socket.on('online_status_changed', handleOnlineStatusChange);
     socket.on('away_status_changed', handleAwayStatusChange);
     socket.on('busy_status_changed', handleBusyStatusChange);
+    socket.on('channel_owner_changed', handleChangeChannelOwner);
     socket.on('chat_invitation_received', handleChatInvitation);
     socket.on('chat_message_deleted', onDeleteMessage);
     socket.on('chat_message_edited', onEditMessage);
     socket.on('chat_reaction_added', onAddReactionToMessage);
     socket.on('chat_reaction_removed', onRemoveReactionFromMessage);
     socket.on('chat_subject_purchased', onEnableChatSubject);
+    socket.on('new_message_received', handleReceiveMessage);
+    socket.on('new_wordle_attempt_received', handleNewWordleAttempt);
+    socket.on('subject_changed', handleTopicChange);
 
     return function cleanUp() {
       socket.removeListener('online_status_changed', handleOnlineStatusChange);
       socket.removeListener('away_status_changed', handleAwayStatusChange);
       socket.removeListener('busy_status_changed', handleBusyStatusChange);
+      socket.removeListener('channel_owner_changed', handleChangeChannelOwner);
       socket.removeListener('chat_invitation_received', handleChatInvitation);
       socket.removeListener('chat_message_deleted', onDeleteMessage);
       socket.removeListener('chat_message_edited', onEditMessage);
@@ -60,6 +101,12 @@ export default function useChatSocket({
         onRemoveReactionFromMessage
       );
       socket.removeListener('chat_subject_purchased', onEnableChatSubject);
+      socket.removeListener('new_message_received', handleReceiveMessage);
+      socket.removeListener(
+        'new_wordle_attempt_received',
+        handleNewWordleAttempt
+      );
+      socket.removeListener('subject_changed', handleTopicChange);
     };
   });
 
@@ -86,6 +133,20 @@ export default function useChatSocket({
       onChangeBusyStatus({ userId, isBusy });
     }
   }
+
+  function handleChangeChannelOwner({
+    channelId,
+    message,
+    newOwner
+  }: {
+    channelId: number;
+    message: any;
+    newOwner: any;
+  }) {
+    updateChatLastRead(channelId);
+    onChangeChannelOwner({ channelId, message, newOwner });
+  }
+
   function handleChatInvitation({
     message,
     members,
@@ -120,6 +181,48 @@ export default function useChatSocket({
       pathId
     });
   }
+
+  function handleNewWordleAttempt({
+    channelId,
+    channelName,
+    user,
+    message,
+    pathId
+  }: {
+    channelId: number;
+    channelName: string;
+    user: any;
+    message: any;
+    pathId: string;
+  }) {
+    const isForCurrentChannel = channelId === selectedChannelId;
+    if (isForCurrentChannel) {
+      if (usingChatRef.current) {
+        updateChatLastRead(channelId);
+      }
+      onReceiveMessage({
+        message,
+        pageVisible,
+        usingChat: usingChatRef.current
+      });
+    }
+    if (!isForCurrentChannel) {
+      onReceiveMessageOnDifferentChannel({
+        message,
+        channel: {
+          id: channelId,
+          channelName,
+          pathId
+        },
+        pageVisible,
+        usingChat: usingChatRef.current
+      });
+    }
+    if (user.id === userId && user.newXp) {
+      onUpdateMyXp();
+    }
+  }
+
   function handleOnlineStatusChange({
     userId,
     member,
@@ -130,5 +233,107 @@ export default function useChatSocket({
     isOnline: boolean;
   }) {
     onChangeOnlineStatus({ userId, member, isOnline });
+  }
+
+  async function handleReceiveMessage({
+    message,
+    channel,
+    newMembers,
+    isNotification
+  }: {
+    message: any;
+    channel: any;
+    newMembers: any[];
+    isNotification: boolean;
+  }) {
+    const messageIsForCurrentChannel = message.channelId === selectedChannelId;
+    const senderIsUser = message.userId === userId && !isNotification;
+    if (senderIsUser && pageVisible) return;
+    if (messageIsForCurrentChannel) {
+      if (usingChatRef.current) {
+        updateChatLastRead(message.channelId);
+        if (message.subchannelId === subchannelId) {
+          updateSubchannelLastRead(message.subchannelId);
+        }
+      }
+      onReceiveMessage({
+        message,
+        pageVisible,
+        usingChat: usingChatRef.current,
+        newMembers,
+        currentSubchannelId: subchannelId
+      });
+    }
+    if (!messageIsForCurrentChannel) {
+      onReceiveMessageOnDifferentChannel({
+        message,
+        channel,
+        pageVisible,
+        usingChat: usingChatRef.current,
+        newMembers
+      });
+    }
+    if (message.transactionDetails?.id) {
+      onUpdateCurrentTransactionId({
+        channelId: message.channelId,
+        transactionId: message.transactionDetails.id
+      });
+    }
+    if (message.targetMessage?.userId === userId && message.rewardAmount) {
+      onUpdateMyXp();
+    }
+  }
+
+  function handleTopicChange({
+    message,
+    channelId,
+    pathId,
+    channelName,
+    subchannelId,
+    subject,
+    topicObj,
+    isFeatured
+  }: {
+    message: any;
+    channelId: number;
+    pathId: number | string;
+    channelName: string;
+    subchannelId: number;
+    subject: string;
+    topicObj: any;
+    isFeatured: boolean;
+  }) {
+    const messageIsForCurrentChannel = message.channelId === selectedChannelId;
+    const senderIsUser = message.userId === userId;
+
+    if (senderIsUser) return;
+
+    if (channelId === GENERAL_CHAT_ID && !subchannelId) {
+      onNotifyChatSubjectChange(subject);
+    }
+
+    onChangeChatSubject({
+      subject,
+      topicObj,
+      channelId,
+      subchannelId,
+      isFeatured
+    });
+
+    if (messageIsForCurrentChannel) {
+      onReceiveMessage({ message, pageVisible });
+    } else {
+      onReceiveMessageOnDifferentChannel({
+        pageVisible,
+        message,
+        channel: {
+          id: channelId,
+          pathId,
+          channelName,
+          isHidden: false,
+          numUnreads: 1
+        }
+      });
+    }
   }
 }
