1ff70bc41e7ed37b912d065bc3a136259d7efdba
mikey1384 <mikey1384@gmail.com>
2024-10-15 17:36:47 +0700

    increase max call time for ai

--- src/containers/Chat/ChatFlatButton.tsx
+++ src/containers/Chat/ChatFlatButton.tsx
@@ -90,7 +90,7 @@ export default function ChatFlatButton({
         }
       `}
       style={style}
-      onClick={onClick}
+      onClick={disabled ? undefined : onClick}
       disabled={disabled}
     >
       <Icon icon={icon} />
