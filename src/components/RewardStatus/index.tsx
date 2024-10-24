d86c16ec96f3b07f5e8a4b3a63a238e228b3aeae
mikey1384 <mikey1384@gmail.com>
2024-10-24 08:26:22 +0700

    remove unnecessary reward amount desc

--- src/components/RewardStatus/index.tsx
+++ src/components/RewardStatus/index.tsx
@@ -1,9 +1,9 @@
 import React, { memo, useEffect, useState, useMemo, useCallback } from 'react';
 import { css } from '@emotion/css';
 import { mobileMaxWidth } from '~/constants/css';
-import { addCommasToNumber, stringIsEmpty } from '~/helpers/stringHelpers';
+import { stringIsEmpty } from '~/helpers/stringHelpers';
 import { returnTheme } from '~/helpers';
-import { returnMaxRewards, SELECTED_LANGUAGE } from '~/constants/defaultValues';
+import { returnMaxRewards } from '~/constants/defaultValues';
 import LoadMoreButton from '~/components/Buttons/LoadMoreButton';
 import Comment from './Comment';
 import ErrorBoundary from '~/components/ErrorBoundary';
@@ -66,19 +66,6 @@ function RewardStatus({
     );
     return Math.min(total, maxRewards);
   }, [maxRewards, rewards]);
-
-  const rewardStatusLabel = useMemo(() => {
-    if (SELECTED_LANGUAGE === 'kr') {
-      return `총 ${amountRewarded}개의 트윈클(${addCommasToNumber(
-        amountRewarded * 200
-      )} XP)이 지급되었습니다 (최대 ${maxRewards}개)`;
-    }
-    return `${amountRewarded} Twinkle${
-      amountRewarded > 1 ? 's' : ''
-    } (${addCommasToNumber(
-      amountRewarded * 200
-    )} XP) rewarded out of max ${maxRewards}`;
-  }, [amountRewarded, maxRewards]);
 
   const handleLoadMore = useCallback(() => {
     setNumLoaded((prev) => prev + LOAD_MORE_COUNT);
@@ -161,21 +148,6 @@ function RewardStatus({
         `}`}
       >
         <Starmarks stars={amountRewarded} />
-        <div
-          className={css`
-            font-size: 1.15rem;
-            font-weight: 400;
-            margin-top: 0.3rem;
-            opacity: 0.85;
-            @media (max-width: ${mobileMaxWidth}) {
-              font-size: 1.2rem;
-              margin-top: 0.4rem;
-              line-height: 1.4;
-            }
-          `}
-        >
-          {rewardStatusLabel}
-        </div>
       </div>
       {numLoaded < sortedRewards.length && (
         <LoadMoreButton
