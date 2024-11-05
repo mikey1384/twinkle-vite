47c49b910da592cfa062970b347a75519d8fd984
mikey1384 <mikey1384@gmail.com>
2024-11-05 17:31:00 +0700

    version up

--- src/constants/defaultValues.ts
+++ src/constants/defaultValues.ts
@@ -5,7 +5,7 @@ import { Color } from '~/constants/css';
 // General Configurations
 // ===========================
 
-export const clientVersion = '1.9.11';
+export const clientVersion = '1.9.12';
 export const MIKEY_ID = 5;
 export const CIEL_TWINKLE_ID = Number(import.meta.env.VITE_CIEL_TWINKLE_ID);
 export const CIEL_PFP_URL = import.meta.env.VITE_CIEL_PFP_URL;
