e5759600add0ca4ecf1c990a25442e6b298415fe
mikey1384 <mikey1384@gmail.com>
2024-11-05 09:12:44 +0700

    simplify ai card feed notification

--- src/containers/Chat/LeftMenu/Collect/AICards.tsx
+++ src/containers/Chat/LeftMenu/Collect/AICards.tsx
@@ -1,16 +1,10 @@
 import React, { useMemo } from 'react';
 import Icon from '~/components/Icon';
-import localize from '~/constants/localize';
 import { css } from '@emotion/css';
 import { tabletMaxWidth, mobileMaxWidth } from '~/constants/css';
-import { useChatContext, useKeyContext } from '~/contexts';
-import { addCommasToNumber } from '~/helpers/stringHelpers';
-import { qualityProps } from '~/constants/defaultValues';
-
-const youLabel = localize('You');
+import { useChatContext } from '~/contexts';
 
 export default function AICards() {
-  const { userId: myId } = useKeyContext((v) => v.myState);
   const aiCardFeedIds = useChatContext((v) => v.state.aiCardFeedIds);
   const aiCardFeedObj = useChatContext((v) => v.state.aiCardFeedObj);
   const cardObj = useChatContext((v) => v.state.cardObj);
@@ -20,48 +14,18 @@ export default function AICards() {
     [aiCardFeedIds, aiCardFeedObj]
   );
 
+  const lastActivity = useMemo(
+    () => aiCardFeeds?.[aiCardFeeds?.length - 1],
+    [aiCardFeeds]
+  );
+
   const card = useMemo(() => {
-    const lastActivity = aiCardFeeds?.[aiCardFeeds?.length - 1];
     let cardId = lastActivity?.contentId;
     if (lastActivity?.type === 'offer') cardId = lastActivity?.offer?.cardId;
     if (lastActivity?.type === 'transfer')
       cardId = lastActivity?.transfer?.cardId;
     return cardObj[cardId];
-  }, [aiCardFeeds, cardObj]);
-
-  const user = useMemo(() => {
-    const lastActivity = aiCardFeeds?.[aiCardFeeds?.length - 1];
-    if (lastActivity?.type === 'offer') {
-      return lastActivity?.offer?.user;
-    }
-    if (lastActivity?.type === 'transfer') {
-      if (lastActivity?.transfer?.askId) {
-        return lastActivity?.transfer?.to;
-      }
-      if (lastActivity?.transfer?.offerId) {
-        return lastActivity?.transfer?.from;
-      }
-    }
-    return card?.creator;
-  }, [aiCardFeeds, card]);
-
-  const action = useMemo(() => {
-    const lastActivity = aiCardFeeds?.[aiCardFeeds?.length - 1];
-    if (lastActivity?.type === 'offer') {
-      return `offered ${addCommasToNumber(lastActivity?.offer?.price)} coin${
-        lastActivity?.offer?.price > 1 ? 's' : ''
-      } for a${card?.quality === 'elite' ? 'n' : ''}`;
-    }
-    if (lastActivity?.type === 'transfer') {
-      if (lastActivity?.transfer?.askId) {
-        return `bought a${card?.quality === 'elite' ? 'n' : ''}`;
-      }
-      if (lastActivity?.transfer?.offerId) {
-        return `sold a${card?.quality === 'elite' ? 'n' : ''}`;
-      }
-    }
-    return `summoned a${card?.quality === 'elite' ? 'n' : ''}`;
-  }, [aiCardFeeds, card?.quality]);
+  }, [lastActivity, cardObj]);
 
   return (
     <div style={{ height: '5rem', position: 'relative' }}>
@@ -88,11 +52,7 @@ export default function AICards() {
               width: '100%'
             }}
           >
-            {user.id === myId ? youLabel : user.username}: {action}{' '}
-            <span style={{ ...qualityProps[card.quality] }}>
-              {card.quality}
-            </span>{' '}
-            card
+            {lastActivity?.description}
           </p>
         </div>
       )}
