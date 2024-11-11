82b3769caebc0e0012105af6624bc66bc65183f5
mikey1384 <mikey1384@gmail.com>
2024-11-11 21:43:43 +0700

    more fix

--- src/contexts/requestHelpers/axiosInstance.ts
+++ src/contexts/requestHelpers/axiosInstance.ts
@@ -3,19 +3,12 @@ import URL from '~/constants/URL';
 // import { userIdRef } from '~/constants/state';
 
 let isOnline = navigator.onLine;
-const failedQueue = new Map();
-let isRetrying = false;
-
-window.addEventListener('online', () => {
-  isOnline = true;
-  retryFailedRequests();
-});
 
 window.addEventListener('offline', () => {
   isOnline = false;
 });
 
-const MIN_TIMEOUT = 5000;
+const MIN_TIMEOUT = 2000;
 const MAX_TIMEOUT = 30000;
 
 const axiosInstance = axios.create({
@@ -25,31 +18,6 @@ const axiosInstance = axios.create({
     Expires: '0'
   }
 });
-
-function getRequestKey(config: any) {
-  return `${config.method}-${config.url}-${JSON.stringify(
-    config.data || {}
-  )}-${JSON.stringify(config.params || {})}`;
-}
-
-async function retryFailedRequests() {
-  if (isRetrying || failedQueue.size === 0) return;
-  isRetrying = true;
-
-  const queue = new Map(failedQueue);
-
-  for (const [requestKey, { config, resolve, reject }] of queue) {
-    try {
-      const response = await axiosInstance(config);
-      resolve(response);
-      failedQueue.delete(requestKey);
-    } catch (error) {
-      reject(error);
-    }
-  }
-
-  isRetrying = false;
-}
 
 axiosInstance.interceptors.request.use(async (config: any) => {
   const isApiRequest = config.url?.startsWith(URL);
@@ -98,32 +66,13 @@ axiosInstance.interceptors.response.use(
         error.message.includes('Network Error')
       ) {
         config.__retryCount += 1;
-
-        if (!isOnline) {
-          return new Promise((resolve, reject) => {
-            const requestKey = getRequestKey(config);
-            if (!failedQueue.has(requestKey)) {
-              failedQueue.set(requestKey, { config, resolve, reject });
-            }
-          });
-        } else {
-          await new Promise((resolve) => setTimeout(resolve, 1000));
-          return axiosInstance(config);
-        }
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        return axiosInstance(config);
       }
     }
 
     return Promise.reject(error);
   }
 );
-
-document.addEventListener('visibilitychange', () => {
-  if (document.visibilityState === 'visible') {
-    isOnline = navigator.onLine;
-    if (isOnline) {
-      retryFailedRequests();
-    }
-  }
-});
 
 export default axiosInstance;
