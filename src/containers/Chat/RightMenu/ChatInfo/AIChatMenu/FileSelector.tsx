9436df0eb5af05e4960bbe3ccbdc0448da94b73c
mikey1384 <mikey1384@gmail.com>
2024-11-23 10:04:28 +0700

    more fix

--- src/containers/Chat/RightMenu/ChatInfo/AIChatMenu/FileSelector.tsx
+++ src/containers/Chat/RightMenu/ChatInfo/AIChatMenu/FileSelector.tsx
@@ -64,18 +64,9 @@ export default function FileSelector({
         prevScrollHeightRef.current = scrollContainer.scrollHeight;
 
         try {
-          const date = new Date(files[0]?.lastUsed || '');
-          const year = date.getFullYear();
-          const month = String(date.getMonth() + 1).padStart(2, '0');
-          const day = String(date.getDate()).padStart(2, '0');
-          const hours = String(date.getHours()).padStart(2, '0');
-          const minutes = String(date.getMinutes()).padStart(2, '0');
-          const seconds = String(date.getSeconds()).padStart(2, '0');
-          const lastFileLastUsed = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-          console.log(lastFileLastUsed);
           const { files: newFiles, fileDataObj } = await loadAIChatFiles({
             channelId,
-            lastFileLastUsed
+            lastFileLastUsed: files[0]?.lastUsed
           });
           onLoadMoreAIChatFiles({
             channelId,
