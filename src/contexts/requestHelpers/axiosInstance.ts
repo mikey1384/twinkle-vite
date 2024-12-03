99636855c938ffacf3f40849828a8c06c36edcbd
mikey1384 <mikey1384@gmail.com>
2024-12-03 11:38:58 +0700

    more fix

--- src/contexts/requestHelpers/axiosInstance.ts
+++ src/contexts/requestHelpers/axiosInstance.ts
@@ -25,7 +25,7 @@ const axiosInstance = axios.create({
   }
 });
 
-const limit = pLimit(5); // Limit to 5 concurrent requests (adjust as needed)
+const limit = pLimit(1); // Limit to 5 concurrent requests (adjust as needed)
 
 // Map to store the state associated with each request identifier
 const requestStateMap = new Map<string, RetryConfig>();
