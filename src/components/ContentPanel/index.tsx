8d7e0737cca3c386833d7547f58fc3412cccddfa
mikey1384 <mikey1384@gmail.com>
2024-12-10 19:26:54 +0700

    more fix

--- src/components/ContentPanel/index.tsx
+++ src/components/ContentPanel/index.tsx
@@ -10,7 +10,6 @@ import Embedly from '~/components/Embedly';
 import Profile from './Profile';
 import { css } from '@emotion/css';
 import { Color, borderRadius, mobileMaxWidth } from '~/constants/css';
-import { container } from './Styles';
 import { placeholderHeights } from '~/constants/state';
 import { useContentState, useLazyLoad } from '~/helpers/hooks';
 import { useAppContext, useContentContext, useKeyContext } from '~/contexts';
@@ -57,6 +56,7 @@ export default function ContentPanel({
   autoExpand,
   className,
   commentsLoadLimit,
+  isContentPage,
   feedId,
   contentId,
   contentType,
@@ -72,6 +72,7 @@ export default function ContentPanel({
   className?: string;
   commentsLoadLimit?: number;
   feedId?: number;
+  isContentPage?: boolean;
   contentId: number;
   contentType: string;
   rootType?: string;
@@ -225,6 +226,90 @@ export default function ContentPanel({
   ) {
     return null;
   }
+
+  const container = useMemo(
+    () => css`
+      background: #fff;
+      width: 100%;
+      border: 1px solid ${Color.borderGray()};
+      border-radius: ${borderRadius};
+      position: static;
+      &:last-child {
+        margin-bottom: 0;
+      }
+      .heading {
+        user-select: none;
+        padding: 1rem;
+        display: flex;
+        align-items: center;
+        width: 100%;
+        justify-content: space-between;
+      }
+      .body {
+        width: 100%;
+        font-size: 1.7rem;
+        padding: 0;
+        position: static;
+        z-index: 10;
+        .bottom-interface {
+          padding: 0 1rem 0 1rem;
+          display: flex;
+          flex-direction: column;
+          @media (max-width: ${mobileMaxWidth}) {
+            padding: 0 0.5rem 0 0.5rem;
+          }
+        }
+      }
+      .content-panel__likes {
+        font-weight: bold;
+        color: ${Color.darkerGray()};
+        font-size: 1.2rem;
+        line-height: 1;
+      }
+      .subject {
+        font-size: 2rem;
+        font-weight: bold;
+        white-space: pre-wrap;
+        overflow-wrap: break-word;
+        word-break: break-word;
+        margin-bottom: 1.5rem;
+      }
+      .title {
+        font-size: 1.7rem;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: pre-wrap;
+        overflow-wrap: break-word;
+        word-break: break-word;
+      }
+      .timestamp {
+        font-size: 1rem;
+        color: ${Color.gray()};
+      }
+      @media (max-width: ${mobileMaxWidth}) {
+        border-radius: 0;
+        ${isContentPage ? 'border-top: none;' : ''}
+        border-left: none;
+        border-right: none;
+        .body {
+          font-size: 1.8rem;
+        }
+        .heading {
+          > a,
+          > span {
+            font-size: 1.7rem;
+          }
+          > small {
+            font-size: 1.2rem;
+          }
+          > button {
+            font-size: 1.2rem;
+          }
+        }
+      }
+    `,
+    [isContentPage]
+  );
 
   return (
     <ErrorBoundary componentPath="ContentPanel/index">
