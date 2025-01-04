386b47d7a13a926ae80131d9aaff6fb8d27dbbc9
mikey1384 <mikey1384@gmail.com>
2025-01-04 09:42:31 +0700

    separate collectors component as a reusable component

---
+++ src/containers/Chat/RightMenu/VocabInfo/TopMenu.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { Color } from '~/constants/css';
+import { useChatContext, useKeyContext } from '~/contexts';
+import CollectorRankingList from './CollectorRankingList';
+import localize from '~/constants/localize';
+import Loading from '~/components/Loading';
+
+const collectorsOfHighLevelWordsLabel = localize('collectorsOfHighLevelWords');
+
+export default function TopMenu() {
+  const { numWordsCollected } = useKeyContext((v) => v.myState);
+  const { all, top30s } = useChatContext((v) => v.state.wordCollectors);
+  const loadingVocabulary = useChatContext((v) => v.state.loadingVocabulary);
+  const [allSelected, setAllSelected] = useState(numWordsCollected > 0);
+
+  return (
+    <div
+      style={{
+        height: '50%',
+        borderBottom: `1px solid ${Color.borderGray()}`,
+        overflow: 'scroll'
+      }}
+    >
+      {loadingVocabulary ? (
+        <Loading style={{ height: '100%' }} />
+      ) : (
+        <>
+          <div
+            style={{
+              fontSize: '1.7rem',
+              padding: '1rem',
+              textAlign: 'center',
+              fontWeight: 'bold',
+              background: Color.brownOrange(),
+              color: '#fff'
+            }}
+          >
+            {collectorsOfHighLevelWordsLabel}
+          </div>
+          <CollectorRankingList
+            allCollectors={all || []}
+            top30Collectors={top30s || []}
+            hasWordsCollected={numWordsCollected > 0}
+            allSelected={allSelected}
+            onSetAllSelected={setAllSelected}
+          />
+        </>
+      )}
+    </div>
+  );
+}
