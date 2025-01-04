1f977f9f162489947e95096ee79039da1f3e3354
mikey1384 <mikey1384@gmail.com>
2025-01-04 08:42:46 +0700

    refactor the right side vocab menu

---
+++ src/containers/Chat/RightMenu/VocabInfo/BottomMenu/XPRankings.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import MyRank from '~/components/MyRank';
+import RankingsListItem from '~/components/RankingsListItem';
+import ErrorBoundary from '~/components/ErrorBoundary';
+
+export default function XPRankings({
+  userId,
+  rank,
+  twinkleXP,
+  allRanks
+}: {
+  userId: number;
+  rank: number;
+  twinkleXP: number;
+  allRanks: any[];
+}) {
+  return (
+    <ErrorBoundary componentPath="Chat/RightMenu/VocabInfo/BottomMenu/XPRankings">
+      <div>
+        <MyRank
+          noBorderRadius
+          myId={userId}
+          rank={rank}
+          twinkleXP={twinkleXP}
+          style={{ marginTop: 0 }}
+        />
+        {allRanks.map((user) => (
+          <RankingsListItem
+            key={user.id}
+            small
+            style={{
+              padding: '1rem'
+            }}
+            myId={userId}
+            user={user}
+          />
+        ))}
+      </div>
+    </ErrorBoundary>
+  );
+}
