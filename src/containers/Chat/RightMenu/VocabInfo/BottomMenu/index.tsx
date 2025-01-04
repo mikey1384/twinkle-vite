b66aa865ebb9f1d9be254bc8e96606d9797fc89d
mikey1384 <mikey1384@gmail.com>
2025-01-04 08:42:46 +0700

    refactor the right side vocab menu

---
+++ src/containers/Chat/RightMenu/VocabInfo/BottomMenu/index.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import FilterBar from '~/components/FilterBar';
+import XPRankings from './XPRankings';
+import League from './League';
+
+export default function BottomMenu({
+  rank,
+  allRanks,
+  twinkleXP,
+  userId
+}: {
+  rank: number;
+  allRanks: any[];
+  twinkleXP: number;
+  userId: number;
+}) {
+  const [selected, setSelected] = useState('xp');
+  return (
+    <div style={{ height: '50%' }}>
+      <FilterBar
+        style={{ fontSize: '1.5rem', height: '4rem', marginBottom: '0.5rem' }}
+      >
+        <nav
+          onClick={() => setSelected('league')}
+          className={selected === 'league' ? 'active' : ''}
+        >
+          2025 League
+        </nav>
+        <nav
+          onClick={() => setSelected('xp')}
+          className={selected === 'xp' ? 'active' : ''}
+        >
+          XP
+        </nav>
+      </FilterBar>
+      <div style={{ overflow: 'scroll', height: 'calc(100% - 4rem - 0.5rem)' }}>
+        {selected === 'xp' ? (
+          <XPRankings
+            userId={userId}
+            rank={rank}
+            twinkleXP={twinkleXP}
+            allRanks={allRanks}
+          />
+        ) : (
+          <League />
+        )}
+      </div>
+    </div>
+  );
+}
