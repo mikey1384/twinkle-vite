3d983ab02989dc8fc7e91a71d881eb1ce5036644
mikey1384 <mikey1384@gmail.com>
2025-07-06 12:04:23 +0700

    minor fix

--- src/containers/Chat/Body/MessagesContainer/MessageInput/InputArea.tsx
+++ src/containers/Chat/Body/MessagesContainer/MessageInput/InputArea.tsx
@@ -58,24 +58,6 @@ export default function InputArea({
   const { userId } = useKeyContext((v) => v.myState);
 
   useEffect(() => {
-    if (
-      inputText &&
-      ((isOwnerPostingOnly && isMain) ||
-        (isOnlyOwnerPostingTopic && !isMain)) &&
-      !isOwner
-    ) {
-      onSetText('');
-    }
-  }, [
-    inputText,
-    isOwnerPostingOnly,
-    isOnlyOwnerPostingTopic,
-    isMain,
-    isOwner,
-    onSetText
-  ]);
-
-  useEffect(() => {
     const handleResize = () => {
       if (innerRef.current) {
         onHeightChange(innerRef.current.clientHeight);
