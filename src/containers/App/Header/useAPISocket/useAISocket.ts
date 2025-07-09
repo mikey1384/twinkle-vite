bfedc07a931497cddbbc7eb075218aa522e90201
mikey1384 <mikey1384@gmail.com>
2025-07-09 12:30:27 +0700

    more updates

--- src/containers/App/Header/useAPISocket/useAISocket.ts
+++ src/containers/App/Header/useAPISocket/useAISocket.ts
@@ -28,14 +28,13 @@ export default function useAISocket({
   const onSetChannelState = useChatContext((v) => v.actions.onSetChannelState);
   const channelsObj = useChatContext((v) => v.state.channelsObj);
   const onSetAICall = useChatContext((v) => v.actions.onSetAICall);
+  const onUpdateLastUsedFiles = useChatContext(
+    (v) => v.actions.onUpdateLastUsedFiles
+  );
+  const onDeleteMessage = useChatContext((v) => v.actions.onDeleteMessage);
   const onChangeAIThinkingStatus = useChatContext(
     (v) => v.actions.onChangeAIThinkingStatus
   );
-  const onUpdateLastUsedFile = useChatContext(
-    (v) => v.actions.onUpdateLastUsedFile
-  );
-  const onDeleteMessage = useChatContext((v) => v.actions.onDeleteMessage);
-  const channelsObj = useChatContext((v) => v.state.channelsObj);
 
   const onUpdateTodayStats = useNotiContext(
     (v) => v.actions.onUpdateTodayStats
@@ -157,7 +156,7 @@ export default function useAISocket({
     socket.on('ai_message_error', handleAIMessageError);
     socket.on('ai_call_duration_updated', handleAICallDurationUpdate);
     socket.on('ai_call_max_duration_reached', handleAICallMaxDurationReached);
-    socket.on('last_used_file_updated', onUpdateLastUsedFile);
+    socket.on('last_used_files_updated', onUpdateLastUsedFiles);
     socket.on('subtitle_translation_progress_update', handleSubtitleProgress);
     socket.on('subtitle_merge_progress_update', handleSubtitleMergeProgress);
 
@@ -176,7 +175,7 @@ export default function useAISocket({
         'ai_call_max_duration_reached',
         handleAICallMaxDurationReached
       );
-      socket.off('last_used_file_updated', onUpdateLastUsedFile);
+      socket.off('last_used_files_updated', onUpdateLastUsedFiles);
       socket.off(
         'subtitle_translation_progress_update',
         handleSubtitleProgress
