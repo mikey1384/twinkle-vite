9783e238c9775cb48f443ac00fa9160207ab6f46
mikey1384 <mikey1384@gmail.com>
2025-07-27 08:56:55 +0700

    version up

--- src/containers/Profile/Body/Home/Pictures/AddPictureModal/index.tsx
+++ src/containers/Profile/Body/Home/Pictures/AddPictureModal/index.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import Modal from '~/components/Modal';
 import Button from '~/components/Button';
 import StartScreen from './StartScreen';
 import SelectFromArchive from './SelectFromArchive';
 import { useKeyContext } from '~/contexts';
+import NewModal from '~/components/NewModal';
 
 AddPictureModal.propTypes = {
   currentPictures: PropTypes.array.isRequired,
@@ -32,56 +32,60 @@ export default function AddPictureModal({
   const [selectedPictureIds, setSelectedPictureIds] = useState([]);
 
   return (
-    <Modal large={section === 'archive'} onHide={onHide}>
-      <header>
-        Add Picture{section === 'archive' ? `s from Archive` : ''}
-      </header>
-      <main>
-        {section === 'start' && (
-          <StartScreen
-            profileId={profileId}
-            navigateTo={setSection}
-            onHide={onHide}
-          />
-        )}
-        {section === 'archive' && (
-          <SelectFromArchive
-            currentPictures={currentPictures}
-            selectedPictureIds={selectedPictureIds}
-            onSetSelectedPictureIds={setSelectedPictureIds}
-          />
-        )}
-      </main>
-      <footer>
-        <Button
-          transparent
-          onClick={
-            section === 'start'
-              ? onHide
-              : () => {
-                  setSection('start');
-                  setSelectedPictureIds([]);
-                }
-          }
-        >
-          {section === 'start' ? 'Cancel' : 'Back'}
-        </Button>
-        {section !== 'start' && (
+    <NewModal
+      isOpen={true}
+      onClose={onHide}
+      title={`Add Picture${section === 'archive' ? `s from Archive` : ''}`}
+      size="lg"
+      closeOnBackdropClick={false}
+      modalLevel={0}
+      footer={
+        <>
           <Button
-            disabled={
-              selectedPictureIds.length === 0 ||
-              selectedPictureIds.length > maxNumSelectable
+            transparent
+            onClick={
+              section === 'start'
+                ? onHide
+                : () => {
+                    setSection('start');
+                    setSelectedPictureIds([]);
+                  }
             }
-            color={doneColor}
-            style={{ marginLeft: '0.7rem' }}
-            onClick={() => onConfirm({ selectedPictureIds })}
           >
-            {selectedPictureIds.length > maxNumSelectable
-              ? `Cannot select more than ${maxNumSelectable}`
-              : 'Confirm'}
+            {section === 'start' ? 'Cancel' : 'Back'}
           </Button>
-        )}
-      </footer>
-    </Modal>
+          {section !== 'start' && (
+            <Button
+              disabled={
+                selectedPictureIds.length === 0 ||
+                selectedPictureIds.length > maxNumSelectable
+              }
+              color={doneColor}
+              style={{ marginLeft: '0.7rem' }}
+              onClick={() => onConfirm({ selectedPictureIds })}
+            >
+              {selectedPictureIds.length > maxNumSelectable
+                ? `Cannot select more than ${maxNumSelectable}`
+                : 'Confirm'}
+            </Button>
+          )}
+        </>
+      }
+    >
+      {section === 'start' && (
+        <StartScreen
+          profileId={profileId}
+          navigateTo={setSection}
+          onHide={onHide}
+        />
+      )}
+      {section === 'archive' && (
+        <SelectFromArchive
+          currentPictures={currentPictures}
+          selectedPictureIds={selectedPictureIds}
+          onSetSelectedPictureIds={setSelectedPictureIds}
+        />
+      )}
+    </NewModal>
   );
 }
