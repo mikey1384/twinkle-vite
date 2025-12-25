096f698d64c43b721e9444936d4ef09dfcf2b1fa
mikey1384 <mikey1384@gmail.com>
2025-12-25 09:14:37 +0700

    more fix

--- src/containers/Home/Store/DonorLicenseItem.tsx
+++ src/containers/Home/Store/DonorLicenseItem.tsx
@@ -271,6 +271,12 @@ export default function DonorLicenseItem({
 
             &:hover:not(:disabled) {
               color: #fff;
+              background: linear-gradient(
+                  180deg,
+                  rgba(255, 255, 255, 0.25) 0%,
+                  rgba(0, 0, 0, 0.08) 100%
+                ),
+                ${Color.pinkOrange(0.9)};
             }
 
             &:active:not(:disabled) {
@@ -278,6 +284,12 @@ export default function DonorLicenseItem({
               box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1),
                 inset 0 1px 3px rgba(0, 0, 0, 0.2);
               color: #fff;
+              background: linear-gradient(
+                  180deg,
+                  rgba(0, 0, 0, 0.05) 0%,
+                  rgba(0, 0, 0, 0.1) 100%
+                ),
+                ${Color.pinkOrange()};
             }
 
             &:disabled {
