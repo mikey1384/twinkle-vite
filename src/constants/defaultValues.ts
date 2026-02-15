fab592091bcd6f4081e3eb48677bd76ca5f5f944
mikey1384 <mikey1384@gmail.com>
2026-02-15 11:40:02 +0700

    concrete migration to 2 twinkle-api instance system to prevent disruption during api update

--- src/constants/defaultValues.ts
+++ src/constants/defaultValues.ts
@@ -4,7 +4,7 @@ import { Color } from '~/constants/css';
 // General Configurations
 // ===========================
 
-export const clientVersion = '2.0.42';
+export const clientVersion = '2.0.43';
 
 export const BOOKMARK_VIEWS = {
   AI: 'ai',
